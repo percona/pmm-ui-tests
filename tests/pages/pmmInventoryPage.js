77b0d2690c2d18b779b10cbedf59a84fb3e1e0db
Ivana Wzientková <ivana.wzientkova@percona.com>
2021-04-20 12:49:19 +0200

    Get count of agents

--- tests/pages/pmmInventoryPage.js
+++ tests/pages/pmmInventoryPage.js
@@ -237,7 +237,7 @@ module.exports = {
     hence (pmm-agent, Node exporter, postgres exporter, mysql exporter, QAN RDS,
     QAN postgres, RDS exporter, QAN PostgreSQL PgStatements Agent)
      */
-    assert.ok((otherDetails <= 8 && otherDetails >= 4), 'Total Agents running on PMM-Server Instance can not be greater then 8');
+    assert.ok((otherDetails <= 8 && otherDetails >= 4), `Total Agents running: ${otherDetails} on PMM-Server Instance can not be greater then 8`);
     assert.ok(countBefore > countAfter, `Some PMM Agents should have been deleted, Agents running before deleting ${countBefore} and after deleting ${countAfter}`);
   },
 
