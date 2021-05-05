cb18a87f5489f5e28f195b12bc631e8557e3fb9b
Vasyl Yurkovych <59879559+yurkovychv@users.noreply.github.com>
2021-05-05 18:52:04 +0300

    PMM-7588 Clean up tags (#14)
    * PMM-7588 clean up tags
    
    * PMM-7588 skipped failing test
    
    * PMM-7588 add ability to configure timeout for verifyPopUpMessage method
    
    * PMM-7588 add @not-ui-pipeline tags for upgrade tests

--- tests/QAN/timerange_test.js
+++ tests/QAN/timerange_test.js
@@ -7,7 +7,7 @@ Before(async ({ I, qanPage, qanOverview }) => {
 });
 
 Scenario(
-  'Open the QAN Dashboard and check that changing the time range resets current page to the first. @qan @not-pr-pipeline',
+  'Open the QAN Dashboard and check that changing the time range resets current page to the first. @qan',
   async ({ adminPage, qanPagination, qanOverview }) => {
     qanPagination.selectPage(2);
     adminPage.applyTimeRange('Last 3 hours');
@@ -17,7 +17,7 @@ Scenario(
 );
 
 Scenario(
-  'Open the QAN Dashboard and check that changing the time range updates the overview table and URL. @not-pr-pipeline @qan',
+  'Open the QAN Dashboard and check that changing the time range updates the overview table and URL. @qan',
   async ({ I, adminPage, qanOverview }) => {
     const TIME_RANGE_QUERY_PARAMS_BEFORE = 'from=now-5m&to=now';
     const TIME_RANGE_QUERY_PARAMS_AFTER = 'from=now-3h&to=now';
@@ -30,7 +30,7 @@ Scenario(
 );
 
 Scenario(
-  'Open the QAN Dashboard and check that changing the time range doesn"t clear "Group by". @not-pr-pipeline @qan',
+  'Open the QAN Dashboard and check that changing the time range doesn"t clear "Group by". @qan',
   async ({ adminPage, qanOverview }) => {
     await qanOverview.changeGroupBy('Client Host');
     adminPage.applyTimeRange('Last 24 hours');
@@ -39,7 +39,7 @@ Scenario(
 );
 
 Scenario(
-  'Open the QAN Dashboard and check that changing the time range doesn\'t reset sorting. @not-pr-pipeline @qan',
+  'Open the QAN Dashboard and check that changing the time range doesn\'t reset sorting. @qan',
   async ({ adminPage, qanOverview }) => {
     await qanOverview.changeSorting(1);
     adminPage.applyTimeRange('Last 24 hours');
