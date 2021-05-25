67a3d2e9f31f5e871a9a7e78ce02c2f762345bfa
Vasyl Yurkovych <59879559+yurkovychv@users.noreply.github.com>
2021-05-25 14:05:34 +0300

    PMM-8145 unskip postgresql rds dashboard test OVF

--- tests/verifyAWSRDSPostgreSQLInstance_test.js
+++ tests/verifyAWSRDSPostgreSQLInstance_test.js
@@ -32,7 +32,7 @@ Scenario(
 );
 
 Scenario(
-  'PMM-T716 - Verify Dashboard for Postgres RDS added via UI @instances @not-ovf',
+  'PMM-T716 - Verify Dashboard for Postgres RDS added via UI @instances',
   async ({
     I, dashboardPage,
   }) => {
