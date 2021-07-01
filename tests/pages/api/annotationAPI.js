8993c7d478c5e72d20df5b00459efeda1f891d12
Ivana <ivana.wzientkova.sde@gmail.com>
2021-07-01 13:48:56 +0200

    Change API call for add annotation

--- tests/pages/api/annotationAPI.js
+++ tests/pages/api/annotationAPI.js
@@ -1,9 +1,7 @@
 const { I } = inject();
 const assert = require('assert');
 
-/* eslint-disable consistent-return */
 module.exports = {
-
   async setAnnotation(annotationName, tags, nodeName, serviceNames) {
     const headers = { Authorization: `Basic ${await I.getAuth()}` };
     const body = {
@@ -15,14 +13,7 @@ module.exports = {
 
     const resp = await I.sendPostRequest('v1/management/Annotations/Add', body, headers);
 
-    if (resp.status !== 200) {
-      return resp.status;
-    }
-
-    assert.ok(
-      resp.status === 200,
-      `Failed to add annotation for service/s name: ${serviceNames}. Response message is ${resp.data.message}`,
-    );
+    return resp.status;
   },
 
   async setAnnotationWithoutServiceName(annotationName, tags, nodeName) {
