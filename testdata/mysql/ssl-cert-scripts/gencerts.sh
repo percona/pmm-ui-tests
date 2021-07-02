242ba1f9ffcd396558f5e3e85d96cdbda5bbc2e2
puneet0191 <puneet.0191@gmail.com>
2021-07-02 20:20:19 +0530

    PMM-8349 debug

--- testdata/mysql/ssl-cert-scripts/gencerts.sh
+++ testdata/mysql/ssl-cert-scripts/gencerts.sh
@@ -7,9 +7,12 @@ OPENSSL_SERVER="${OPENSSL_SUBJ}/CN=fake-server"
 OPENSSL_CLIENT="${OPENSSL_SUBJ}/CN=fake-client"
 
 sh ./genroot.sh "${OPENSSL_CA}"
+sleep 5
 sh ./genserver.sh "${OPENSSL_SERVER}"
+sleep 5
 sh ./genclient.sh "${OPENSSL_CLIENT}"
 
+sleep 10
 openssl rsa -in certs/client-key.pem -out certs/client-key.pem
 openssl rsa -in certs/server-key.pem -out certs/server-key.pem
 openssl verify -CAfile certs/root-ca.pem certs/server-cert.pem certs/client-cert.pem
