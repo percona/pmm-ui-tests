68603f2d5824ee91726e26d039baea3d56df25b3
puneet0191 <puneet.0191@gmail.com>
2021-07-19 14:23:02 +0530

    PMM-8339 fix the ports

--- tests/upgradePMM_test.js
+++ tests/upgradePMM_test.js
@@ -45,7 +45,7 @@ Before(async ({ I }) => {
 BeforeSuite(async ({ I, codeceptjsConfig }) => {
   const mysqlComposeConnection = {
     host: (process.env.AMI_UPGRADE_TESTING_INSTANCE === 'true' ? process.env.VM_CLIENT_IP : '127.0.0.1'),
-    port: connection.port,
+    port: (process.env.AMI_UPGRADE_TESTING_INSTANCE === 'true' ? remoteInstancesHelper.remote_instance.mysql.ps_5_7.port : connection.port),
     username: connection.username,
     password: connection.password,
   };
@@ -55,7 +55,7 @@ BeforeSuite(async ({ I, codeceptjsConfig }) => {
   // Connect to MongoDB
   const mongoConnection = {
     host: (process.env.AMI_UPGRADE_TESTING_INSTANCE === 'true' ? process.env.VM_CLIENT_IP : codeceptjsConfig.config.helpers.MongoDBHelper.host),
-    port: codeceptjsConfig.config.helpers.MongoDBHelper.port,
+    port: (process.env.AMI_UPGRADE_TESTING_INSTANCE === 'true' ? remoteInstancesHelper.remote_instance.mongodb.psmdb_4_2.port : codeceptjsConfig.config.helpers.MongoDBHelper.port),
     username: codeceptjsConfig.config.helpers.MongoDBHelper.username,
     password: codeceptjsConfig.config.helpers.MongoDBHelper.password,
   };
