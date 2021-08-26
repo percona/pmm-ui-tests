29a07b45e839664e208b04a8ebea4e81c3480e00
puneet0191 <puneet.0191@gmail.com>
2021-08-26 15:33:20 +0530

    PMM-8625 typo

--- testdata/docker-db-setup-scripts/docker_postgres_ssl_13.sh
+++ testdata/docker-db-setup-scripts/docker_postgres_ssl_13.sh
@@ -2,7 +2,7 @@
 
 pushd testdata/mysql/ssl-cert-scripts/
 bash ./gencerts.sh
-chown 0:70 certs/serve-key.pem
+chown 0:70 certs/server-key.pem
 chmod 640 certs/server-key.pem
 popd
 PWD=$(pwd) docker-compose -f docker-compose-postgresql-ssl.yml up -d
