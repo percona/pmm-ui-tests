03b00e6d1a1503f96a1cf426d6c88f47cd306aa1
puneet0191 <puneet.0191@gmail.com>
2021-09-03 21:55:06 +0530

    PMM-8625 fix permission

--- testdata/docker-db-setup-scripts/docker_postgres_ssl_13.sh
+++ testdata/docker-db-setup-scripts/docker_postgres_ssl_13.sh
@@ -5,7 +5,7 @@ cp testdata/mysql/ssl-cert-scripts/*.sh testdata/pgsql/ssl-cert-scripts
 pushd testdata/pgsql/ssl-cert-scripts
 bash ./gencerts.sh
 sudo chown 0:70 certs/server-key.pem
-sudo chmod 777 certs/server-key.pem
+sudo chmod 600 certs/server-key.pem
 ls -la
 popd
 PWD=$(pwd) docker-compose -f docker-compose-postgresql-ssl.yml up -d
