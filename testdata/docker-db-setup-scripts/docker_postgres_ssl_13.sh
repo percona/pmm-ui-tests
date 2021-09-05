85f23b6cccc5193814c839d39b3535d281256d91
puneet0191 <puneet.0191@gmail.com>
2021-09-05 13:19:44 +0530

    PMM-8625 permission error

--- testdata/docker-db-setup-scripts/docker_postgres_ssl_13.sh
+++ testdata/docker-db-setup-scripts/docker_postgres_ssl_13.sh
@@ -4,7 +4,7 @@ mkdir -p testdata/pgsql/ssl-cert-scripts
 cp testdata/mysql/ssl-cert-scripts/*.sh testdata/pgsql/ssl-cert-scripts
 pushd testdata/pgsql/ssl-cert-scripts
 bash ./gencerts.sh
-sudo chown $UID:$GID certs/server-key.pem
+sudo chown 70:70 certs/server-key.pem
 sudo chmod 600 certs/server-key.pem
 ls -la
 popd
