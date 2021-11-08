8cc0411094f7bd09487e1289de05d4f903acb99b
Vadym Yarosh <vadym.yarosh@percona.com>
2021-11-08 18:13:48 +0200

    PMM-7425 permission fix

--- tests/verifyPMMInventory_test.js
+++ tests/verifyPMMInventory_test.js
@@ -1,7 +1,7 @@
 const assert = require('assert');
 const faker = require('faker');
 
-const pmmManagerCmd = '/srv/pmm-qa/pmm-tests/pmm-framework.sh';
+const pmmManagerCmd = 'sudo /srv/pmm-qa/pmm-tests/pmm-framework.sh';
 const mysqlServiceName = `mysql-push-mode-${faker.datatype.number()}`;
 const postgresServiceName = `postgres-push-mode-${faker.datatype.number()}`;
 const mongoServiceName = `mongo-push-mode-${faker.datatype.number()}`;
