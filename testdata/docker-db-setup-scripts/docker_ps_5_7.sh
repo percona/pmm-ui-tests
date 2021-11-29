f90a77040c2eb53084d63aba5647eec2e364b6be
Puneet Kala <puneet.0191@gmail.com>
2021-11-29 11:55:03 +0530

    Not needed for 5.7

--- testdata/docker-db-setup-scripts/docker_ps_5_7.sh
+++ testdata/docker-db-setup-scripts/docker_ps_5_7.sh
@@ -10,4 +10,4 @@ docker exec pmm-agent_mysql_5_7 mysql -h 127.0.0.1 -u root -pps -e "INSTALL PLUG
 docker exec pmm-agent_mysql_5_7 mysql -h 127.0.0.1 -u root -pps -e "INSTALL PLUGIN QUERY_RESPONSE_TIME_READ SONAME 'query_response_time.so';"
 docker exec pmm-agent_mysql_5_7 mysql -h 127.0.0.1 -u root -pps -e "INSTALL PLUGIN QUERY_RESPONSE_TIME_WRITE SONAME 'query_response_time.so';"
 docker exec pmm-agent_mysql_5_7 mysql -h 127.0.0.1 -u root -pps -e "SET GLOBAL query_response_time_stats=ON;"
-docker exec pmm-agent_mysql_5_7 mysql -h 127.0.0.1 -u root -pps -e "GRANT SELECT, PROCESS, REPLICATION CLIENT, RELOAD, BACKUP_ADMIN ON *.* TO 'pmm-agent'@'%';"
+docker exec pmm-agent_mysql_5_7 mysql -h 127.0.0.1 -u root -pps -e "GRANT SELECT, PROCESS, REPLICATION CLIENT, RELOAD ON *.* TO 'pmm-agent'@'%';"
