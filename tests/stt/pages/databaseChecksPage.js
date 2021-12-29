7a5d0352fc0d42f2a2c64cca106a0fa2e4d4acc4
yurkovychv <vasyl.yurkovych@percona.com>
2021-12-29 18:49:17 +0200

    PMM-9325 add extra waiting for checks

--- tests/stt/pages/databaseChecksPage.js
+++ tests/stt/pages/databaseChecksPage.js
@@ -145,5 +145,8 @@ module.exports = {
     I.waitForVisible(this.buttons.startDBChecks, 30);
     I.click(this.buttons.startDBChecks);
     I.verifyPopUpMessage(this.messages.securityChecksDone, 60);
+    I.wait(15);
+    I.refreshPage();
+    I.waitForVisible(this.buttons.startDBChecks, 30);
   },
 };
