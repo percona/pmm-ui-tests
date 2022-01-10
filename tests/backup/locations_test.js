d98626971aa06a050c21f41c2684494b8124ea3b
Vasyl Yurkovych <59879559+yurkovychv@users.noreply.github.com>
2022-01-10 13:09:36 +0200

    Update locations_test.js

--- tests/backup/locations_test.js
+++ tests/backup/locations_test.js
@@ -31,7 +31,6 @@ s3Errors.add(['bucket_name', 'pmm', 'AccessDenied: Access Denied']);
 s3Errors.add(['bucket_name', 'pmm-backup12', 'Bucket doesn\'t exist']);
 s3Errors.add(['bucket_name', 'random-bucket', '301 response missing Location header']);
 s3Errors.add(['endpoint', 'unknown', 'no such host']);
-s3Errors.add(['endpoint', 'http://33.22.11.00', 'i/o timeout']);
 s3Errors.add(['access_key', 'invalid', 'InvalidAccessKeyId: The AWS Access Key Id you provided does not exist in our records.']);
 s3Errors.add(['secret_key', 'invalid', 'SignatureDoesNotMatch: The request signature we calculated does not match the signature you provided. Check your key and signing method.']);
 
