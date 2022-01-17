2f81348dbfdd8f49c276e8d3fbb6a67294b2888a
yurkovychv <vasyl.yurkovych@percona.com>
2022-01-17 18:44:43 +0200

    PMM-9465 add check for upgrade logs from fs

--- tests/pages/homePage.js
+++ tests/pages/homePage.js
@@ -113,18 +113,25 @@ module.exports = {
     I.waitForText(locators.inProgressMessage, 30, locators.updateProgressModal);
 
     // skipping milestones checks for 2.9 and 2.10, 2.11 versions due logs not showing issue
-    if (version > 11) {
+    if (version > 11 && process.env.AMI_UPGRADE_TESTING_INSTANCE === 'true') {
       for (const milestone of milestones) {
         I.waitForElement(`//pre[contains(text(), '${milestone}')]`, 1200);
       }
+    } else {
+      // Get upgrade logs from a container
+      const upgradeLogs = await I.verifyCommand('docker exec pmm-server cat /srv/logs/pmm-update-perform.log');
+
+      milestones.forEach((milestone) => {
+        assert.ok(upgradeLogs.includes(milestone), `Expected to see ${milestone} in upgrade logs`);
+      });
     }
 
     I.waitForText(locators.successUpgradeMessage, 1200, locators.successUpgradeMsgSelector);
     if (version < 12) {
       // we have a bug we need this https://jira.percona.com/browse/PMM-9294
-      I.wait(60); 
+      I.wait(60);
     }
-    
+
     I.click(locators.reloadButtonAfterUpgrade);
     locators = this.getLocators('latest');
 
