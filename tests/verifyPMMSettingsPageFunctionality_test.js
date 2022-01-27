fa4a4ff3c5b7d943594fab2b9097ca997f74f3b4
puneet0191 <puneet.0191@gmail.com>
2022-01-27 19:54:56 +0530

    PMM-T245 removing the tag for settings tests

--- tests/verifyPMMSettingsPageFunctionality_test.js
+++ tests/verifyPMMSettingsPageFunctionality_test.js
@@ -190,7 +190,7 @@ Scenario('PMM-T520 - Verify that alert is in Firing State - internal alert manag
   await pmmSettingsPage.verifyAlertmanagerRuleAdded(pmmSettingsPage.alertManager.ruleName2, true);
 });
 
-Scenario('PMM-T520 - Verify that alert is being fired to external Alert Manager @nightly @not-ovf @settings', async ({ I, pmmSettingsPage }) => {
+Scenario('PMM-T520 - Verify that alert is being fired to external Alert Manager @nightly @not-ovf', async ({ I, pmmSettingsPage }) => {
   const scheme = 'http://';
   const sectionNameToExpand = pmmSettingsPage.sectionTabsList.alertmanager;
 
