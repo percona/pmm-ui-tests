5d222f487e81b4d725d7025fe9b5047d57fbc90f
beatahandzelova <beata.handzelova.sde@gmail.com>
2022-01-31 14:34:21 +0100

    fixed warning text

--- tests/DbaaS/pages/dbaasPage.js
+++ tests/DbaaS/pages/dbaasPage.js
@@ -8,7 +8,7 @@ module.exports = {
   apiKeysUrl: 'graph/org/apikeys',
 
   apiKeysPage: {
-    apiKeysWarningText: 'If a resource (e.g. DB cluster) uses an API key, deleting that API key might affect the functionality of that resource.',
+    apiKeysWarningText: 'If a resource (for example, DB cluster) uses an API key, deleting that API key might affect the functionality of that resource.',
     apiKeysWarningLocator: '$warning-block',
     apiKeysTable: '.filter-table',
   },
