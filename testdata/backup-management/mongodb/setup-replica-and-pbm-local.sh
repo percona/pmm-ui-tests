d8c453d58ed3d574009cea7b9617d266d68d52db
Vasyl Yurkovych <59879559+yurkovychv@users.noreply.github.com>
2022-02-08 12:37:40 +0200

    Update setup-replica-and-pbm-local.sh

--- testdata/backup-management/mongodb/setup-replica-and-pbm-local.sh
+++ testdata/backup-management/mongodb/setup-replica-and-pbm-local.sh
@@ -53,9 +53,9 @@ sleep 10
 docker cp setup-replica.js mongors1:/
 docker exec mongors1 mongo --port=27027 --authenticationDatabase admin setup-replica.js
 
-docker exec -u 0 mongors1 /bin/bash -c "percona-release enable pbm release && microdnf -y install percona-backup-mongodb"
-docker exec -u 0 mongors2 /bin/bash -c "percona-release enable pbm release && microdnf -y install percona-backup-mongodb"
-docker exec -u 0 mongors3 /bin/bash -c "percona-release enable pbm release && microdnf -y install percona-backup-mongodb"
+docker exec -u 0 mongors1 /bin/bash -c "percona-release enable pbm release && yum -y install percona-backup-mongodb"
+docker exec -u 0 mongors2 /bin/bash -c "percona-release enable pbm release && yum -y install percona-backup-mongodb"
+docker exec -u 0 mongors3 /bin/bash -c "percona-release enable pbm release && yum -y install percona-backup-mongodb"
 
 docker exec  -d mongors1 /bin/bash -c 'PBM_MONGODB_URI="mongodb://pbmuser:secretpwd@localhost:27027" pbm-agent'
 docker exec  -d mongors2 /bin/bash -c 'PBM_MONGODB_URI="mongodb://pbmuser:secretpwd@localhost:27028" pbm-agent'
