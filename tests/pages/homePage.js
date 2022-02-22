42587a690e6295776aba52ce050d4b3d50ea396f
Vasyl Yurkovych <59879559+yurkovychv@users.noreply.github.com>
2022-02-22 17:43:23 +0200

    Update homePage.js

--- tests/pages/homePage.js
+++ tests/pages/homePage.js
@@ -120,16 +120,16 @@ module.exports = {
     if (version > 11) {
       if (this.isAmiUpgrade) {
         for (const milestone of milestones) {
-          I.waitForElement(`//pre[contains(text(), '${milestone}')]`, 1200);
+          I.waitForElement(`//pre[contains(text(), '${milestone}')]`, 1800);
         }
 
-        I.waitForText(locators.successUpgradeMessage, 1200, locators.successUpgradeMsgSelector);
+        I.waitForText(locators.successUpgradeMessage, 1800, locators.successUpgradeMsgSelector);
       }
 
       if (!this.isAmiUpgrade) {
         // to ensure that the logs window is never empty during upgrade
-        I.waitForElement(`//pre[contains(text(), '${milestones[0]}')]`, 1200);
-        I.waitForText(locators.successUpgradeMessage, 1200, locators.successUpgradeMsgSelector);
+        I.waitForElement(`//pre[contains(text(), '${milestones[0]}')]`, 1800);
+        I.waitForText(locators.successUpgradeMessage, 1800, locators.successUpgradeMsgSelector);
 
         // Get upgrade logs from a container
         const upgradeLogs = await I.verifyCommand('docker exec pmm-server cat /srv/logs/pmm-update-perform.log');
@@ -141,7 +141,7 @@ module.exports = {
 
       I.click(locators.reloadButtonAfterUpgrade);
     } else {
-      I.waitForText(locators.successUpgradeMessage, 1200, locators.successUpgradeMsgSelector);
+      I.waitForText(locators.successUpgradeMessage, 1800, locators.successUpgradeMsgSelector);
       // we have a bug we need this https://jira.percona.com/browse/PMM-9294
       I.wait(60);
 
