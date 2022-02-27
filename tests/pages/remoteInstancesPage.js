26b213d83fd0c3d5e4b14641b6e6dc1b67e43574
puneet0191 <puneet.0191@gmail.com>
2022-02-27 11:29:44 +0530

    PMM-9210 fixing the object

--- tests/pages/remoteInstancesPage.js
+++ tests/pages/remoteInstancesPage.js
@@ -153,7 +153,7 @@ module.exports = {
   },
 
   async getFileContent(filePath) {
-    const fileContent = await I.verifyCommand(`cat ${filePath}`);
+    const fileContent = await I.verifyCommand(`cat ${filePath} | head -n -1`);
 
     return fileContent;
   },
@@ -246,7 +246,7 @@ module.exports = {
       await this.fillFileContent(this.fields.tlscaInput, details.tlsCAFile);
       await this.fillFileContent(this.fields.tlsCertificateInput, details.tlsCertFile);
       await this.fillFileContent(this.fields.tlsCertificateKeyInput, details.tlsKeyFile);
-      I.click(this.fields.usePgStatStatements);
+      if (details.serviceType === 'postgres_ssl') I.click(this.fields.usePgStatStatements);
     }
 
     if (details.serviceType === 'mongodb_ssl') {
