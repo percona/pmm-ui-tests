015bf62d4b1c38024240a152fba536e71ba8b55d
Vasyl Yurkovych <59879559+yurkovychv@users.noreply.github.com>
2022-04-05 19:37:31 +0300

    PMM-7 new connect form (#242)

--- linksHelper.js
+++ linksHelper.js
@@ -7,4 +7,5 @@ module.exports = {
   privacyPolicy: 'https://per.co.na/pmm/platform-privacy',
   forgotPassword: 'https://okta.percona.com/signin/forgot-password',
   imageRendererPlugin: 'https://www.percona.com/doc/percona-monitoring-and-management/2.x/how-to/render-dashboard-images.html',
+  portalProfile: 'https://platform-dev.percona.com/profile',
 };
