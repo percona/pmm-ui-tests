507b6617ad11f395480b3ba39822ffcbb8088ab9
Peter <sirotnak.p@gmail.com>
2022-04-12 10:24:44 +0200

    Added flag to UI upgrade test

--- tests/upgradePMM_test.js
+++ tests/upgradePMM_test.js
@@ -428,7 +428,7 @@ Scenario(
 );
 
 Scenario(
-  'PMM-T3 Verify user is able to Upgrade PMM version [blocker] @pmm-upgrade @ami-upgrade  ',
+  'PMM-T3 Verify user is able to Upgrade PMM version [blocker] @pmm-upgrade @ami-upgrade @pmm-portal-upgrade',
   async ({ I, homePage }) => {
     I.amOnPage(homePage.url);
     await homePage.upgradePMM(versionMinor);
