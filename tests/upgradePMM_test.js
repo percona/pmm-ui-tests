ca2fc410344bc59e3b5446b4e4a527090a3ff856
yurkovychv <vasyl.yurkovych@percona.com>
2022-04-13 10:57:32 +0300

    fix stt tests

--- tests/upgradePMM_test.js
+++ tests/upgradePMM_test.js
@@ -24,6 +24,7 @@ const clientDbServices = new DataTable(['serviceType', 'name', 'metric', 'annota
 clientDbServices.add(['MYSQL_SERVICE', 'ps_', 'mysql_global_status_max_used_connections', 'annotation-for-mysql', dashboardPage.mysqlInstanceSummaryDashboard.url, 'mysql_upgrade']);
 clientDbServices.add(['POSTGRESQL_SERVICE', 'PGSQL_', 'pg_stat_database_xact_rollback', 'annotation-for-postgres', dashboardPage.postgresqlInstanceSummaryDashboard.url, 'pgsql_upgrade']);
 // temp skip for mongo
+// eslint-disable-next-line max-len
 // clientDbServices.add(['MONGODB_SERVICE', 'mongodb_', 'mongodb_connections', 'annotation-for-mongo', dashboardPage.mongoDbInstanceSummaryDashboard.url, 'mongo_upgrade']);
 
 const connection = perconaServerDB.defaultConnection;
