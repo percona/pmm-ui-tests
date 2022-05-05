884b57f89db2627c84d5d037489fbc15402fd0d0
yurkovychv <vasyl.yurkovych@percona.com>
2022-05-05 13:14:25 +0300

    move advisors on by default test

--- tests/verifyPMMSettingsPageFunctionality_test.js
+++ tests/verifyPMMSettingsPageFunctionality_test.js
@@ -383,10 +383,10 @@ Scenario(
 );
 
 Scenario(
-  'PMM-T254 ensure Advisors are on by default @settings',
+  'PMM-T254 ensure Advisors are on by default @instances',
   async ({ settingsAPI, I }) => {
     const resp = await settingsAPI.getSettings('stt_enabled');
 
-    assert.ok(resp, `Advisors should be turned on bydefault from 2.28.0 release but found ${resp}`);
+    assert.ok(resp, `Advisors should be turned on by default from 2.28.0 release but found ${resp}`);
   },
 );
