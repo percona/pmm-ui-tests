5fbcb755a5ae30001f9fe409aa681d5af1ad81d3
Beata Handzelova <beata.handzelova@office365.3pillarglobal.com>
2022-05-10 12:04:26 +0200

    remove REST timeout

--- pr.codecept.js
+++ pr.codecept.js
@@ -54,7 +54,6 @@ exports.config = {
     },
     REST: {
       endpoint: process.env.PMM_UI_URL || 'http://127.0.0.1/',
-      timeout: 60000,
     },
     Mailosaur: {
       require: 'codeceptjs-mailosaurhelper',
