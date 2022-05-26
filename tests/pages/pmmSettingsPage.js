baf3e150d05fe6d433d9d3e0ae2f5b66e1bd3d60
Vladimir Nazarov <vladimir.nazarov@simbirsoft.com>
2022-05-26 23:27:06 +0300

    Fixed readability issues. Added dedicated helper for tooltips. Moved linksHelper.js to helper folder. Improved readability of assert messages

--- tests/pages/pmmSettingsPage.js
+++ tests/pages/pmmSettingsPage.js
@@ -2,7 +2,7 @@ const assert = require('assert');
 const { communicationData, emailDefaults } = require('./testData');
 
 const {
-  I, adminPage, links, perconaPlatformPage,
+  I, adminPage, tooltips, perconaPlatformPage,
 } = inject();
 
 const locateLabel = (selector) => locate(I.useDataQA(selector)).find('span');
@@ -91,139 +91,7 @@ module.exports = {
   sectionButtonText: {
     applyChanges: 'Apply changes', applySSHKey: 'Apply SSH key', applyAlertmanager: 'Apply Alertmanager settings',
   },
-  tooltips: {
-    diagnostics: {
-      iconLocator: locate('$diagnostics-label').find('div[class$="-Icon"]'),
-      text: 'You can download server logs to make the problem detection simpler. Please include this file if you are submitting a bug report.',
-      link: false,
-    },
-    metricsResolution: {
-      metricsResolutionSec: {
-        iconLocator: locate('$metrics-resolution-label').find('div[class$="-Icon"]'),
-        text: 'This setting defines how frequently the data will be collected.',
-        link: links.metricsResolutionDocs,
-      },
-    },
-    advancedSettings: {
-      dataRetention: {
-        iconLocator: locate('$advanced-label').find('div[class$="-Icon"]'),
-        text: 'This is the value for how long data will be stored.',
-        link: links.dataRetentionDocs,
-      },
-      telemetry: {
-        iconLocator: locate('$advanced-telemetry').find('div[class$="-Icon"]'),
-        text: 'Option to send usage data back to Percona to let us make our product better.',
-        link: links.telemetryDocs,
-      },
-      checkForUpdates: {
-        iconLocator: locate('$advanced-updates').find('div[class$="-Icon"]'),
-        text: 'Option to check new versions and ability to update PMM from UI.',
-        link: links.checkForUpdates,
-      },
-      stt: {
-        iconLocator: locate('$advanced-advisors').find('div[class$="-Icon"]'),
-        text: 'Enable Advisor Checks and get updated checks from Percona.',
-        link: links.advisorsDocs,
-      },
-      publicAddress: {
-        iconLocator: locate('$public-address-label').find('div[class$="-Icon"]'),
-        text: 'Public Address to this PMM server.',
-        link: false,
-      },
-      executionIntervals: {
-        iconLocator: locate('$check-intervals-label').find('div[class$="-Icon"]'),
-        text: 'Interval between check runs',
-        link: false,
-      },
-      dbaas: {
-        iconLocator: locate('$advanced-dbaas').find('div[class$="-Icon"]'),
-        text: 'Option to enable/disable DBaaS features. Disabling DBaaS does not suspend or remove running clusters.',
-        link: links.dbaasDocs,
-      },
-      backupManagement: {
-        iconLocator: locate('$advanced-backup').find('div[class$="-Icon"]'),
-        text: 'Option to enable/disable Backup Management features.',
-        link: links.backupManagementDocs,
-      },
-      integratedAlerting: {
-        iconLocator: locate('$advanced-alerting').find('div[class$="-Icon"]'),
-        text: 'Option to enable/disable Integrated Alerting features.',
-        link: links.integratedAlertingDocs,
-      },
-      microsoftAzureMonitoring: {
-        iconLocator: locate('$advanced-azure-discover').find('div[class$="-Icon"]'),
-        text: 'Option to enable/disable Microsoft Azure DB instanced discovery and monitoring',
-        link: links.microsoftAzureMonitoringDocs,
-      },
-    },
-    ssh: {
-      sshKey: {
-        iconLocator: locate('$ssh-key-label').find('div[class$="-Icon"]'),
-        text: 'Public SSH key to let you login into the server using SSH.',
-        link: links.sshKeyDocs,
-      },
-    },
-    alertManagerIntegration: {
-      alertManagerUrl: {
-        iconLocator: locate('$alertmanager-url-label').find('div[class$="-Icon"]'),
-        text: 'The URL of the external Alertmanager to use.',
-        link: links.prometheusAlertManagerDocs,
-      },
-      prometheusAlertingRules: {
-        iconLocator: locate('$alertmanager-rules-label').find('div[class$="-Icon"]'),
-        text: 'Alerting rules in the YAML configuration format.',
-        link: links.prometheusAlertManagerDocs,
-      },
-    },
-    perconaPlatform: {},
-    communication: {
-      email: {
-        serverAddress: {
-          iconLocator: locate('div').after(locate('span').withText('Server Address')),
-          text: 'The default SMTP smarthost used for sending emails, including port number (e.g. smtp.example.org:587)',
-          link: links.communicationDocs,
-        },
-        hello: {
-          iconLocator: locate('div').after(locate('span').withText('Hello')),
-          text: 'The hostname to identify the SMTP server',
-          link: links.communicationDocs,
-        },
-        from: {
-          iconLocator: locate('div').after(locate('span').withText('From')),
-          text: 'The sender address',
-          link: links.communicationDocs,
-        },
-        authType: {
-          iconLocator: locate('div').after(locate('span').withText('Auth Type')),
-          text: 'Authentication type',
-          link: links.communicationDocs,
-        },
-        username: {
-          iconLocator: locate('div').after(locate('span').withText('Username')),
-          text: 'SMTP authentication information',
-          link: links.communicationDocs,
-        },
-        password: {
-          iconLocator: locate('div').after(locate('span').withText('Password')),
-          text: 'SMTP authentication information',
-          link: links.communicationDocs,
-        },
-        testEmail: {
-          iconLocator: locate('$testEmail-field-container').find('div[class$="-Icon"]'),
-          text: 'Send a test email to this address',
-          link: false,
-        },
-      },
-      slack: {
-        slackUrl: {
-          tabButton: 'li > a[aria-label="Tab Slack"]',
-          iconLocator: locate('div').after(locate('span').withText('URL')),
-          text: 'Slack incoming webhook URL',
-          link: links.communicationDocs,
-        },
-      },
-    },
-  },
+  tooltips : tooltips.pmmSettings,
   communicationData,
   emailDefaults,
   communication: {
@@ -578,14 +446,14 @@ module.exports = {
     I.waitForVisible(tooltipObj.iconLocator, 5);
     I.moveCursorTo(tooltipObj.iconLocator);
     I.waitForVisible(this.fields.tooltipText, 5);
-    I.seeTextEquals(tooltipObj.text, this.fields.tooltipText);
+    I.seeTextEquals(tooltipObj.text, this.fields.tooltipText.as('Tooltip text'));
     /* there are tooltip without "Read more" link */
     if (tooltipObj.link) {
-      I.seeAttributesOnElements(this.fields.tooltipReadMoreLink, { href: tooltipObj.link });
-      const moreInfoUrl = (await I.grabAttributeFrom(this.fields.tooltipReadMoreLink, 'href'));
-      const response = await I.sendGetRequest(moreInfoUrl);
+      I.seeAttributesOnElements(this.fields.tooltipReadMoreLink.as('Tooltip "Read more" link for ' + tooltipObj.iconLocator), { href: tooltipObj.link });
+      const readMoreLink = (await I.grabAttributeFrom(this.fields.tooltipReadMoreLink, 'href'));
+      const response = await I.sendGetRequest(readMoreLink);
 
-      assert.equal(response.status, 200);
+      assert.equal(response.status, 200, 'Read more link should lead to working documentation page. But the GET request response status is not 200');
     }
   },
 
