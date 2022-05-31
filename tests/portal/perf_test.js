7096fcd2ef0bf879596cbf90ad8b362c2370e1b2
Peter <sirotnak.p@gmail.com>
2022-05-31 11:11:24 +0200

    refactoring

--- tests/portal/perf_test.js
+++ tests/portal/perf_test.js
@@ -2,18 +2,16 @@ Feature('Portal Integration with PMM');
 
 Scenario(
   'PMM-T398 PMM-T809 Verify Connect to Percona Portal elements @portal @pre-pmm-portal-upgrade',
-  async ({ I }) => {
+  async ({
+    I, homePage, pmmInventoryPage, pmmSettingsPage, allChecksPage,
+  }) => {
     await I.Authorize();
-    const newTabs = await I.openNewTabs(10);
+    const newTabs = await I.openNewTabs(4);
+    const addresses = [homePage.landingUrl, pmmInventoryPage.url, pmmSettingsPage.url, allChecksPage.url];
 
-    for (const tab of newTabs) {
-      I.navigateTabTo(tab, '').then(async () => I.say(`Page load time was ${await I.getPageTimeLoad(tab)}ms`));
+    for (const [i, tab] of newTabs.entries()) {
+      await I.navigateTabTo(tab, addresses[i]);
+      await I.say(`Page load time was ${await I.getPageTimeLoad(tab)}ms`);
     }
-
-    for (const tab of newTabs) {
-      I.getPageTimeToInteractive('http://192.168.100.13');
-    }
-
-    I.say('New Window Opened');
   },
 );
