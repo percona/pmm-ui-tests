6dc8aad9444cf27a05c485a18d56883d327b596b
Peter <sirotnak.p@gmail.com>
2022-06-07 20:30:52 +0200

    log admin email

--- tests/portal/snUserCreation_test.js
+++ tests/portal/snUserCreation_test.js
@@ -10,9 +10,14 @@ Scenario(
   }) => {
     const portalCredentials = await portalAPI.createServiceNowUsers();
 
+    I.say(`Admin email is: ${portalCredentials.admin1.email}`);
     I.say(await exec(`SERVICE_NOW_ADMIN_USERNAME=${portalCredentials.admin1.email}`));
     I.say(JSON.stringify(await exec('echo $SERVICE_NOW_ADMIN_USERNAME')));
     I.say(await exec(`SERVICE_NOW_ADMIN_PASSWORD=${portalCredentials.admin1.password}`));
+    I.say(JSON.stringify(await exec('echo $SERVICE_NOW_ADMIN_PASSWORD')));
+    I.say(await exec(`SERVICE_NOW_TECHNICAL_USERNAME=${portalCredentials.technical.email}`));
+    I.say(JSON.stringify(await exec('echo $SERVICE_NOW_ADMIN_USERNAME')));
+    I.say(await exec(`SERVICE_NOW_TECHNICAL_PASSWORD=${portalCredentials.technical.password}`));
     I.say(JSON.stringify(await exec('echo $SERVICE_NOW_ADMIN_PASSWORD')));
     await I.wait(5);
   },
