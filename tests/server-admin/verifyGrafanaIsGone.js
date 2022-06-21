5c60be26e815dec01b92d2d5a2c00286e28e531e
Vojta Koval <vojtech.koval@sde.cz>
2022-06-21 18:34:43 +0200

    ADD test file for grafana enterprise gone.

---
+++ tests/server-admin/verifyGrafanaIsGone.js
@@ -0,0 +1,19 @@
+const assert = require('assert');
+Before(async ({
+    I
+  }) => {
+    await I.Authorize();
+  });
+Scenario(
+    'PMM-10162 Verify that Grafana Enterprise is not present @admin',
+    async ({
+      I, stastsAndLicense,
+    }) => {
+      I.amOnPage(stastsAndLicense.url);
+      I.seeElement(stastsAndLicense.buttons.contactUs);
+      //I.waitForVisible(pmmSettingsPage.fields.rareIntervalInput, 30);
+  
+      // // Verify that you can see element
+      //I.seeElementsEnabled(pmmSettingsPage.fields.rareIntervalInput);
+    },
+  );
