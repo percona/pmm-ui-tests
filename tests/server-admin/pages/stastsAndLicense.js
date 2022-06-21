8b11700d061bcf587025d91c828582e99fc3b15b
Vojta Koval <vojtech.koval@sde.cz>
2022-06-21 18:34:25 +0200

    Add final version of PO.

--- tests/server-admin/pages/stastsAndLicense.js
+++ tests/server-admin/pages/stastsAndLicense.js
@@ -1,23 +1,52 @@
 const { I } = inject();
 module.exports = {
     url: 'graph/admin/upgrading',
+    headLine: 'Instance statistics',
+    fields: {
+        enterpriseLicense: locate('h2').withText('Enterprise license'),
+        grafanaEnterpriseLogo: locate('h2').withText('Grafana Enterprise'),
+        freeTrial: locate('h3').withText('Get your free trial'),
+        dataSourcePermission: locate('div').withText('Data source permissions'),
+        reporting: locate('div').withText('Reporting'),
+        saml: locate('div').withText('SAML authentication'),
+        teamSync: locate('div').withText('Team Sync'),
+        whiteLabeling: locate('div').withText('White labeling'),
+        auditing: locate('div').withText('Auditing'),
+        grafanaUsageInsights: locate('div').withText('Grafana usage insights'),
+        sortDashByPop: locate('div').withText('Sort dashboards by popularity in search'),
+        findUnusedDash: locate('div').withText('Find unused dashboards'),
+        dashboardUsageStatsDrawer: locate('div').withText('Dashboard usage stats drawer'),
+        enterprisePlugins: locate('div').withText('Enterprise plugins'),
+        oracle: locate('div').withText('Oracle'),
+        splunk: locate('div').withText('Splunk'),
+        serviceNow: locate('div').withText('Service Now'),
+        dynaTrace: locate('div').withText('Dynatrace'),
+        newRelic: locate('div').withText('New Relic'),
+        dataDog: locate('div').withText('DataDog'),
+        appDynamics: locate('div').withText('AppDynamics'),
+        sapHana: locate('div').withText('SAP HANA®'),
+        honeycomb: locate('div').withText('Honeycomb'),
+        jira: locate('div').withText('Jira'),
+        mongoDb: locate('div').withText('MongoDB'),
+        salesForce: locate('div').withText('Salesforce'),
+        snowflake: locate('div').withText('Snowflake'),
+        waveFront: locate('div').withText('Wavefront'),
+        atYourService: locate('h4').withText('At your service'),
+        Enterpriseplugins: locate('h4').withText('Enterprise Plugins'),
+        criticalSla: locate('div').withText('At your service'),
+        unlimitedExpert: locate('div').withText('Unlimited Expert Support'),
+        email: locate('div').withText('Email'),
+        slack: locate('div').withText('Private Slack channel'),
+        phone: locate('div').withText('At your Phone'),
+        handInHand: locate('div').withText('Hand-in-hand support'),
+
+        
+        
+    },
     buttons: {
         contactUs: locate('span').withText('Contact us and get a free trial'),
-      // restoreByName returns Restore button locator for a given Artifact name
-      restoreByName: (name) => locate('$restore-backup-artifact-button').inside(artifactCell(name)),
-      deleteByName: (name) => locate('$delete-backup-artifact-button').inside(artifactCell(name)),
-      showDetails: (name) => locate('$show-details').inside(artifactCell(name)),
-      hideDetails: (name) => locate('$hide-details').inside(artifactCell(name)),
-      addBackup: '$backup-add-button',
-      modalRestore: '$restore-button',
-      forceDeleteCheckbox: '$force-checkbox-input',
-      confirmDelete: '$confirm-delete-modal-button',
-      retryModeOption: (option) => locate('$retry-mode-selector').find('div').at(1).find('label')
-        .withText(option),
-      dataModel: '$dataModel-radio-button',
     },
     openStatsAndLicensePage() {
       I.amOnPage(this.url);
-      I.waitForText('Add', 30, this.buttons.openAddBackupModal);
     },
   };
