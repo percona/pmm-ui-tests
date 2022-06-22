57d74ef46704378b8ec4460d16aec67fe8819de9
Vojta Koval <vojtech.koval@sde.cz>
2022-06-22 12:21:23 +0200

    Add forLoop 4 fields.

--- tests/server-admin/verifyGrafanaIsGone_test.js
+++ tests/server-admin/verifyGrafanaIsGone_test.js
@@ -1,4 +1,5 @@
 const assert = require('assert');
+const { fields } = require('./pages/stastsAndLicensePage');
 Feature('Grafana-Enterprise');
 Before(async ({
     I
@@ -12,9 +13,13 @@ Scenario(
     }) => {
       I.amOnPage(stastsAndLicensePage.url);
       I.seeElement(stastsAndLicensePage.buttons.contactUs);
-      //I.waitForVisible(pmmSettingsPage.fields.rareIntervalInput, 30);
-  
-      // // Verify that you can see element
-      //I.seeElementsEnabled(pmmSettingsPage.fields.rareIntervalInput);
+      I.seeElement(stastsAndLicensePage.fields.enterpriseLicense);
+      I.seeElement(stastsAndLicensePage.fields.dataSourcePermission);
+      for (const key in fields) {
+        if (fields.hasOwnProperty(key)) {
+            I.waitForVisible(fields[key]);
+            //I.seeElement(fields[key]);
+        }
+    }
     },
   );
