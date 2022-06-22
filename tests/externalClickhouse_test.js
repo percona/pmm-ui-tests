29469ce702c24e1a9e87869301612abab6def060
Peter <sirotnak.p@gmail.com>
2022-06-22 14:04:45 +0200

    test for second PMM trough docker compose

---
+++ tests/externalClickhouse_test.js
@@ -0,0 +1,10 @@
+Feature('External Clickhouse Tests');
+
+Scenario(
+  'PMM-9550 Verify downloading server diagnostics logs @externalClickhouse',
+  async ({ I, homePage }) => {
+    await I.verifyCommand('docker-compose up -d -f docker-compose-clickhouse.yml');
+    await I.wait(180);
+    I.say(await I.verifyCommand('docker ps -a'));
+  },
+);
