3397587678bfd6fe4e7bfee02dace4d79edb594f
Vojta Koval <vojtech.koval@sde.cz>
2022-06-23 10:18:41 +0200

    Fix Formatting

--- tests/server-admin/verifyGrafanaIsGone_test.js
+++ tests/server-admin/verifyGrafanaIsGone_test.js
@@ -1,18 +1,18 @@
 const { fields } = require('./pages/stastsAndLicensePage');
+
 Feature('Grafana-EnterpriseAds');
 Before(async ({
-    I
-  }) => {
-    await I.Authorize();
-  });
+  I,
+}) => {
+  await I.Authorize();
+});
 Scenario(
-    'PMM-10162 Verify that Grafana Enterprise is not present @admin',
-    async ({
-      I, stastsAndLicensePage,
-    }) => {
-      I.amOnPage(stastsAndLicensePage.url);
-       stastsAndLicensePage.waitForStatsAndLicensePageLoaded();
-      Object.values(fields).forEach(val =>I.dontSeeElement(val));
-      
-    },
-  );
+  'PMM-10162 Verify that Grafana Enterprise is not present @admin',
+  async ({
+    I, stastsAndLicensePage,
+  }) => {
+    I.amOnPage(stastsAndLicensePage.url);
+    stastsAndLicensePage.waitForStatsAndLicensePageLoaded();
+    Object.values(fields).forEach((val) => I.dontSeeElement(val));
+  },
+);
