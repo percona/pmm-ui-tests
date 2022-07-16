40ff7ee984d32d3f03227905dd19f5d2480e083e
Peter <sirotnak.p@gmail.com>
2022-07-16 13:16:16 +0200

    TC 1244

--- tests/verifySrvDataDirectoryBootstrap_test.js
+++ tests/verifySrvDataDirectoryBootstrap_test.js
@@ -4,23 +4,7 @@ const moment = require('moment');
 Feature('Test PMM server with srv local folder');
 
 const basePmmUrl = 'http://127.0.0.1:8080/';
-const testCaseName = 'Initialized value';
-
-const testCase = new DataTable(['number']);
-
-testCase.add(['PMM-T1243']);
-testCase.add(['PMM-T1244']);
-
-After(async ({ I, current }) => {
-  I.say(testCaseName);
-  if (testCaseName === 'PMM-T1244') {
-    I.say(testCaseName);
-    await stopAndRemoveContainerWithDataContainer(I);
-  } else if (testCaseName === 'PMM-T1243') {
-    I.say(testCaseName);
-    await stopAndRemoveContainerWithoutDataContainer(I);
-  }
-});
+let testCaseName = '';
 
 const runContainerWithoutDataContainer = async (I) => {
   await I.verifyCommand('docker run -v $HOME/srv:/srv -d --restart always --publish 8080:80 --publish 8443:443 --name pmm-server-srv perconalab/pmm-server:2.29.0-rc');
@@ -43,17 +27,23 @@ const stopAndRemoveContainerWithDataContainer = async (I) => {
   await I.verifyCommand('docker rm pmm-server-test');
 };
 
-Data(testCase).Scenario(
-  'PMM-T1243 Verify PMM Server without data container @srv3',
-  async ({
-    I, adminPage, qanPage, dashboardPage, current,
-  }) => {
-    const { number } = current;
-    const testCaseName = number;
+After(async ({ I }) => {
+  if (testCaseName === 'PMM-T1243') {
+    await stopAndRemoveContainerWithoutDataContainer(I);
+  } else if (testCaseName === 'PMM-T1244') {
+    await stopAndRemoveContainerWithDataContainer(I);
+  }
+});
 
+Scenario(
+  'PMM-T1243 Verify PMM Server without data container @srv',
+  async ({
+    I, adminPage, qanPage, dashboardPage,
+  }) => {
     await runContainerWithoutDataContainer(I);
+    await I.Authorize();
     await I.wait(90);
-    await I.Authorize('admin', 'new-flag-password');
+    testCaseName = 'PMM-T1243';
     await I.amOnPage(basePmmUrl + qanPage.url);
     I.dontSeeElement(qanPage.elements.noQueryAvailable);
     await I.waitForVisible(qanPage.elements.qanRow);
@@ -81,32 +71,30 @@ Data(testCase).Scenario(
     await dashboardPage.verifyThereAreNoGraphsWithNA();
     await dashboardPage.verifyThereAreNoGraphsWithoutData();
     I.say(await I.verifyCommand('docker logs pmm-server-srv'));
-    await stopAndRemoveContainerWithoutDataContainer(I);
   },
 );
 
 Scenario(
-  'PMM-T1243 Verify PMM Server without data container @srv2',
+  'PMM-T1244 Verify PMM Server with empty data container @srv',
   async ({
     I, adminPage, qanPage, dashboardPage,
   }) => {
     await runContainerWithDataContainer(I);
+    await I.Authorize();
     await I.wait(90);
-    await I.Authorize('admin', 'new-flag-password');
+    testCaseName = 'PMM-T1244';
     await I.amOnPage(basePmmUrl + qanPage.url);
-    /*
     I.dontSeeElement(qanPage.elements.noQueryAvailable);
     await I.waitForVisible(qanPage.elements.qanRow);
     const qanRows = await I.grabNumberOfVisibleElements(qanPage.elements.qanRow);
 
     assert.ok(qanRows > 0, 'Query Analytics are empty');
-
     await I.amOnPage(basePmmUrl + dashboardPage.nodeSummaryDashboard.url);
     await dashboardPage.verifyThereAreNoGraphsWithNA();
     await dashboardPage.verifyThereAreNoGraphsWithoutData();
 
-    await stopAndRemoveContainerWithoutDataContainer(I);
-    await runContainerWithoutDataContainer(I);
+    await stopAndRemoveContainerWithDataContainer(I);
+    await runContainerWithDataContainer(I);
     await I.wait(60);
     await I.amOnPage(basePmmUrl + qanPage.url);
     adminPage.setAbsoluteTimeRange(moment().subtract({ hours: 12 }).format('YYYY-MM-DD HH:mm:00'), moment().subtract({ minutes: 2 }).format('YYYY-MM-DD HH:mm:00'));
@@ -121,7 +109,5 @@ Scenario(
     await dashboardPage.verifyThereAreNoGraphsWithNA();
     await dashboardPage.verifyThereAreNoGraphsWithoutData();
     I.say(await I.verifyCommand('docker logs pmm-server-srv'));
-
-    */
   },
 );
