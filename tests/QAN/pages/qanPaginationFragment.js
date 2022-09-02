f5212b6442ac98b86ce8a1004e991704cf1b709f
yurkovychv <vasyl.yurkovych@percona.com>
2022-09-02 17:36:08 +0300

    PMM-10622 QAN and dashboard fixes

--- tests/QAN/pages/qanPaginationFragment.js
+++ tests/QAN/pages/qanPaginationFragment.js
@@ -18,7 +18,7 @@ module.exports = {
 
   getPageLocator: (number) => `//li[@class='ant-pagination-item ant-pagination-item-${number}']`,
 
-  getPerPageOptionLocator: (option) => `//li[contains(@class, 'ant-select-dropdown-menu-item') and contains(text(), '${option}' )]`,
+  getPerPageOptionLocator: (option) => `//div[contains(@class, 'ant-select-item-option-content') and contains(text(), '${option}' )]`,
 
   async getLastPageNumber() {
     return await I.grabAttributeFrom(this.buttons.lastPageNumber, 'title');
