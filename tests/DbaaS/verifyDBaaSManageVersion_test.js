23f7bd24f0c95f750ebd51de97ce203683175707
Beata Handzelova <64002801+beatahandzelova@users.noreply.github.com>
2022-09-15 09:05:49 +0200

    PMM-7 Skip failing PMM-T760 PMM-T762 PMM-T770 (#389)
    * skip failing PMM-T760 PMM-T762 PMM-T770
    
    * lint error

--- tests/DbaaS/verifyDBaaSManageVersion_test.js
+++ tests/DbaaS/verifyDBaaSManageVersion_test.js
@@ -109,105 +109,106 @@ Scenario('PMM-T765 Verify Manage Components Versions '
     I.click(dbaasManageVersionPage.manageVersion.operator);
   });
 
-Data(versionVerification).Scenario('PMM-T760 PMM-T762 PMM-T770 Saving Custom Version for Dbaas Operators @dbaas',
-  async ({
-    I, dbaasPage, dbaasActionsPage, dbaasManageVersionPage, current,
-  }) => {
-    const {
-      component, operatorVersion, componentName, dbType,
-    } = current;
+// FIXME: Skip until https://jira.percona.com/browse/PMM-10683 is fixed
+// Data(versionVerification).Scenario('PMM-T760 PMM-T762 PMM-T770 Saving Custom Version for Dbaas Operators @dbaas',
+//   async ({
+//     I, dbaasPage, dbaasActionsPage, dbaasManageVersionPage, current,
+//   }) => {
+//     const {
+//       component, operatorVersion, componentName, dbType,
+//     } = current;
 
-    I.amOnPage(dbaasPage.url);
-    dbaasPage.checkCluster(clusterName, false);
-    dbaasManageVersionPage.waitForManageVersionPopup(clusterName);
-    await dbaasManageVersionPage.selectOperatorVersion(operatorVersion);
-    await dbaasManageVersionPage.selectComponent(componentName);
-    // To uncheck all supported versions.
-    await dbaasManageVersionPage.selectAllSupportedVersions(component);
+//     I.amOnPage(dbaasPage.url);
+//     dbaasPage.checkCluster(clusterName, false);
+//     dbaasManageVersionPage.waitForManageVersionPopup(clusterName);
+//     await dbaasManageVersionPage.selectOperatorVersion(operatorVersion);
+//     await dbaasManageVersionPage.selectComponent(componentName);
+//     // To uncheck all supported versions.
+//     await dbaasManageVersionPage.selectAllSupportedVersions(component);
 
-    // Error Message for Required Field
-    I.see(
-      dbaasManageVersionPage.manageVersion.errorMessageRequiredField,
-      dbaasManageVersionPage.manageVersion.versionSelectorFieldErrorMessage(component),
-    );
-    I.see(
-      dbaasManageVersionPage.manageVersion.errorMessageRequiredField,
-      dbaasManageVersionPage.manageVersion.defaultVersionSelectorFieldErrorMessage,
-    );
-    I.seeAttributesOnElements(dbaasManageVersionPage.manageVersion.saveButton, { disabled: true });
+//     // Error Message for Required Field
+//     I.see(
+//       dbaasManageVersionPage.manageVersion.errorMessageRequiredField,
+//       dbaasManageVersionPage.manageVersion.versionSelectorFieldErrorMessage(component),
+//     );
+//     I.see(
+//       dbaasManageVersionPage.manageVersion.errorMessageRequiredField,
+//       dbaasManageVersionPage.manageVersion.defaultVersionSelectorFieldErrorMessage,
+//     );
+//     I.seeAttributesOnElements(dbaasManageVersionPage.manageVersion.saveButton, { disabled: true });
 
-    const getRecommendedVersions = await dbaasManageVersionPage.getAllRecommendedVersions(component);
+//     const getRecommendedVersions = await dbaasManageVersionPage.getAllRecommendedVersions(component);
 
-    // Select Recommended Versions
-    await dbaasManageVersionPage.setVersions(component, getRecommendedVersions);
+//     // Select Recommended Versions
+//     await dbaasManageVersionPage.setVersions(component, getRecommendedVersions);
 
-    // Error Message on Version Selector Should be gone
-    I.dontSee(
-      dbaasManageVersionPage.manageVersion.errorMessageRequiredField,
-      dbaasManageVersionPage.manageVersion.versionSelectorFieldErrorMessage(component),
-    );
+//     // Error Message on Version Selector Should be gone
+//     I.dontSee(
+//       dbaasManageVersionPage.manageVersion.errorMessageRequiredField,
+//       dbaasManageVersionPage.manageVersion.versionSelectorFieldErrorMessage(component),
+//     );
 
-    // Save button should still be disabled.
-    I.see(
-      dbaasManageVersionPage.manageVersion.errorMessageRequiredField,
-      dbaasManageVersionPage.manageVersion.defaultVersionSelectorFieldErrorMessage,
-    );
-    I.seeAttributesOnElements(dbaasManageVersionPage.manageVersion.saveButton, { disabled: true });
+//     // Save button should still be disabled.
+//     I.see(
+//       dbaasManageVersionPage.manageVersion.errorMessageRequiredField,
+//       dbaasManageVersionPage.manageVersion.defaultVersionSelectorFieldErrorMessage,
+//     );
+//     I.seeAttributesOnElements(dbaasManageVersionPage.manageVersion.saveButton, { disabled: true });
 
-    I.click(dbaasManageVersionPage.manageVersion.defaultVersionSelector);
-    getRecommendedVersions.forEach((version) => {
-      I.seeElement(dbaasManageVersionPage.manageVersion.defaultVersionOption(version));
-    });
-    const getAllSupportedVersion = await dbaasManageVersionPage.getAllVersionsSupported(component);
-    const unselectedVersions = getAllSupportedVersion.filter(
-      (version) => !getRecommendedVersions.includes(version),
-    );
+//     I.click(dbaasManageVersionPage.manageVersion.defaultVersionSelector);
+//     getRecommendedVersions.forEach((version) => {
+//       I.seeElement(dbaasManageVersionPage.manageVersion.defaultVersionOption(version));
+//     });
+//     const getAllSupportedVersion = await dbaasManageVersionPage.getAllVersionsSupported(component);
+//     const unselectedVersions = getAllSupportedVersion.filter(
+//       (version) => !getRecommendedVersions.includes(version),
+//     );
 
-    unselectedVersions.forEach((version) => {
-      I.dontSeeElement(dbaasManageVersionPage.manageVersion.defaultVersionOption(version));
-    });
-    I.click(dbaasManageVersionPage.manageVersion.defaultVersionSelector);
+//     unselectedVersions.forEach((version) => {
+//       I.dontSeeElement(dbaasManageVersionPage.manageVersion.defaultVersionOption(version));
+//     });
+//     I.click(dbaasManageVersionPage.manageVersion.defaultVersionSelector);
 
-    const defaultRecommendedVersion = await dbaasManageVersionPage.getRecommendedVersion(component);
+//     const defaultRecommendedVersion = await dbaasManageVersionPage.getRecommendedVersion(component);
 
-    await dbaasManageVersionPage.selectDefaultVersion(
-      defaultRecommendedVersion,
-    );
+//     await dbaasManageVersionPage.selectDefaultVersion(
+//       defaultRecommendedVersion,
+//     );
 
-    // Save button should be enabled now and error messages gone.
-    I.dontSee(
-      dbaasManageVersionPage.manageVersion.errorMessageRequiredField,
-      dbaasManageVersionPage.manageVersion.defaultVersionSelectorFieldErrorMessage,
-    );
-    I.click(dbaasManageVersionPage.manageVersion.saveButton);
-    I.waitForText(dbaasManageVersionPage.manageVersion.changeVersionSuccessMessage, 30);
-    I.refreshPage();
-    dbaasPage.checkCluster(clusterName, false);
-    await dbaasManageVersionPage.waitForManageVersionPopup(clusterName);
-    await dbaasManageVersionPage.selectOperatorVersion(operatorVersion);
-    await dbaasManageVersionPage.selectComponent(componentName);
-    I.click(dbaasManageVersionPage.manageVersion.defaultVersionSelector);
-    getRecommendedVersions.forEach((version) => {
-      I.seeElement(dbaasManageVersionPage.manageVersion.defaultVersionOption(version));
-    });
-    unselectedVersions.forEach((version) => {
-      I.dontSeeElement(dbaasManageVersionPage.manageVersion.defaultVersionOption(version));
-    });
-    if (componentName !== dbaasManageVersionPage.components.HAPROXY.name) {
-      await dbaasPage.waitForDbClusterTab(clusterName);
-      await dbaasActionsPage.createClusterBasicOptions(clusterName, dbClusterName, dbType);
-      I.seeElement(
-        dbaasPage.tabs.dbClusterTab.basicOptions.fields.defaultDbVersionValue(
-          defaultRecommendedVersion,
-        ),
-      );
-      I.click(dbaasPage.tabs.dbClusterTab.basicOptions.fields.dbClusterDatabaseVersionField);
-      getRecommendedVersions.forEach((version) => {
-        I.waitForElement(dbaasPage.tabs.dbClusterTab.basicOptions.fields.dbClusterDatabaseVersion(version));
-        I.seeElement(dbaasPage.tabs.dbClusterTab.basicOptions.fields.dbClusterDatabaseVersion(version));
-      });
-      unselectedVersions.forEach((version) => {
-        I.dontSeeElement(dbaasPage.tabs.dbClusterTab.basicOptions.fields.dbClusterDatabaseVersion(version));
-      });
-    }
-  });
+//     // Save button should be enabled now and error messages gone.
+//     I.dontSee(
+//       dbaasManageVersionPage.manageVersion.errorMessageRequiredField,
+//       dbaasManageVersionPage.manageVersion.defaultVersionSelectorFieldErrorMessage,
+//     );
+//     I.click(dbaasManageVersionPage.manageVersion.saveButton);
+//     I.waitForText(dbaasManageVersionPage.manageVersion.changeVersionSuccessMessage, 30);
+//     I.refreshPage();
+//     dbaasPage.checkCluster(clusterName, false);
+//     await dbaasManageVersionPage.waitForManageVersionPopup(clusterName);
+//     await dbaasManageVersionPage.selectOperatorVersion(operatorVersion);
+//     await dbaasManageVersionPage.selectComponent(componentName);
+//     I.click(dbaasManageVersionPage.manageVersion.defaultVersionSelector);
+//     getRecommendedVersions.forEach((version) => {
+//       I.seeElement(dbaasManageVersionPage.manageVersion.defaultVersionOption(version));
+//     });
+//     unselectedVersions.forEach((version) => {
+//       I.dontSeeElement(dbaasManageVersionPage.manageVersion.defaultVersionOption(version));
+//     });
+//     if (componentName !== dbaasManageVersionPage.components.HAPROXY.name) {
+//       await dbaasPage.waitForDbClusterTab(clusterName);
+//       await dbaasActionsPage.createClusterBasicOptions(clusterName, dbClusterName, dbType);
+//       I.seeElement(
+//         dbaasPage.tabs.dbClusterTab.basicOptions.fields.defaultDbVersionValue(
+//           defaultRecommendedVersion,
+//         ),
+//       );
+//       I.click(dbaasPage.tabs.dbClusterTab.basicOptions.fields.dbClusterDatabaseVersionField);
+//       getRecommendedVersions.forEach((version) => {
+//         I.waitForElement(dbaasPage.tabs.dbClusterTab.basicOptions.fields.dbClusterDatabaseVersion(version));
+//         I.seeElement(dbaasPage.tabs.dbClusterTab.basicOptions.fields.dbClusterDatabaseVersion(version));
+//       });
+//       unselectedVersions.forEach((version) => {
+//         I.dontSeeElement(dbaasPage.tabs.dbClusterTab.basicOptions.fields.dbClusterDatabaseVersion(version));
+//       });
+//     }
+//   });
