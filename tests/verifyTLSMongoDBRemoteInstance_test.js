3daea18437be268e5741f6e4de3f3e01ac915933
puneet0191 <puneet.0191@gmail.com>
2022-09-23 16:02:28 +0530

    PMM-7 SSL coverage unstable tests

--- tests/verifyTLSMongoDBRemoteInstance_test.js
+++ tests/verifyTLSMongoDBRemoteInstance_test.js
@@ -10,11 +10,9 @@ const instances = new DataTable(['serviceName', 'version', 'container', 'service
 
 instances.add(['mongodb_4.4_ssl_service', '4.4', 'mongodb_4.4', 'mongodb_ssl', 'mongodb_connections']);
 instances.add(['mongodb_4.2_ssl_service', '4.2', 'mongodb_4.2', 'mongodb_ssl', 'mongodb_connections']);
-instances.add(['mongodb_4.0_ssl_service', '4.0', 'mongodb_4.0', 'mongodb_ssl', 'mongodb_connections']);
 instances.add(['mongodb_5.0_ssl_service', '5.0', 'mongodb_5.0', 'mongodb_ssl', 'mongodb_connections']);
 
 BeforeSuite(async ({ I, codeceptjsConfig }) => {
-  await I.verifyCommand(`${pmmFrameworkLoader} --mo-version=4.0 --setup-mongodb-ssl --pmm2`);
   await I.verifyCommand(`${pmmFrameworkLoader} --mo-version=4.2 --setup-mongodb-ssl --pmm2`);
   await I.verifyCommand(`${pmmFrameworkLoader} --mo-version=4.4 --setup-mongodb-ssl --pmm2`);
   await I.verifyCommand(`${pmmFrameworkLoader} --mo-version=5.0 --setup-mongodb-ssl --pmm2`);
@@ -23,7 +21,6 @@ BeforeSuite(async ({ I, codeceptjsConfig }) => {
 AfterSuite(async ({ I }) => {
   await I.verifyCommand('docker stop mongodb_4.4 || docker rm mongodb_4.4');
   await I.verifyCommand('docker stop mongodb_4.2 || docker rm mongodb_4.2');
-  await I.verifyCommand('docker stop mongodb_4.0 || docker rm mongodb_4.0');
   await I.verifyCommand('docker stop mongodb_5.0 || docker rm mongodb_5.0');
 });
 
