6ad042fa37a693f54f63a21c03e5a2d1ef08da5d
yurkovychv <vasyl.yurkovych@percona.com>
2022-09-27 15:32:19 +0300

    PMM-7 update install command

--- testdata/backup-management/mongodb/setup-replica-and-pbm-local.sh
+++ testdata/backup-management/mongodb/setup-replica-and-pbm-local.sh
@@ -54,16 +54,16 @@ docker cp setup-replica.js mongors1:/
 docker exec -u 0 mongors1 mongo --port=27027 --authenticationDatabase admin setup-replica.js
 
 # Install PBM 1.8.1
-docker exec -u 0 mongors1 /bin/bash -c "percona-release enable pbm release && yum -y install percona-backup-mongodb=1.8.1-1.focal"
-docker exec -u 0 mongors2 /bin/bash -c "percona-release enable pbm release && yum -y install percona-backup-mongodb=1.8.1-1.focal"
-docker exec -u 0 mongors3 /bin/bash -c "percona-release enable pbm release && yum -y install percona-backup-mongodb=1.8.1-1.focal"
+docker exec -u 0 mongors1 /bin/bash -c "percona-release enable pbm release && yum -y install https://repo.percona.com/pbm/yum/release/8/RPMS/x86_64/percona-backup-mongodb-1.8.1-1.el8.x86_64.rpm"
+docker exec -u 0 mongors2 /bin/bash -c "percona-release enable pbm release && yum -y install https://repo.percona.com/pbm/yum/release/8/RPMS/x86_64/percona-backup-mongodb-1.8.1-1.el8.x86_64.rpm"
+docker exec -u 0 mongors3 /bin/bash -c "percona-release enable pbm release && yum -y install https://repo.percona.com/pbm/yum/release/8/RPMS/x86_64/percona-backup-mongodb-1.8.1-1.el8.x86_64.rpm"
 
 docker exec  -d mongors1 /bin/bash -c 'PBM_MONGODB_URI="mongodb://pbmuser:secretpwd@localhost:27027" pbm-agent'
 docker exec  -d mongors2 /bin/bash -c 'PBM_MONGODB_URI="mongodb://pbmuser:secretpwd@localhost:27028" pbm-agent'
 docker exec  -d mongors3 /bin/bash -c 'PBM_MONGODB_URI="mongodb://pbmuser:secretpwd@localhost:27029" pbm-agent'
 
 docker exec -u 0 -it pmm-client /bin/bash -c "yum -y install https://repo.percona.com/yum/percona-release-latest.noarch.rpm"
-docker exec -u 0 -it pmm-client /bin/bash -c "percona-release enable pbm release && yum -y install percona-backup-mongodb"
+docker exec -u 0 -it pmm-client /bin/bash -c "percona-release enable pbm release && yum -y install https://repo.percona.com/pbm/yum/release/8/RPMS/x86_64/percona-backup-mongodb-1.8.1-1.el8.x86_64.rpm"
 docker exec -u 0 -it pmm-client /bin/bash -c "pmm-admin add mongodb --service-name=mongo-backup-locations --username=admin --password=password --host=mongors1 --port=27027"
 docker exec -u 0 -it pmm-client /bin/bash -c "pmm-admin add mongodb --service-name=mongo-backup-schedule --username=admin --password=password --host=mongors1 --port=27027"
 docker exec -u 0 -it pmm-client /bin/bash -c "pmm-admin add mongodb --service-name=mongo-backup-inventory --username=admin --password=password --host=mongors1 --port=27027"
