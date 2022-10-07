04cdcffae8839f43117887a0f1ef7df26f9bd420
Peter <sirotnak.p@gmail.com>
2022-10-07 10:23:56 +0200

    changes to vacuum test

--- tests/dashboards/verifyExperimantalDashboards_test.js
+++ tests/dashboards/verifyExperimantalDashboards_test.js
@@ -8,8 +8,26 @@ Before(async ({ I }) => {
 
 Scenario(
   'PMM-T1365 Verify PostgreSQL Vacuum monitoring dashboard @nightly @dashboards @tempTest',
-  async ({ I, experimentalDashboardsPage }) => {
+  async ({
+    I, experimentalDashboardsPage, perconaPlatformPage, dashboardPage,
+  }) => {
     await I.amOnPage(experimentalDashboardsPage.vacuumDashboardPostgres.url);
-    await I.wait(180);
+    await dashboardPage.verifyThereAreNoGraphsWithoutData(0);
+    const output = await I.verifyCommand('sudo docker exec pgsql_vacuum_db psql -U postgres -d dvdrental -c \'SELECT tablename FROM pg_catalog.pg_tables;\'');
+    const allTables = output.split(/\r?\n/);
+
+    allTables.forEach(async (table) => {
+      if (table.includes('film_testing')) {
+        console.log(`Running Vacuum analyze for table: ${table.trim()}`);
+        await I.verifyCommand(`sudo docker exec pgsql_vacuum_db psql -U postgres -d dvdrental -c 'VACUUM  ( ANALYZE ) ${table.trim()}'`);
+      }
+    });
+
+    const failedReports = dashboardPage.grabFailedReportTitles();
+
+    console.log(failedReports);
+    await I.wait(480);
+    await I.refreshPage();
+    await I.waitForVisible(perconaPlatformPage.perconaPlatformPage_2_26.elements.connectForm, 30);
   },
 );
