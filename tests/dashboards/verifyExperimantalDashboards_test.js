f6991c7d8bb62af17272dc46709d902bc8ff0c13
Peter <sirotnak.p@gmail.com>
2022-10-11 13:22:36 +0300

    change timeout for the explicit wait

--- tests/dashboards/verifyExperimantalDashboards_test.js
+++ tests/dashboards/verifyExperimantalDashboards_test.js
@@ -32,7 +32,7 @@ Scenario(
       }
     });
     // await I.waitForInvisible(experimentalDashboardsPage.elements.neverRunField, 1200);
-    await I.wait(1200);
+    await I.wait(120);
     await I.refreshPage();
     await I.waitForVisible(perconaPlatformPage.perconaPlatformPage_2_26.elements.connectForm, 30);
   },
