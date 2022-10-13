89ba0a6a9a6bbb96195106313267dc998f396377
Peter <sirotnak.p@gmail.com>
2022-10-13 10:22:54 +0300

    add more conditions to do the vacuum

--- tests/pages/experimentalDashboards.js
+++ tests/pages/experimentalDashboards.js
@@ -16,7 +16,20 @@ module.exports = {
 
   async vacuumAnalyzeTables(tables) {
     for await (const table of tables.values()) {
-      if (table.includes('film_testing') || table.includes('dvdrental')) {
+      if (table.includes('film')
+      || table.includes('actor')
+      || table.includes('store')
+      || table.includes('address')
+      || table.includes('category')
+      || table.includes('city')
+      || table.includes('country')
+      || table.includes('customer')
+      || table.includes('inventory')
+      || table.includes('language')
+      || table.includes('rental')
+      || table.includes('staff')
+      || table.includes('payment')
+      ) {
         await I.say(table);
         await I.verifyCommand(`sudo docker exec pgsql_vacuum_db psql -U postgres -d dvdrental -c 'VACUUM  ( ANALYZE ) ${table.trim()}'`);
       }
