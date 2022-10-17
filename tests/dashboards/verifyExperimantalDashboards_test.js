f3eff2eb0a8c579b22322ea46f2195fc3c150707
Peter <sirotnak.p@gmail.com>
2022-10-17 08:48:03 +0200

    reordering test steps

--- tests/dashboards/verifyExperimantalDashboards_test.js
+++ tests/dashboards/verifyExperimantalDashboards_test.js
@@ -11,11 +11,6 @@ Scenario(
   async ({
     I, experimentalDashboardsPage, perconaPlatformPage,
   }) => {
-    const output = await I.verifyCommand('sudo docker exec pgsql_vacuum_db psql -U postgres -d dvdrental -c \'SELECT tablename FROM pg_catalog.pg_tables;\'');
-    const allTables = output.split(/\r?\n/);
-
-    await I.say(allTables);
-    await experimentalDashboardsPage.vacuumAnalyzeTables(allTables);
     await I.amOnPage(experimentalDashboardsPage.vacuumDashboardPostgres.url);
     await I.waitForVisible(experimentalDashboardsPage.elements.barValue, 60);
     const values = await I.grabTextFromAll(experimentalDashboardsPage.elements.barValue);
@@ -25,6 +20,12 @@ Scenario(
 
       assert.ok(valueInt > 0, 'The value for Postgres vacuum is zero, it supposed to be > 0');
     });
+
+    const output = await I.verifyCommand('sudo docker exec pgsql_vacuum_db psql -U postgres -d dvdrental -c \'SELECT tablename FROM pg_catalog.pg_tables;\'');
+    const allTables = output.split(/\r?\n/);
+
+    await I.say(allTables);
+    await experimentalDashboardsPage.vacuumAnalyzeTables(allTables);
 
     // await I.waitForInvisible(experimentalDashboardsPage.elements.neverRunField, 1200);
     await I.waitForVisible(perconaPlatformPage.perconaPlatformPage_2_26.elements.connectForm, 1000);
