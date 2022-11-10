8564266f730f21f275501f7ede5ce7216a566de7
yurkovychv <vasyl.yurkovych@percona.com>
2022-11-10 16:14:11 +0200

    PMM-11027 fix ssl test

--- tests/verifyTLSMySQLRemoteInstance_test.js
+++ tests/verifyTLSMySQLRemoteInstance_test.js
@@ -101,11 +101,14 @@ Data(instances).Scenario(
 Data(instances).Scenario(
   'PMM-T937 PMM-T938 Verify MySQL cannot be added without specified --tls-key, Verify MySQL cannot be added without specified --tls-cert @ssl @ssl-remote @not-ui-pipeline',
   async ({
-    I, current, grafanaAPI,
+    I, current, grafanaAPI, remoteInstancesPage,
   }) => {
     const {
       container,
     } = current;
+
+    I.amOnPage(remoteInstancesPage.url);
+
     let responseMessage = 'Connection check failed: register MySQL client cert failed: tls: failed to find any PEM data in key input.\n';
     let command = `docker exec ${container} pmm-admin add mysql --username=pmm --password=pmm --port=3306 --query-source=perfschema --tls --tls-skip-verify --tls-ca=/var/lib/mysql/ca.pem --tls-cert=/var/lib/mysql/client-cert.pem TLS_mysql`;
 
@@ -176,7 +179,7 @@ Data(instances).Scenario(
 Data(instances).Scenario(
   'PMM-T1277 (1.0) Verify tlsCa, tlsCert, tlsKey are generated on every MySQL exporter (added with TLS flags) restart @ssl @ssl-remote @not-ui-pipeline',
   async ({
-    I, current, dashboardPage
+    I, current, dashboardPage,
   }) => {
     const {
       container,
