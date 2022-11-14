122f1a9d3e114d0d1223134916f5cc7d807ed7c7
yurkovychv <vasyl.yurkovych@percona.com>
2022-11-14 18:36:27 +0200

    PMM-11080 use buffer

--- cli/tests/pmm-cli/pmm.spec.ts
+++ cli/tests/pmm-cli/pmm.spec.ts
@@ -1,5 +1,6 @@
 import { test, expect } from '@playwright/test';
 import * as cli from '@helpers/cliHelper';
+import child_process from 'node:child_process';
 
 test.describe('PMM binary tests @pmm-cli', async () => {
   test('--version', async ({}) => {
@@ -8,9 +9,13 @@ test.describe('PMM binary tests @pmm-cli', async () => {
   });
 
   test('server docker install ', async ({}) => {
-    const output = await cli.exec('pmm server docker install --admin-password="test" --https-listen-port=443 --http-listen-port=80');
+    const b = child_process.execFileSync('pmm', ['server', 'docker', 'install', '--admin-password="test"', '--https-listen-port=443', '--http-listen-port=80'], { stdio: 'inherit' });
+
+    b.forEach((a) => console.log(a));
+    // const output = await cli.exec('pmm server docker install --admin-password="test" --https-listen-port=443 --http-listen-port=80');
+    // const output = await cli.exec('pmm server docker install --admin-password="test" --https-listen-port=443 --http-listen-port=80');
     // await output.assertSuccess();
 
-    await output.containsMany(['PMM Server is now available at http://localhost/']);
+    // await output.containsMany(['PMM Server is now available at http://localhost/']);
   });
 });
