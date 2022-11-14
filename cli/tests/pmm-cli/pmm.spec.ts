dcf61791a22c699bf744e68b68d8dda61ddc6cdb
yurkovychv <vasyl.yurkovych@percona.com>
2022-11-14 18:01:48 +0200

    PMM-11080 add docker install command

--- cli/tests/pmm-cli/pmm.spec.ts
+++ cli/tests/pmm-cli/pmm.spec.ts
@@ -1,10 +1,14 @@
 import { test, expect } from '@playwright/test';
 import * as cli from '@helpers/cliHelper';
 
-test.describe('PMM binary tests ', async () => {
-  test('pmm cli @pmm-cli', async ({}) => {
+test.describe('PMM binary tests @pmm-cli', async () => {
+  test('--version', async ({}) => {
     const output = await cli.exec('pmm --version');
+    await output.assertSuccess();
+  });
 
+  test('server docker install ', async ({}) => {
+    const output = await cli.exec('pmm server docker install --admin-password="test" --https-listen-port=443 --http-listen-port=80');
     console.log(output);
     await output.assertSuccess();
   });
