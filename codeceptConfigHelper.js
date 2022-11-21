51dbfb3dadc799b26ae150253384df1c7c1cf86c
Peter <sirotnak.p@gmail.com>
2022-11-21 11:56:39 +0100

    PMM-7: Refactor vacuum test

--- codeceptConfigHelper.js
+++ codeceptConfigHelper.js
@@ -23,7 +23,7 @@ module.exports = {
     dbaasPage: './tests/DbaaS/pages/dbaasPage.js',
     dbaasActionsPage: './tests/DbaaS/pages/dbaasActionsPage.js',
     dbaasManageVersionPage: './tests/DbaaS/pages/dbaasManageVersionPage.js',
-    experimentalPostgresqlDashboardsPage: './tests/pages/experimentalPostgresqlDashboards.js',
+    experimentalPostgresqlDashboardsPage: './tests/PageObjects/Dashboards/Postgresql/ExperimentalPostgresqlDashboards.js',
     grafanaAPI: './tests/pages/api/grafanaAPI.js',
     homePage: './tests/pages/homePage.js',
     inventoryAPI: './tests/pages/api/inventoryAPI.js',
