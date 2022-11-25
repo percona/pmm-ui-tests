aa9c147de8692d4a40f7dcb40e36f066dc077ba1
Matej Kubinec <matej.kubinec@3pillarglobal.com>
2022-11-25 16:16:49 +0100

    PMM-10881 Change "Edit folder" to "Go to folder"
    In grafana 9.2.5 the "Edit folder" button changed to
    the "Go to folder" button.

--- tests/ia/alertRules_test.js
+++ tests/ia/alertRules_test.js
@@ -61,8 +61,8 @@ Scenario(
     });
     const folderUID = await rulesAPI.getFolderUID(ruleFolder);
 
-    I.seeElement(alertRulesPage.buttons.editFolderButton(folderUID, ruleFolder.toLowerCase()));
-    I.seeElement(alertRulesPage.buttons.managePermissionsButton(folderUID, ruleFolder.toLowerCase()));
+    I.seeElement(alertRulesPage.buttons.goToFolderButton(folderUID));
+    I.seeElement(alertRulesPage.buttons.managePermissionsButton(folderUID));
     I.seeElement(alertRulesPage.elements.totalRulesCounter('1 rule', ruleFolder));
     await rulesAPI.removeAlertRule(ruleFolder);
   },
