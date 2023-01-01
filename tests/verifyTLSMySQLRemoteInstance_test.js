289ec7f1daa5f675c0552df32fe008be42d3b339
Vadym Yarosh <vadym.yarosh@percona.com>
2023-01-01 16:47:48 +0100

    PMM-11066 missing queries fix try

--- tests/verifyTLSMySQLRemoteInstance_test.js
+++ tests/verifyTLSMySQLRemoteInstance_test.js
@@ -239,6 +239,7 @@ Data(maxQueryLengthInstances).Scenario(
 
     // Main check: Query label is cut of by max_query_length option on QAN Page
     // I.amOnPage(I.buildUrlWithParams(qanPage.clearUrl, { from: 'now-5m', service_name: remoteServiceName, search: 'SELECT' }));
+    I.wait(30);
     I.amOnPage(I.buildUrlWithParams(qanPage.clearUrl, { from: 'now-5m', service_name: remoteServiceName }));
     I.waitForElement(qanOverview.elements.querySelector, 30);
     const queryFromRow = await qanOverview.getQueryFromRow(1);
