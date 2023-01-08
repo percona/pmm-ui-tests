b386d1bd63f01d7fa5840c8d525006af5ff22b5b
Vadym Yarosh <vadym.yarosh@percona.com>
2023-01-08 17:42:30 +0100

    PMM-11066 pipeline debug

--- tests/verifyTLSPostgresRemoteInstance_test.js
+++ tests/verifyTLSPostgresRemoteInstance_test.js
@@ -348,17 +348,8 @@ Data(instances).Scenario(
       await pmmInventoryPage.checkAgentOtherDetailsMissing('max_query_length:', serviceId);
     }
 
-    // Check max visible query length is less than max_query_length option
     // Main check: Query label is cut of by max_query_length option on QAN Page
-    // I.amOnPage(I.buildUrlWithParams(qanPage.clearUrl, { from: 'now-5m', service_name: remoteServiceName, search: 'SELECT' }));
-    // I.wait(30);
-    // I.amOnPage(I.buildUrlWithParams(qanPage.clearUrl, { from: 'now-5m', service_name: remoteServiceName }));
-    // I.waitForElement(qanOverview.elements.querySelector, 30);
-    // const queryFromRow = await qanOverview.getQueryFromRow(1);
-
-    I.amOnPage(I.buildUrlWithParams(qanPage.clearUrl, { from: 'now-5m' }));
-    qanOverview.waitForOverviewLoaded();
-    await qanFilters.applyFilter(remoteServiceName);
+    I.amOnPage(I.buildUrlWithParams(qanPage.clearUrl, { from: 'now-5m', service_name: remoteServiceName, search: 'SELECT' }));
     I.waitForElement(qanOverview.elements.querySelector, 30);
     const queryFromRow = await qanOverview.getQueryFromRow(1);
 
