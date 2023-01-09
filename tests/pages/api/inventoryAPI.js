992e2278cf532eba5719d15ed3481cacff140b1c
Vadym Yarosh <vadym.yarosh@percona.com>
2023-01-09 11:32:17 +0100

    PMM-11086 fixed typo

--- tests/pages/api/inventoryAPI.js
+++ tests/pages/api/inventoryAPI.js
@@ -47,7 +47,7 @@ module.exports = {
       .flat(Infinity)
       .filter(({ service_name }) => service_name.includes(serviceName));
 
-    if (data.length === 0) await I.say(`Service "${serviceName}" of "${serviceType}" type is no found!`);
+    if (data.length === 0) await I.say(`Service "${serviceName}" of "${serviceType}" type is not found!`);
 
     if (excludeSubstring) {
       return data.find(({ service_name }) => !service_name.includes(excludeSubstring));
