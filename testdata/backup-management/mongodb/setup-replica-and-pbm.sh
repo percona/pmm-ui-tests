97da8bcc1c66c281a6b62444165b243a9ec8dc9b
Peter <sirotnak.p@gmail.com>
2023-01-30 20:42:11 +0100

    fixes docker container names

--- testdata/backup-management/mongodb/setup-replica-and-pbm.sh
+++ testdata/backup-management/mongodb/setup-replica-and-pbm.sh
@@ -83,8 +83,8 @@ docker exec  -d mongors1 /bin/bash -c 'PBM_MONGODB_URI="mongodb://pbmuser:secret
 docker exec  -d mongors2 /bin/bash -c 'PBM_MONGODB_URI="mongodb://pbmuser:secretpwd@localhost:27028" pbm-agent'
 docker exec  -d mongors3 /bin/bash -c 'PBM_MONGODB_URI="mongodb://pbmuser:secretpwd@localhost:27029" pbm-agent'
 
-docker exec  -d mongors3 /bin/bash -c 'pmm-admin add mongodb --service-name=mongo1 --host=127.0.0.1 --port=27027 --cluster=rs0'
-docker exec  -d mongors3 /bin/bash -c 'pmm-admin add mongodb --service-name=mongo2 --host=127.0.0.1 --port=27027 --cluster=rs0'
+docker exec  -d mongors1 /bin/bash -c 'pmm-admin add mongodb --service-name=mongo1 --host=127.0.0.1 --port=27027 --cluster=rs0'
+docker exec  -d mongors2 /bin/bash -c 'pmm-admin add mongodb --service-name=mongo2 --host=127.0.0.1 --port=27027 --cluster=rs0'
 docker exec  -d mongors3 /bin/bash -c 'pmm-admin add mongodb --service-name=mongo3 --host=127.0.0.1 --port=27027 --cluster=rs0'
 
 sudo -- sh -c "echo '127.0.0.1 mongors1 mongors2 mongors3' >> /etc/hosts"
