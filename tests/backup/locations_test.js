4739cb586aa30b6c4011c1679f6c16ed15ecb66b
yurkovychv <vasyl.yurkovych@percona.com>
2023-02-02 18:51:54 +0200

    PMM-11500 trigger

--- tests/backup/locations_test.js
+++ tests/backup/locations_test.js
@@ -14,7 +14,7 @@ Feature('BM: Backup Locations').retry(1);
 
 BeforeSuite(async ({ I }) => {
   // await I.suppressTour();
-  I.say(await I.verifyCommand(`docker exec rs101 pmm-admin add mongodb --username=pmm --password=pmmpass --port=27027 --service-name=${mongoServiceName} --replication-set=rs0`));
+  I.say(await I.verifyCommand(`docker exec rs101 pmm-admin add mongodb --username=pmm --password=pmmpass --port=27027 --service-name=${mongoServiceName} --replication-set=rs0 `));
 });
 
 Before(async ({
