d030b18f4df331d00e6b2df24c0c850abba685f2
Vladimir Nazarov <vladimir.nazarov@simbirsoft.com>
2023-02-02 21:22:46 +0300

    Add case for os dashboards

---
+++ tests/dashboards/verifyHomeDashboards_test.js
@@ -0,0 +1,58 @@
+const panels = new DataTable(['panelName', 'dashboardType', 'dashboardName', 'dashboard']);
+
+Feature('Test Dashboards collection inside the Folders').retry(1);
+
+panels.add(['Disk Space Total', 'singleNode', 'Disk Details', 'osDiskDetails']);
+panels.add(['Disk Reads', 'singleNode', 'Disk Details', 'osDiskDetails']);
+panels.add(['Disk Writes', 'singleNode', 'Disk Details', 'osDiskDetails']);
+panels.add(['Total RAM', 'singleNode', 'Memory Details', 'osMemoryDetails']);
+panels.add(['Virtual Memory Total', 'multipleNodes', 'Nodes Overview', 'osNodesOverview']);
+panels.add(['Monitored Nodes', 'multipleNodes', 'Nodes Overview', 'osNodesOverview']);
+panels.add(['Total Virtual CPUs', 'multipleNodes', 'Nodes Overview', 'osNodesOverview']);
+Before(async ({ I, homePage }) => {
+  await I.Authorize();
+  await homePage.open();
+});
+
+Data(panels).Scenario(
+  '@PMM-T1565 Verify ability to access OS dashboards with correct filter setup from Home Dashboard',
+  async ({
+    I, current, dashboardPage, adminPage,
+  }) => {
+    const {
+      panelName, dashboardType, dashboardName, dashboard,
+    } = current;
+
+    const expectedDashboard = dashboardPage[dashboard];
+
+    I.click(dashboardPage.fields.openFiltersDropdownLocator('Node Name'));
+    const nodeNames = await I.grabTextFromAll('//a[@role="checkbox"]/span[text()!="All"]');
+
+    I.click(dashboardPage.fields.filterDropdownOptionsLocator(nodeNames[0]));
+    I.click(dashboardPage.fields.filterDropdownOptionsLocator(nodeNames[1]));
+    I.click(dashboardPage.fields.refresh);
+    dashboardPage.waitForDataLoaded();
+    let expectedNodeName;
+
+    if (dashboardType === 'singleNode') {
+      // eslint-disable-next-line prefer-destructuring
+      expectedNodeName = nodeNames.sort()[0];
+    } else {
+      expectedNodeName = await I.grabTextFrom(dashboardPage.fields.openFiltersDropdownLocator('Node Name'));
+    }
+
+    I.click(dashboardPage.fields.clickablePanel(panelName));
+    I.switchToNextTab();
+    I.waitForElement(`//span[text()="${dashboardName}"]`, 60);
+    I.seeInCurrentUrl(expectedDashboard.clearUrl);
+    await I.assertEqual(await I.grabTextFrom(dashboardPage.fields.openFiltersDropdownLocator('Node Name')), expectedNodeName);
+    await dashboardPage.expandEachDashboardRow();
+
+    // needed to refocus on new tab
+    I.click('//label[text()="PMM Annotations"]');
+    adminPage.performPageDown(1);
+    dashboardPage.verifyMetricsExistence(expectedDashboard.metrics);
+    await dashboardPage.verifyThereAreNoGraphsWithNA(0);
+    await dashboardPage.verifyThereAreNoGraphsWithoutData(0);
+  },
+);
