be4faf88be9cadbe226349161cd9e14990a13a4b
yurkovychv <vasyl.yurkovych@percona.com>
2023-02-08 17:49:04 +0200

    PMM-11500 fix tests

--- tests/backup/scheduled_test.js
+++ tests/backup/scheduled_test.js
@@ -47,7 +47,6 @@ BeforeSuite(async ({
   });
 
   I.say(await I.verifyCommand(`docker exec rs101 pmm-admin add mongodb --username=pmm --password=pmmpass --port=27017 --service-name=${mongoServiceName} --replication-set=rs --cluster=${mongoCluster}`));
-  I.say(await I.verifyCommand(`docker exec rs101 pmm-admin add mongodb --username=pmm --password=pmmpass --port=27017 --service-name=${mongoServiceName} --replication-set=rs --cluster=${mongoCluster}`));
   I.say(await I.verifyCommand(`docker exec rs101 pmm-admin add mongodb --username=pmm --password=pmmpass --port=27017 --service-name=${mongoServiceName2} --replication-set=rs --cluster=${mongoCluster}`));
   I.say(await I.verifyCommand(`docker exec rs101 pmm-admin add mongodb --username=pmm --password=pmmpass --port=27017 --service-name=${mongoNameWithoutCluster} --replication-set=rs --cluster=${mongoCluster}`));
 });
