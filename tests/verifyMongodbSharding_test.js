c9bc27f1ad2771badda580aba187b821f2d8ad3d
Vladimir Nazarov <vladimir.nazarov@simbirsoft.com>
2023-02-16 17:09:35 +0300

    Small fixes

--- tests/verifyMongodbSharding_test.js
+++ tests/verifyMongodbSharding_test.js
@@ -2,23 +2,23 @@ Feature('MongoDB Sharding tests');
 
 const { adminPage } = inject();
 const pmmFrameworkLoader = `bash ${adminPage.pathToFramework}`;
-const exactMongoDbInfo = new DataTable(['version', 'exactVersion', 'edition']);
+const exactMongoDbInfo = new DataTable(['version', 'edition']);
 
-// exactMongoDbInfo.add(['4.0', 'TBD', 'Community']);
-exactMongoDbInfo.add(['4.2', '4.2.23-23', 'Community']);
-exactMongoDbInfo.add(['4.4', '4.4.16-16', 'Community']);
-// exactMongoDbInfo.add(['5.0', 'TBD', 'Community']);
-exactMongoDbInfo.add(['6.0', '6.0.2-1', 'Community']);
+// exactMongoDbInfo.add(['4.0', 'Community']);
+exactMongoDbInfo.add(['4.2', 'Community']);
+exactMongoDbInfo.add(['4.4', 'Community']);
+// exactMongoDbInfo.add(['5.0', 'Community']);
+exactMongoDbInfo.add(['6.0', 'Community']);
 
 Data(exactMongoDbInfo).Scenario(
   '@PMM-T1539 Verify that MongoDB exporter shows version for mongos instance @mongodb-exporter',
   async ({
     I, current,
   }) => {
-    const { version, exactVersion, edition } = current;
+    const { version, edition } = current;
 
     await I.verifyCommand(`${pmmFrameworkLoader} --mongomagic --with-sharding --pmm2 --mo-version=${version}`);
-    const containerName = await I.verifyCommand(`docker ps --format "table {{.ID}}\\t{{.Image}}\\t{{.Names}}" | grep 'psmdb.*${version}' | awk -F " " '{print $3}'`);
+    const containerName = await I.verifyCommand(`docker ps --format "table {{.ID}}\\t{{.Image}}\\t{{.Names}}" | grep 'psmdb.*${version}_sharded' | awk -F " " '{print $3}'`);
     const agentId = await I.verifyCommand(`docker exec ${containerName} pmm-admin list | grep "42002" | awk -F " " '{print $4}'`);
 
     const actualEdition = await I.verifyCommand(`docker exec ${containerName} curl --silent -u pmm:${agentId} localhost:42002/metrics | grep "mongodb_version_info" | grep -oP '(?<=edition=").*?(?=")'`);
@@ -30,10 +30,10 @@ Data(exactMongoDbInfo).Scenario(
       `Expected the Edition of MongoDB to be equal to ${edition} but found ${actualEdition}`,
     );
 
-    I.assertEqual(
+    await I.assertContain(
       actualExactVersion,
-      exactVersion,
-      `Expected the Version of MongoDB to be equal to ${exactVersion} but found ${actualExactVersion}`,
+      version,
+      `Expected the Version of MongoDB to begin with ${version} but found ${actualExactVersion}`,
     );
 
     await I.verifyCommand(`docker rm -f ${containerName}`);
