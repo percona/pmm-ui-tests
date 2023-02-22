e52ebe1b779fd397d02daccb907013a011b88364
Vladimir Nazarov <vladimir.nazarov@simbirsoft.com>
2023-02-22 14:07:52 +0300

    Reverted changes

--- tests/dashboards/verifyExperimentalPostgresqlDashboards_test.js
+++ tests/dashboards/verifyExperimentalPostgresqlDashboards_test.js
@@ -43,7 +43,7 @@ Data(panels).Scenario(
     I.click(dashboardPage.fields.clickablePanel(panelName));
     I.switchToNextTab();
     I.waitForElement(`//span[text()="${dashboardName}"]`, 60);
-    // I.seeInCurrentUrl(expectedDashboard.clearUrl);
+    I.seeInCurrentUrl(expectedDashboard.clearUrl);
     // await I.assertEqual(await I.grabTextFrom(dashboardPage.fields.openFiltersDropdownLocator('Node Name')), expectedNodeName);
     // await dashboardPage.expandEachDashboardRow();
     //
