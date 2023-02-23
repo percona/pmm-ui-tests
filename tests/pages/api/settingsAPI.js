61b6b5557ca95003f447225061ac629e3b2e833b
Puneet Kala <puneet.0191@gmail.com>
2023-02-23 13:26:07 +0530

    PMM-11523 update function

--- tests/pages/api/settingsAPI.js
+++ tests/pages/api/settingsAPI.js
@@ -227,4 +227,17 @@ module.exports = {
 
     return resp.data.settings[property];
   },
+  
+  async setTourOptions(productTour = true, alertingTour = true) {
+    const headers = { Authorization: `Basic ${await this.getAuth()}` };
+
+    const body = {
+      product_tour_completed: productTour,
+      alerting_tour_completed: alertingTour,
+    };
+
+    const resp = await apiContext.sendPutRequest('v1/user', body, headers);
+
+    assert.equal(resp.status, 200, `Failed to set up PMM tour options! Response with status ${resp.status}`);
+  },
 };
