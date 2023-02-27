bd6c0d6fc040a0623b666d0e3ddbe334eda2d7fe
Vadym Yarosh <vadym.yarosh@percona.com>
2023-02-27 02:06:12 +0100

    PMM-11549 added login flow fix

--- tests/configuration/profile_test.js
+++ tests/configuration/profile_test.js
@@ -6,49 +6,64 @@
 
 Feature('PMM User Profile tests');
 
+const INITIAL_ADMIN_PASSWORD = process.env.ADMIN_PASSWORD;
 const NEW_ADMIN_PASSWORD = 'admin1';
-// process.env['NODE_ENV'] = 'production';
 
 After(async ({ I, profileAPI }) => {
+  // eslint-disable-next-line no-undef
   await tryTo(() => {
-    I.Authorize('admin', NEW_ADMIN_PASSWORD);
-    profileAPI.changePassword('admin', NEW_ADMIN_PASSWORD, process.env.ADMIN_PASSWORD);
+    I.Authorize();
+    profileAPI.changePassword('admin', process.env.ADMIN_PASSWORD, INITIAL_ADMIN_PASSWORD);
   });
 });
 
 Scenario(
   'PMM-T1559 Verify clients still can connect to PMM server after password\'s changing @user-password',
   async ({
-    I, changePasswordPage, loginPage, pmmInventoryPage,
+    I, changePasswordPage, loginPage, pmmInventoryPage, grafanaAPI, inventoryAPI,
   }) => {
     await I.amOnPage(loginPage.url);
-    await I.say(`process.env.ADMIN_PASSWORD is '${process.env.ADMIN_PASSWORD}'`);
-    loginPage.login();
+    await loginPage.login();
+
+    await grafanaAPI.checkMetricExist('pg_stat_activity_count', null);
+    await grafanaAPI.checkMetricExist('mysql_global_status_threads_connected', null);
+    // await grafanaAPI.checkMetricExist('mongodb_mongod_op_counters_total', null);
+    await grafanaAPI.checkMetricExist('node_cpu_seconds_total', null);
+
     await changePasswordPage.open();
     changePasswordPage.fillChangePasswordForm(process.env.ADMIN_PASSWORD, NEW_ADMIN_PASSWORD);
     changePasswordPage.applyChanges();
     I.signOut();
     await I.waitForVisible(loginPage.fields.loginInput, 30);
-    await I.Authorize('admin', NEW_ADMIN_PASSWORD);
+    process.env.ADMIN_PASSWORD = NEW_ADMIN_PASSWORD;
+    await loginPage.login();
 
     await I.say('Verify all agents have "Running" status. There is no agent with "DONE" and "UNKNOWN" status');
+    for (const name of ['vmagent', 'node_exporter', 'postgres_exporter', 'mongodb_exporter', 'mysqld_exporter']) {
+      const listStatus = (await I.verifyCommand(`pmm-admin list | grep ${name} | awk -F' ' '{print $2}' | tail -n 1`)).trim();
+      const status = (await I.verifyCommand(`pmm-admin status | grep ${name} | awk -F' ' '{print $3}' | tail -n 1`)).trim();
+
+      I.assertEqual(listStatus, 'Running', `'${name}' is expected to have 'Running' status in pmm-admin list, but found: '${status}'`);
+      I.assertEqual(status, 'Running', `'${name}' is expected to have 'Running' status in pmm-admin status, but found: '${status}'`);
+    }
+
     await pmmInventoryPage.agentsTab.open();
     await pmmInventoryPage.agentsTab.pagination.selectRowsPerPage(100);
 
     // TODO: improve inventoryAPI.apiGetServices() to handle flexible auth.
-    const resp = await I.sendPostRequest('v1/inventory/Services/List', {},
-      { Authorization: `Basic ${await I.getAuth('admin', NEW_ADMIN_PASSWORD)}` });
-    const services = Object.values(resp.data).flat(Infinity).map((o) => ({ id: o.service_id, name: o.service_name }));
+    const services = Object.values((await inventoryAPI.apiGetServices()).data).flat(Infinity)
+      .map((o) => ({ id: o.service_id, name: o.service_name }));
 
     for (const service of services) {
       await pmmInventoryPage.agentsTab.verifyAgentOtherDetailsSection('status:', 'status: RUNNING', service.name, service.id);
     }
 
+    await I.say('Verify QAN continues to receive data');
     // TODO: refactor grafanaAPI.getMetric() to have time range argument, add sleep 5 sec and get metrics for last 5 sec
     // Verify metrics exists: useless with current hardcoded time range
-    // await grafanaAPI.checkMetricExist('pg_stat_activity_count', null);
-    // await grafanaAPI.checkMetricExist('mysql_global_status_threads_connected', null);
+    await grafanaAPI.checkMetricExist('pg_stat_activity_count', null);
+    await grafanaAPI.checkMetricExist('mysql_global_status_threads_connected', null);
     // await grafanaAPI.checkMetricExist('mongodb_mongod_op_counters_total', null);
-    // await grafanaAPI.checkMetricExist('node_cpu_seconds_total', null);
+    await grafanaAPI.checkMetricExist('node_cpu_seconds_total', null);
   },
 );
--- tests/pages/loginPage.js
+++ tests/pages/loginPage.js
@@ -13,15 +13,22 @@ module.exports = {
   },
 
   // introducing methods
-  login(username = 'admin', password = process.env.ADMIN_PASSWORD) {
+  async login(username = 'admin', password = process.env.ADMIN_PASSWORD) {
     I.seeElement(this.fields.loginInput);
     I.fillField(this.fields.loginInput, username);
     I.seeElement(this.fields.passwordInput);
     I.fillField(this.fields.passwordInput, password);
     I.click(this.fields.loginButton);
-    I.verifyPopUpMessage(this.messages.loginSuccess, 30);
-    I.seeElement(this.fields.skipButton);
-    I.click(this.fields.skipButton);
+
+    // BUG: there is no message on success for changed password
+    // eslint-disable-next-line no-undef
+    tryTo(() => I.verifyPopUpMessage(this.messages.loginSuccess, 5));
+
+    if ((await I.grabCurrentUrl()).includes(this.url)) {
+      I.seeElement(this.fields.skipButton);
+      I.click(this.fields.skipButton);
+    }
+
     I.waitInUrl(homePage.landingUrl);
   },
 
