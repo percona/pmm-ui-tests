8c8571856a6604968e42612c43f44ef04a995b42
Peter Sirotnak <sirotnak.p@gmail.com>
2023-03-17 21:25:03 +0100

    Adds firts batch of tests for the percona server

---
+++ cli/tests/perconaMySqlServer.spec.ts
@@ -0,0 +1,71 @@
+import { test } from '@playwright/test';
+import * as cli from '@helpers/cliHelper';
+
+const MYSQL_USER = 'root'
+const MYSQL_PASSWORD = "GRgrO9301RuF"
+
+test.describe('PMM Client CLI tests for Percona Server Database', async () => {
+
+  /**
+   * @link https://github.com/percona/pmm-qa/blob/main/pmm-tests/pmm-2-0-bats-tests/ps-specific-tests.bats#L9
+   */
+  test('run pmm-admin under regular(non-root) user privileges', async ({ }) => {
+    const output = await cli.exec(`pmm-admin`);
+    await output.assertSuccess();
+    await output.outContains('Usage: pmm-admin <command>');
+  });
+
+  /**
+   * @link https://github.com/percona/pmm-qa/blob/main/pmm-tests/pmm-2-0-bats-tests/ps-specific-tests.bats#L19
+   */
+  test('run pmm-admin under root privileges', async ({ }) => {
+    const output = await cli.exec(`sudo pmm-admin`);
+    await output.assertSuccess();
+    await output.outContains('Usage: pmm-admin <command>');
+  });
+
+  /**
+   * @link https://github.com/percona/pmm-qa/blob/main/pmm-tests/pmm-2-0-bats-tests/ps-specific-tests.bats#L29
+   */
+  test('run pmm-admin add mysql based on running intsancess', async ({ }) => {
+    let hosts = (await cli.exec(`pmm-admin list | grep "MySQL" | awk -F" " '{print $3}'`))
+      .stdout.trim().split('\n').filter((item) => item.trim().length > 0);
+    let n = 1;
+    for (const host of hosts) {
+      let output = await cli.exec(`run pmm-admin add mysql --query-source=perfschema --username=${MYSQL_USER} --password=${MYSQL_PASSWORD} mysql_${n++} ${n++}`);
+      await output.assertSuccess();
+      await output.outContains('MySQL Service added.');
+    }
+  });
+
+
+  /**
+   * @link https://github.com/percona/pmm-qa/blob/main/pmm-tests/pmm-2-0-bats-tests/ps-specific-tests.bats#L44
+   */
+  test('run pmm-admin add mysql again based on running instances', async ({ }) => {
+    let hosts = (await cli.exec(`pmm-admin list | grep "MySQL" | awk -F" " '{print $3}'`))
+      .stdout.trim().split('\n').filter((item) => item.trim().length > 0);
+    let n = 1;
+    for (const host of hosts) {
+      let output = await cli.exec(`run pmm-admin add mysql --query-source=perfschema --username=${MYSQL_USER} --password=${MYSQL_PASSWORD} mysql_${n++} ${n++}`);
+      await output.assertSuccess();
+      await output.outContains('already exists.');
+    }
+  });
+
+  /**
+   * @link https://github.com/percona/pmm-qa/blob/main/pmm-tests/pmm-2-0-bats-tests/ps-specific-tests.bats#L57
+   */
+  test('run pmm-admin status --json check for RUNNING string in output', async ({ }) => {
+    const output = await cli.exec(`pmm-admin status --json | grep "RUNNING"`);
+    await output.assertSuccess();
+  });
+
+  /**
+   * @link https://github.com/percona/pmm-qa/blob/main/pmm-tests/pmm-2-0-bats-tests/ps-specific-tests.bats#L63
+   */
+  test('run pmm-admin status check for RUNNING string in output for VM_AGENT', async ({ }) => {
+    const output = await cli.exec(`pmm-admin status | grep "vmagent Running"`);
+    await output.assertSuccess();
+  });
+});
