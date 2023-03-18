8b4486febe3d4f87f40894aa2489c049feb8902e
Peter Sirotnak <sirotnak.p@gmail.com>
2023-03-18 10:59:50 +0100

    New tests and retry for vmagent

--- cli/tests/perconaMySqlServer.spec.ts
+++ cli/tests/perconaMySqlServer.spec.ts
@@ -24,7 +24,6 @@ test.describe('PMM Client CLI tests for Percona Server Database', async () => {
     let hosts = (await cli.exec(`pmm-admin list | grep "MySQL" | awk -F" " '{print $3}'`))
       .stdout.trim().split('\n').filter((item) => item.trim().length > 0);
     let n = 1;
-    console.log(hosts);
     for (const host of hosts) {
       let output = await cli.exec(`pmm-admin add mysql --query-source=perfschema --username=${MYSQL_USER} --password=${MYSQL_PASSWORD} mysql_${n++} ${host}`);
       await output.assertSuccess();
@@ -40,7 +39,6 @@ test.describe('PMM Client CLI tests for Percona Server Database', async () => {
     let hosts = (await cli.exec(`pmm-admin list | grep "MySQL" | grep "mysql_" | awk -F" " '{print $3}'`))
       .stdout.trim().split('\n').filter((item) => item.trim().length > 0);
     let n = 1;
-    console.log(hosts);
     for (const host of hosts) {
       let output = await cli.exec(`pmm-admin add mysql --query-source=perfschema --username=${MYSQL_USER} --password=${MYSQL_PASSWORD} mysql_${n++} ${host}`);
       await output.exitCodeEquals(1);
@@ -60,8 +58,45 @@ test.describe('PMM Client CLI tests for Percona Server Database', async () => {
    * @link https://github.com/percona/pmm-qa/blob/main/pmm-tests/pmm-2-0-bats-tests/ps-specific-tests.bats#L63
    */
   test('run pmm-admin status check for RUNNING string in output for VM_AGENT', async ({ }) => {
-    const output = await cli.exec(`pmm-admin status | grep "vmagent Running"`);
-    console.log(await cli.exec(`pmm-admin status`));
+    for (let i = 0; i < 5; i++) {
+      console.log(`Retry number: ${i}`)
+      try {
+        const output = await cli.exec(`pmm-admin status | grep "vmagent Running"`);
+        await output.assertSuccess();
+        break;
+      } catch (error) {
+        if (i === 4) {
+          throw new Error("vmagent was not Running");
+        }
+      }
+      await new Promise(f => setTimeout(f, 1000));
+    }
+  });
+
+  /**
+   * @link https://github.com/percona/pmm-qa/blob/main/pmm-tests/pmm-2-0-bats-tests/ps-specific-tests.bats#L69
+   */
+  test('run pmm-admin status check for Running string in output', async ({ }) => {
+    const output = await cli.exec(`pmm-admin status | grep "Running"`);
     await output.assertSuccess();
   });
+
+  /**
+   * @link https://github.com/percona/pmm-qa/blob/main/pmm-tests/pmm-2-0-bats-tests/ps-specific-tests.bats#L75
+   */
+  test('run pmm-admin status check for RUNNING string in output', async ({ }) => {
+    const output = await cli.exec(`pmm-admin status | grep "RUNNING"`);
+    await output.exitCodeEquals(1);
+  });
+
+  /**
+   * @link https://github.com/percona/pmm-qa/blob/main/pmm-tests/pmm-2-0-bats-tests/ps-specific-tests.bats#L81
+   */
+  test('run pmm-admin status check for RUNNING string in output', async ({ }) => {
+    const output = await cli.exec(`pmm-admin status --json | grep "Running"`);
+    await output.exitCodeEquals(1);
+  });
+
+
+
 });
