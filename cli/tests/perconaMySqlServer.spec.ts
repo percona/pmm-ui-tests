a2770b3d5bcb12fa09b3801796f0ebd2095dc9c5
Peter Sirotnak <sirotnak.p@gmail.com>
2023-03-20 11:10:22 +0100

    List pmm submodules folder

--- cli/tests/perconaMySqlServer.spec.ts
+++ cli/tests/perconaMySqlServer.spec.ts
@@ -320,7 +320,7 @@ test.describe('PMM Client CLI tests for Percona Server Database', async () => {
     let n = 1;
     for (const host of hosts) {
       await cli.exec('sleep 20');
-      console.log(await cli.exec('ls ..'));
+      console.log(await cli.exec('ls /home/runner/work/pmm-submodules/pmm-submodules/'));
       console.log(await cli.exec('pwd'));
       await (await cli.exec('sudo chmod +x /srv/pmm-qa/pmm-tests/pmm-2-0-bats-tests/check_metric.sh')).assertSuccess();
       let output = await cli.exec(`/srv/pmm-qa/pmm-tests/pmm-2-0-bats-tests/check_metric.sh mysql_${n++} mysql_up 127.0.0.1 mysqld_exporter pmm mypass`);
