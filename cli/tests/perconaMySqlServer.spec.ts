eda36cf1404368a43942bc5a3fb7f791fb9d7462
Peter Sirotnak <sirotnak.p@gmail.com>
2023-03-20 09:54:13 +0100

    Adds tests for metrics mode push

--- cli/tests/perconaMySqlServer.spec.ts
+++ cli/tests/perconaMySqlServer.spec.ts
@@ -340,4 +340,34 @@ test.describe('PMM Client CLI tests for Percona Server Database', async () => {
       await output.outContains('Service removed.');
     }
   });
+
+  /**
+   * @link https://github.com/percona/pmm-qa/blob/main/pmm-tests/pmm-2-0-bats-tests/ps-specific-tests.bats#L303
+   */
+  test('run pmm-admin add mysql using metrics-mode as push', async ({ }) => {
+    let hosts = (await cli.exec(`sudo pmm-admin list | grep "MySQL" | awk -F" " '{print $3}'`))
+      .stdout.trim().split('\n').filter((item) => item.trim().length > 0);
+    let n = 1;
+    for (const host of hosts) {
+      const mysql_ip = host.split(':')[0];
+      const mysql_port = host.split(':')[1];
+      let output = await cli.exec(`sudo pmm-admin add mysql --query-source=perfschema --username=${MYSQL_USER} --password=${MYSQL_PASSWORD} --host=${mysql_ip} --port=${mysql_port} --service-name=mysql_${n++} --metrics-mode=push`);
+      await output.assertSuccess();
+      await output.outContains('MySQL Service added.');
+    }
+  });
+
+  /**
+   * @link https://github.com/percona/pmm-qa/blob/main/pmm-tests/pmm-2-0-bats-tests/ps-specific-tests.bats#L319
+   */
+  test('run pmm-admin remove mysql added via metrics-mode push', async ({ }) => {
+    let hosts = (await cli.exec(`sudo pmm-admin list | grep "MySQL" | grep "mysql_"`))
+      .stdout.trim().split('\n').filter((item) => item.trim().length > 0);
+    let n = 1;
+    for (const host of hosts) {
+      let output = await cli.exec(`sudo pmm-admin remove mysql mysql_${n++}`);
+      await output.assertSuccess();
+      await output.outContains('Service removed.');
+    }
+  });
 });
