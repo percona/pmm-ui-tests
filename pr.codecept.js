35a050130441b90dde2e71329cf764d68193a362
Peter Sirotnak <sirotnak.p@gmail.com>
2023-03-21 08:55:45 +0100

    Removes unused configuration

--- pr.codecept.js
+++ pr.codecept.js
@@ -123,8 +123,6 @@ exports.config = {
         options: {
           mochaFile: './tests/output/result.xml',
           jenkinsMode: true,
-          // verbose: false,
-          // steps: false,
         },
       },
     },
