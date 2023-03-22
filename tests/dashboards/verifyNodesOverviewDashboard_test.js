ded0d6eb92ce01c127d2f779c2146a8a89d7f02b
Peter Sirotnak <sirotnak.p@gmail.com>
2023-03-22 08:53:33 +0100

    Get names of the failed panels

--- tests/dashboards/verifyNodesOverviewDashboard_test.js
+++ tests/dashboards/verifyNodesOverviewDashboard_test.js
@@ -20,10 +20,7 @@ Scenario(
     await I.amOnPage(nodesOverviewPage.url);
     console.log(`PMM Server Version is: ${pmmVersion}`);
     if (pmmVersion >= 36 || pmmVersion === undefined) {
-      const networks = await I.verifyCommand('docker inspect pmm-server');
-
-      console.log(networks);
-      const output = await I.verifyCommand(`docker run -d \
+      await I.verifyCommand(`docker run -d \
         --name pmm-T1642-client \
         --network="pmm2-ui-tests_pmm-network"
         --add-host host.docker.internal:host-gateway \
@@ -36,24 +33,15 @@ Scenario(
         --env PMM_AGENT_SETUP_CUSTOM_LABELS="environment=dev" \
         --env PMM_AGENT_SETUP_REGION=EU \
         ${dockerVersion}`);
-
-      console.log(output);
-
       await I.wait(60);
-      const containers = await I.verifyCommand('sudo docker ps -a');
-
-      console.log(await inventoryAPI.listNodes());
-      console.log(containers);
-
-      const logs = await I.verifyCommand('sudo docker logs pmm-T1642-client');
-
-      console.log(logs);
       await I.click(nodesOverviewPage.buttons.refreshDashboard);
       await nodesOverviewPage.selectEnvironment('dev');
       const envName = await I.grabTextFromAll(nodesOverviewPage.buttons.environment);
 
       console.log(`Env Name is: ${envName}`);
       await I.assertContain(envName, 'dev', `The value of selected environment "${envName}" does not equal expected one "dev"}`);
+      await dashboardPage.verifyThereAreNoGraphsWithoutData(0);
+      await dashboardPage.verifyThereAreNoGraphsWithNA(0);
       await dashboardPage.waitForGraphsToHaveData(1, 300);
     } else {
       console.log('This functionality was added in PMM 2.36.0');
