b15ab9e51c45358a0561d4c37222842a28b65324
Vladimir Nazarov <vladimir.nazarov@simbirsoft.com>
2023-03-23 11:53:35 +0300

    Add fix from Puneet. Updated tags

--- tests/configuration/verifyPMMServerDisconnect_test.js
+++ tests/configuration/verifyPMMServerDisconnect_test.js
@@ -4,7 +4,10 @@ Feature('Pmm Server stability');
 const basePmmUrl = 'http://127.0.0.1:8180/';
 
 BeforeSuite(async ({ I }) => {
-  await I.verifyCommand('docker-compose -f docker-compose-disconnect.yml up -d');
+  await I.verifyCommand('docker-compose -f docker-compose-disconnect.yml up -d pmm-server-disconnect');
+  await I.verifyCommand('timeout 100 bash -c \'while [[ "$(curl -s -o /dev/null -w \'\'%{http_code}\'\' 127.0.0.1:8180/ping)" != "200" ]]; do sleep 5; done\' || false');
+  await I.verifyCommand('docker-compose -f docker-compose-disconnect.yml up -d pmm-client');
+  await I.verifyCommand('docker-compose -f docker-compose-disconnect.yml up -d mysql5.7');
   I.say(await I.verifyCommand('docker ps'));
   await I.wait(60);
   I.say(await I.verifyCommand('docker ps'));
@@ -18,22 +21,28 @@ Before(async ({ I }) => {
 });
 
 AfterSuite(async ({ I }) => {
-  // await I.verifyCommand('docker-compose -f docker-compose-disconnect.yml down -v');
+  await I.verifyCommand('docker-compose -f docker-compose-disconnect.yml down -v');
 });
 
 Scenario(
   '@PMM-T1442 Verify metrics are saved if PMM server was offline @cli',
-  async ({ I, dashboardPage, qanPage }) => {
+  async ({ I, dashboardPage }) => {
     const withCustomBaseUrl = (url) => `${basePmmUrl}${url}`;
 
-    await I.amOnPage(withCustomBaseUrl(dashboardPage.mySQLInstanceOverview.url));
+    await I.amOnPage(withCustomBaseUrl(dashboardPage.mysqlInstanceSummaryDashboard.url));
+    await dashboardPage.waitForDashboardOpened();
+    await dashboardPage.expandEachDashboardRow();
     I.wait(5);
     I.dontSeeElement(dashboardPage.fields.metricPanelNa('Services panel'));
     await I.verifyCommand('docker stop pmm-server-disconnect');
-    I.wait(180);
+    I.wait(600);
     await I.verifyCommand('docker start pmm-server-disconnect');
     I.wait(60);
-    await I.amOnPage(withCustomBaseUrl(`${dashboardPage.mySQLInstanceOverview.clearUrl}?orgId=1&from=now-2m&to=now-1m`));
+    await I.amOnPage(withCustomBaseUrl(`${dashboardPage.mysqlInstanceSummaryDashboard.clearUrl}?orgId=1&from=now-3m&to=now-1m`));
+    await dashboardPage.waitForDashboardOpened();
+    await dashboardPage.expandEachDashboardRow();
+    await dashboardPage.verifyThereAreNoGraphsWithNA(1);
+    await dashboardPage.verifyThereAreNoGraphsWithoutData(1);
     I.dontSeeElement(dashboardPage.fields.metricPanelNa('Services panel'));
   },
 );
@@ -50,7 +59,10 @@ xScenario(
     // todo: find a way to put pmm-agent offline without collateralAd
     I.wait(60);
     await I.amOnPage(withCustomBaseUrl(`${dashboardPage.mySQLInstanceOverview.clearUrl}?orgId=1&from=now-2m&to=now-1m`));
+    await dashboardPage.waitForDashboardOpened();
+    await dashboardPage.expandEachDashboardRow();
+    await dashboardPage.verifyThereAreNoGraphsWithNA(1);
+    await dashboardPage.verifyThereAreNoGraphsWithoutData(1);
     I.dontSeeElement(dashboardPage.fields.metricPanelNa('Services panel'));
-    I.wait(1000);
   },
 );
