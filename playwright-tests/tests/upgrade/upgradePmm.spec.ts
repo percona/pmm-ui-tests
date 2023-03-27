de3609a967137ee3b509ca355480ecead27603a0
Vadym Yarosh <vadym.yarosh@percona.com>
2023-03-27 22:02:08 +0200

    PMM-11842 conditional updated

--- playwright-tests/tests/upgrade/upgradePmm.spec.ts
+++ playwright-tests/tests/upgrade/upgradePmm.spec.ts
@@ -7,12 +7,10 @@ import {SettingProperty} from "@api/settingsApiV1";
 
 test.describe('General Upgrade PMM tests', async () => {
   test.describe.configure({ retries: 0 });
-    let pmmVersion: number;
+  let pmmVersion: number;
 
   test.beforeAll(async () => {
-    if (!pmmVersion) {
-      pmmVersion = (await api.serverV1.getPmmVersion()).minor;
-    }
+    pmmVersion = (await api.serverV1.getPmmVersion()).minor;
   });
 
   test.beforeEach(async ({ page }) => {
@@ -21,34 +19,31 @@ test.describe('General Upgrade PMM tests', async () => {
     await page.goto('');
   });
 
-  test('PMM-T288 Verify user can see Update widget before upgrade [critical] @pre-upgrade @ovf-upgrade @ami-upgrade @pmm-upgrade', async ({
-    page,
-  }) => {
-    const homeDashboard = new HomeDashboard(page);
-    await homeDashboard.pmmUpgrade.verifyUpgradeWidget();
+  test('PMM-T288 Verify user can see Update widget before upgrade [critical] @pre-upgrade @ovf-upgrade @ami-upgrade @pmm-upgrade',
+    async ({
+      page,
+    }) => {
+      const homeDashboard = new HomeDashboard(page);
+      await homeDashboard.pmmUpgrade.verifyUpgradeWidget();
   });
 
-  test('PMM-T3 Verify user is able to Upgrade PMM version [blocker] @pmm-upgrade @ovf-upgrade @ami-upgrade', async ({ page }) => {
-    const homeDashboard = new HomeDashboard(page);
-
-    await homeDashboard.upgradePMM();
+  test('PMM-T3 Verify user is able to Upgrade PMM version [blocker] @pmm-upgrade @ovf-upgrade @ami-upgrade',
+    async ({ page }) => {
+      const homeDashboard = new HomeDashboard(page);
+      await homeDashboard.upgradePMM();
   });
 
-  if (pmmVersion < 36) {
-      test('PMM-T1659 ensure BM is OFF by default before 2.36.0 version @pre-upgrade @ovf-upgrade @ami-upgrade',
-          async ({}) => {
-              const property = await api.settingsV1.getSettingsProperty(SettingProperty.bm);
-              await expect(property).toBeUndefined();
-          },
-      );
-  }
+  test('PMM-T1659 ensure BM is OFF by default before 2.36.0 version @pre-upgrade @ovf-upgrade @ami-upgrade',
+    async ({}) => {
+      test.skip(pmmVersion > 35, 'Test is for versions earlier 2.36.0');
+      const property = await api.settingsV1.getSettingsProperty(SettingProperty.bm);
+      await expect(property).toBeUndefined();
+  });
 
-  if (pmmVersion >= 36) {
-      test('PMM-T1659 Verify that BM is enabled by default after upgrade starting to 2.36.0 and newer @post-upgrade',
-          async ({}) => {
-              const property = await api.settingsV1.getSettingsProperty(SettingProperty.bm);
-              await expect(property).toBe(true);
-          },
-      );
-  }
+  test('PMM-T1659 Verify that BM is enabled by default after upgrade starting to 2.36.0 and newer @post-upgrade',
+    async ({}) => {
+      test.skip(pmmVersion <= 36, 'Test is for versions 2.36.0+');
+      const property = await api.settingsV1.getSettingsProperty(SettingProperty.bm);
+      await expect(property).toBe(true);
+  });
 });
