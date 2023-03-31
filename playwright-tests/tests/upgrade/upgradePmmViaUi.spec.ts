3c0e72b1a097cc015287f9c8caea06767a7f9684
Vadym Yarosh <vadym.yarosh@percona.com>
2023-03-31 18:56:02 +0200

    PMM-11905 clean up

--- playwright-tests/tests/upgrade/upgradePmmViaUi.spec.ts
+++ playwright-tests/tests/upgrade/upgradePmmViaUi.spec.ts
@@ -12,14 +12,14 @@ test.describe('Common Upgrade PMM tests', async () => {
     await page.goto('');
   });
 
-  test('PMM-T288 Verify user can see Update widget before upgrade [critical] @pre-upgrade @ovf-upgrade @ami-upgrade @pmm-upgrade', async ({
+  test('PMM-T288 Verify user can see Update widget before upgrade [critical] @pmm-upgrade', async ({
     page,
   }) => {
     const homeDashboard = new HomeDashboard(page);
     await homeDashboard.pmmUpgrade.verifyUpgradeWidget();
   });
 
-  test('PMM-T3 Verify user is able to Upgrade PMM version [blocker] @pmm-upgrade @ovf-upgrade @ami-upgrade', async ({ page }) => {
+  test('PMM-T3 Verify user is able to Upgrade PMM version [blocker] @pmm-upgrade', async ({ page }) => {
     const homeDashboard = new HomeDashboard(page);
 
     await homeDashboard.upgradePMM();
