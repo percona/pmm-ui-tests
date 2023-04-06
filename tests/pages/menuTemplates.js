6ebb9fe371f0ab6c9aff303265d74f6b395249bf
beatahandzelova <beata.handzelova.sde@gmail.com>
2023-04-06 21:33:49 +0200

    lint

--- tests/pages/menuTemplates.js
+++ tests/pages/menuTemplates.js
@@ -98,7 +98,7 @@ function MenuOption(menuName, label, locator, path, menuLevel = 1) {
     I.moveCursorTo(elemToClick);
 
     // special check for 'Advisors' and 'Backup' because elemToClick locator matches more than one element
-    if (label == 'Advisors' || label == 'Backup') {
+    if (label === 'Advisors' || label === 'Backup') {
       I.seeTextEquals(label, `.//ul[./@aria-label = '${label}']//div[text()="${label}"]`);
     }
     else {
