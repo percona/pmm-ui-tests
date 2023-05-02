af0267c0ba311deb5663db3e2268e4fad84abdc5
beatahandzelova <beata.handzelova.sde@gmail.com>
2023-05-02 10:57:34 +0200

    correct check_metric.sh path

--- cli/tests/mysql.spec.ts
+++ cli/tests/mysql.spec.ts
@@ -250,8 +250,8 @@ test.describe('PMM Client CLI tests for MySQL', async () => {
     let n = 1;
     for (const host of hosts) {
       await cli.exec('sleep 20');
-      await (await cli.exec('sudo chmod +x /home/runner/work/pmm-submodules/pmm-submodules/pmm-tests/pmm-2-0-bats-tests/check_metric.sh')).assertSuccess();
-      let output = await cli.exec(`sudo /home/runner/work/pmm-submodules/pmm-submodules/pmm-tests/pmm-2-0-bats-tests/check_metric.sh mysql_${n++} mysql_up 127.0.0.1 mysqld_exporter pmm mypass`);
+      await (await cli.exec('sudo chmod +x /srv/pmm-qa/pmm-tests/pmm-2-0-bats-tests/check_metric.sh')).assertSuccess();
+      let output = await cli.exec(`/srv/pmm-qa/pmm-tests/pmm-2-0-bats-tests/check_metric.sh mysql_${n++} mysql_up 127.0.0.1 mysqld_exporter pmm mypass`);
       await output.assertSuccess();
       await output.outContains('mysql_up 1');
     }
