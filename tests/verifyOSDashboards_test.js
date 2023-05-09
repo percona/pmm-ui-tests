dba29160cb0ad61d494c670adb59094290f0ad4f
beatahandzelova <beata.handzelova.sde@gmail.com>
2023-05-09 11:37:10 +0200

    add PMM-T1695

--- tests/verifyOSDashboards_test.js
+++ tests/verifyOSDashboards_test.js
@@ -1,4 +1,5 @@
 const nodes = new DataTable(['node-type', 'name']);
+const assert = require('assert');
 
 nodes.add(['pmm-client', 'ip']);
 
@@ -67,5 +68,40 @@ Scenario(
     I.forceClick(adminPage.fields.timePickerMenu);
     I.waitForVisible(adminPage.getTimeZoneSelector(timeZone), 30);
     I.seeElement(adminPage.getTimeZoneSelector(timeZone));
+  },
+);
+
+Scenario(
+  'PMM-T1695 Verify that user is able to filter OS / Node Compare dashboard by Node Name @nightly @dashboards',
+  async ({ I, dashboardPage, adminPage }) => {
+    const nodeName = await I.verifyCommand(`pmm-admin inventory list nodes | awk '$2 ~ /^ip-/ {print $2}'`);
+
+    I.amOnPage(dashboardPage.nodesCompareDashboard.url);
+    dashboardPage.waitForDashboardOpened();
+
+    // clear selections first
+    dashboardPage.expandFilters('Node Name');
+    I.click(dashboardPage.toggleAllValues);
+
+    await dashboardPage.applyFilter('Node Name', 'pmm-server');
+    await dashboardPage.expandEachDashboardRow();
+
+    let numOfPanels = await I.grabNumberOfVisibleElements(dashboardPage.panel);
+
+    assert.ok(numOfPanels === 28, `There should be 28 panels for one node but found "${numOfPanels}".`);
+
+    I.scrollTo(adminPage.fields.metricTitle);
+    I.dontSeeElement(dashboardPage.systemUptimePanel(nodeName));
+    I.seeElement(dashboardPage.systemUptimePanel('pmm-server'));
+
+    await dashboardPage.applyFilter('Node Name', nodeName);
+    I.click(adminPage.fields.metricTitle);
+
+    numOfPanels = await I.grabNumberOfVisibleElements(dashboardPage.panel);
+
+    assert.ok(numOfPanels === 50, `There should be 50 panels for two nodes but found "${numOfPanels}".`);
+
+    I.seeElement(dashboardPage.systemUptimePanel(nodeName));
+    I.seeElement(dashboardPage.systemUptimePanel('pmm-server'));
   },
 );
