2711881c2d644ac40058a4fe250a0c9fcc974ea9
Vladimir Nazarov <104006070+nazarovvi@users.noreply.github.com>
2023-05-11 14:09:59 +0300

    PMM-11897 Automate cases for rds_exporter and sql injections (#617)
    * Added case for maxScrapeSize
    
    * Fixed assertions for maxScrape
    
    * Fixed grep
    
    * Fixed grep config command
    
    * Fixed grep
    
    * Minor fixes
    
    * Minor fixes
    
    * Added cli tatg
    
    * Added test for SQL injections
    
    * Minor fixes
    
    * Minor fixes
    
    * Minor fixes
    
    * Minor fixes
    
    * Minor fixes
    
    * Minor fixes

--- tests/QAN/details_test.js
+++ tests/QAN/details_test.js
@@ -2,6 +2,11 @@ Feature('QAN details');
 
 const { adminPage } = inject();
 
+const querySources = new DataTable(['querySource']);
+
+querySources.add(['slowlog']);
+// querySources.add(['perfschema']);
+
 Before(async ({ I, qanPage, qanOverview }) => {
   await I.Authorize();
   I.amOnPage(I.buildUrlWithParams(qanPage.clearUrl, { from: 'now-1h' }));
@@ -140,5 +145,45 @@ Scenario(
     I.dontSeeElement(qanDetails.getTabLocator('Explain'));
     I.dontSeeElement(qanDetails.getTabLocator('Tables'));
     I.dontSeeElement(qanDetails.getTabLocator('Plan'));
+  },
+);
+
+Data(querySources).Scenario(
+  '@PMM-T1667 Verify that SQL injection is filtered in placeholders on QAN tab @qan',
+  async ({
+    I, qanPage, adminPage, qanOverview, qanDetails, qanFilters, current,
+  }) => {
+    const { querySource } = current;
+    const pmmFrameworkLoader = `bash ${adminPage.pathToFramework}`;
+    const port = '3307';
+    const containerName = `ps_${querySource}_8.0`;
+    const username = 'msandbox';
+
+    const password = 'msandbox';
+    const serviceName = `mysql_client_${querySource}`;
+    const mysqlCommandPrefix = `docker exec ${containerName} mysql -h 127.0.0.1 -u ${username} -p${password} test --port ${port} -e`;
+
+    await I.verifyCommand(`export PS_CONTAINER=${containerName} ; ${pmmFrameworkLoader} --setup-pmm-ps-integration --pmm2 --query-source=${querySource} --ps-version=8.0`);
+    await I.verifyCommand(`docker exec ${containerName} pmm-admin add mysql --username=${username} --password=${password} --port=${port} --query-source=${querySource} --disable-queryexamples ${serviceName}`);
+
+    await I.verifyCommand(`${mysqlCommandPrefix} "CREATE TABLE cities (ID int, Name varchar(255), Country varchar(255));"`);
+    await I.verifyCommand(`${mysqlCommandPrefix} "INSERT INTO cities VALUES (1,'New York','USA'),(2,'Atlanta','USA'), (3,'Paris','France');"`);
+    await I.verifyCommand(`${mysqlCommandPrefix} "SELECT * FROM test.cities WHERE ID = 1;"`);
+
+    I.amOnPage(I.buildUrlWithParams(qanPage.clearUrl, { from: 'now-15m' }));
+    qanOverview.waitForOverviewLoaded();
+    qanOverview.waitForOverviewLoaded();
+    await qanOverview.searchByValue('SELECT * FROM test.cities WHERE ID');
+    await I.asyncWaitFor(async () => {
+      I.click(qanOverview.buttons.refresh);
+
+      return !(await I.grabNumberOfVisibleElements(qanOverview.elements.noResultTableText));
+    }, 300);
+    qanOverview.selectRowByText('select * from test.cities where id = ?');
+    I.click(qanDetails.getTabLocator('Explain'));
+    qanFilters.waitForFiltersToLoad();
+    I.fillField(qanDetails.elements.firstPlaceholder, '1');
+    I.waitForVisible(qanDetails.elements.explainTable);
+    I.assertEqual(await I.grabNumberOfVisibleElements(locate('$query-analytics-details').find('$table-row')), 1, 'Explain is expected to have one row in a table, but found more');
   },
 );
