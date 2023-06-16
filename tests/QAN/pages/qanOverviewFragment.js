9c9b9aa0b0583203e7f4d9a0179a1143535e5ee5
Vladimir Nazarov <104006070+nazarovvi@users.noreply.github.com>
2023-06-16 11:06:06 +0300

    PMM-12190 Automate tasks from backlog (#631)
    * Minor fixes
    
    * Minor fixes
    
    * Minor fixes
    
    * Minor fixes
    
    * Minor fixes
    
    * Minor fixes
    
    * move setup to docker compose
    
    * lint
    
    * add docker compose down
    
    * add dashboards tag
    
    * replace seeElement with waitForVisible due to load
    
    * cleanup
    
    * remove double quotes
    
    ---------
    
    Co-authored-by: beatahandzelova <beata.handzelova.sde@gmail.com>
    Co-authored-by: Beata Handzelova <64002801+beatahandzelova@users.noreply.github.com>

--- tests/QAN/pages/qanOverviewFragment.js
+++ tests/QAN/pages/qanOverviewFragment.js
@@ -36,6 +36,7 @@ module.exports = {
     clipboardLink: locate(I.getPopUpLocator()).find('span'),
     mainMetricDropdown: locate('$group-by'),
     selectedMainMetric: locate('$group-by').find('//div[@class="ant-select-selection-selected-value"]'),
+    tooltipContent: locate('div.tippy-content'),
   },
   messages: {
     noResultTableText: 'No queries available for this combination of filters in the selected time frame',
@@ -51,6 +52,7 @@ module.exports = {
   getMetricLocatorInDropdown: (name) => locate('[role="listbox"]').find(`[label='${name}']`),
 
   getCellValueLocator: (rowNumber, columnNumber) => `div.tr-${rowNumber} > div:nth-child(${columnNumber + 2}) span > div > span`,
+  getLoadLocator: (rowNumber) => `div.tr-${rowNumber} .td canvas`,
 
   // using below to concatenate locators
   getMetricSortingLocator: (columnNumber) => `(//a[@data-testid='sort-by-control'])[${columnNumber}]`,
