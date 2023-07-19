55e421534d477faa90098fdae4077760723bd522
saikumar-vs <saikumar.vs@percona.com>
2023-07-19 11:32:34 +0530

    Revert "PMM-T2282 Verfied Alerting is able to monitor for "PMM Agent Down""
    This reverts commit 19bf2db95c98c93662feaf63d0e6bbc6a11c4a28.

--- tests/ia/alertRules_test.js
+++ tests/ia/alertRules_test.js
@@ -102,37 +102,10 @@ Scenario(
     // FIXME: unskip after https://jira.percona.com/browse/PMM-11399 is fixed
     // I.verifyPopUpMessage(alertRulesPage.messages.successRuleCreate(newRule.ruleName));
     alertRulesPage.verifyRuleList(newRule.folder, newRule.ruleName);
-    await alertRulesPage.verifyRuleState('Normal',60);
+    I.seeTextEquals('Normal', alertRulesPage.elements.ruleState);
     await rulesAPI.removeAlertRule(newRule.folder);
   },
 ).retry(1);
-
-Scenario(
-  'PMM-T2282 Verfied Alerting is able to monitor for "PMM Agent Down" @ia @alerting-fb',
-  async ({ I, alertRulesPage, rulesAPI }) => {
-    const rule = page.rules[29];
-    const newRule = page.rules[30];
-
-    alertRulesPage.openAlertRulesTab();
-    I.waitForEnabled(alertRulesPage.buttons.newAlertRule, 10);
-    I.click(alertRulesPage.buttons.newAlertRule);
-    await alertRulesPage.fillPerconaAlert(rule,newRule);
-    I.click(alertRulesPage.buttons.saveAndExit);
-    // FIXME: unskip after https://jira.percona.com/browse/PMM-11399 is fixed
-    // I.verifyPopUpMessage(alertRulesPage.messages.successRuleCreate(newRule.ruleName));
-    await alertRulesPage.verifyRuleList(newRule.folder, newRule.ruleName);
-    await I.verifyCommand('docker pause ms_pmm_8.0');
-    await alertRulesPage.verifyRuleState('Pending',180);
-    //await I.waitForText('Pending', 180, alertRulesPage.elements.ruleState1);
-    await alertRulesPage.verifyRuleState('Firing',180);
-    //await I.waitForText('Firing', 180, alertRulesPage.elements.ruleState2);
-    await I.verifyCommand('docker unpause ms_pmm_8.0');
-    //await I.waitForText('Normal', 180, alertRulesPage.elements.ruleState3);
-    await alertRulesPage.verifyRuleState('Normal',180);
-    await rulesAPI.removeAlertRule(newRule.folder);
-  },
-).retry(1);
-
 
 // TODO: check ovf failure
 Scenario(
