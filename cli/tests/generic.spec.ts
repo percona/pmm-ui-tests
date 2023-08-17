66709d8e0caf221d928eeb642122853ed95a53dc
saikumar-vs <112166980+saikumar-vs@users.noreply.github.com>
2023-08-17 16:51:27 +0530

    T1258 : Verify pmm-admin status shows node name (#668)
    * T1258 : Verify pmm-admin status shows node name

---
+++ cli/tests/generic.spec.ts
@@ -0,0 +1,12 @@
+import { test, expect } from '@playwright/test';
+import * as cli from '@helpers/cliHelper';
+
+ /**
+   * T1258 : Verify pmm-admin status shows node name
+   */
+test('run pmm-status and grep NodeName', async ({}) => {
+  const output = await cli.exec('sudo pmm-admin status');
+  await output.assertSuccess();
+  await output.outContains('Node name: client_container_');
+});
+
