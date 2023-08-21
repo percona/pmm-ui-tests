d5c24bbd96bd15ab7b3204cbcf6682954e83eb27
yurkovychv <vasyl.yurkovych@percona.com>
2023-08-21 12:30:46 +0300

    PMM-7 default scrape size test

--- cli/tests/generic.spec.ts
+++ cli/tests/generic.spec.ts
@@ -9,17 +9,3 @@ test('run pmm-status and grep NodeName', async ({}) => {
   await output.assertSuccess();
   await output.outContains('Node name: client_container_');
 });
-
-test('scrape interval default value', async ({page}) => {
-    const expectedScrapeSize = '64';
-
-    await (await cli.exec(`docker-compose -f test-setup/docker-compose-scrape-intervals.yml up -d`)).assertSuccess();
-    await (await cli.exec('sudo pmm-admin config --force \'--server-url=https://admin:admin@0.0.0.0:1443\' --server-insecure-tls 127.0.0.1')).assertSuccess()
-
-    const scrapeSizeContainer = await cli.exec('docker logs pmm-client-scrape-intervals 2>&1 | grep \'promscrape.maxScrapeSize.*vm_agent\' | tail -1');
-    await scrapeSizeContainer.outContains(`promscrape.maxScrapeSize=\\\"${expectedScrapeSize}MiB\\\"`)
-
-    const scrapeSizeTarball = await cli.exec('ps aux | grep -v \'grep\' | grep \'vm_agent\' | tail -1 | grep -o \'promscrape.maxScrapeSize.*vm_agent\' | tail -1')
-    await scrapeSizeTarball.outContains(`promscrape.maxScrapeSize=\\\"${expectedScrapeSize}MiB\\\"`)
-});
-
