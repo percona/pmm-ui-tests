b27911047acbedf20cac92ef1f7e1f46b4cc99fb
yurkovychv <vasyl.yurkovych@percona.com>
2023-08-21 11:35:29 +0300

    PMM-7 default scrape size test

--- cli/tests/generic.spec.ts
+++ cli/tests/generic.spec.ts
@@ -13,7 +13,7 @@ test('run pmm-status and grep NodeName', async ({}) => {
 test('scrape interval default value', async ({page}) => {
     const expectedScrapeSize = '64';
 
-    await (await cli.exec(`docker-compose -f cli/test-setup/docker-compose-scrape-intervals.yml up -d`)).assertSuccess();
+    await (await cli.exec(`docker-compose -f test-setup/docker-compose-scrape-intervals.yml up -d`)).assertSuccess();
     await (await cli.exec('sudo pmm-admin config --force \'--server-url=https://admin:admin@0.0.0.0:1443\' --server-insecure-tls 127.0.0.1')).assertSuccess()
 
     const scrapeSizeContainer = await cli.exec('docker logs pmm-client-scrape-intervals 2>&1 | grep \'promscrape.maxScrapeSize.*vm_agent\' | tail -1');
