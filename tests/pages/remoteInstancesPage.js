7c2ce15d8eb001e7d76311307aec3e3209988199
Saikumar <saikumar.vs@percona.com>
2023-08-22 14:52:16 +0530

    Fixes

--- tests/pages/remoteInstancesPage.js
+++ tests/pages/remoteInstancesPage.js
@@ -109,7 +109,7 @@ module.exports = {
     database: '$database-text-input',
     disableBasicMetrics: '//input[@id="input-disable_basic_metrics-id"]/following-sibling::*[2]',
     disableEnhancedMetrics: '//input[@id="input-disable_enhanced_metrics-id"]/following-sibling::*[2]',
-    discoverBtn: '$credentials-search-button',
+    discoverBtn: '//span[normalize-space()=\'Discover\']',
     discoveryResults: 'tbody[role="rowgroup"]',
     doNotTrack: locate('label').withText('Don\'t track'),
     environment: '$environment-text-input',
