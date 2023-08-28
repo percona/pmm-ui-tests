6d479ff9a7964a2c78df8cddf1c1ba26d13db248
Vadim Yarosh <vadym.yarosh@percona.com>
2023-08-28 16:57:23 +0200

    PMM-7 removed payloads log (#678)

--- playwright-tests/api/service-now.api.ts
+++ playwright-tests/api/service-now.api.ts
@@ -5,15 +5,13 @@ import constants from '@helpers/constants';
 import { expect } from '@playwright/test';
 
 const apiConfig: AxiosRequestConfig = {
-  auth: {
-    username: constants.serviceNow.username, password: constants.serviceNow.password,
-  },
+  auth: { username: constants.serviceNow.username, password: constants.serviceNow.password },
   httpsAgent: new https.Agent({ rejectUnauthorized: false }),
 };
 
 export const serviceNowApi = {
   async getServiceNowCredentials(): Promise<ServiceNowResponse> {
-    console.log(`POST: ${constants.serviceNow.devUrl}\nPayload: ${JSON.stringify({})}`);
+    console.log(`POST: ${constants.serviceNow.devUrl}`);
     const response = await axios.post(constants.serviceNow.devUrl, {}, apiConfig);
     expect(response.status, `Expected to be 200: ${response.status} ${response.statusText}`).toEqual(200);
     console.log(`Status: ${response.status} ${response.statusText}`);
