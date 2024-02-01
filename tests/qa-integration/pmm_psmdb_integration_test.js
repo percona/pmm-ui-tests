7511ad323492c87103ed790e2fb5dca88ea08dcd
Saikumar <saikumar.vs@percona.com>
2024-02-01 07:49:44 +0530

    - Updates

--- tests/qa-integration/pmm_psmdb_integration_test.js
+++ tests/qa-integration/pmm_psmdb_integration_test.js
@@ -182,8 +182,6 @@ Scenario(
       I.amOnPage(`${dashboardPage.mongodbReplicaSetSummaryDashboard.url}&var-replset=rs1`);
       dashboardPage.waitForDashboardOpened();
 
-      // This is due to some instances with many services take filter to load
-      I.wait(3);
       const serviceName = "mongodb_rs2_1";
       // Check service name from Replication Lag field in UI
       const replLagService = `(//a[@data-testid='data-testid dashboard-row-title-Replication Lag']/following::a[contains(text(),'${serviceName}')])`;
@@ -193,5 +191,5 @@ Scenario(
       const replLagValue = `(//a[@data-testid='data-testid dashboard-row-title-Replication Lag']/following::a[contains(text(),'${serviceName}')]/following::td[contains(text(),' year')])[1]`;
       await I.dontSeeElement(replLagValue,180);
     },
-).retry(2);
+).retry(3);
 
