44cb6c9d0600a2800e68584c8d94b3a2dc09b4d3
Saikumar <saikumar.vs@percona.com>
2024-02-05 16:48:53 +0530

    - Updates

--- cli/tests/mongoDb-psmdb-new.spec.ts
+++ cli/tests/mongoDb-psmdb-new.spec.ts
@@ -5,8 +5,7 @@ const version = process.env.PSMDB_VERSION ? `${process.env.PSMDB_VERSION}` : '4.
 const shard_container_name = `psmdb_pmm_${version}_sharded`;
 test.describe('Percona Server MongoDB (PSMDB) CLI tests New', async () => {
   test('PMM-T1853 Collect Data about Sharded collections in MongoDB', async ({}) => {
-    console.log(shard_container_name);
-    const serviceName= 'mongo_shard_test';
+    const serviceName= 'mongo_shards_test';
     const hosts = (await cli.exec(`docker exec '${shard_container_name}' pmm-admin list | grep "mongodb_shraded_node" | awk -F" " \'{print $3}\'`))
         .stdout.trim().split('\n').filter((item) => item.trim().length > 0);
     for (const host of hosts) {
@@ -23,9 +22,9 @@ test.describe('Percona Server MongoDB (PSMDB) CLI tests New', async () => {
       const expectedValue = 'mongodb_shards_collection_chunks_count';
       expect(metrics, `Scraped metrics do not contain ${expectedValue}!`).toContain(expectedValue);
 
-      const output1 = await cli.exec(`docker exec ${shard_container_name} pmm-admin remove mongodb ${serviceName}`);
-      await output1.assertSuccess();
-      await output1.outContains('Service removed.');
+      const results = await cli.exec(`docker exec ${shard_container_name} pmm-admin remove mongodb ${serviceName}`);
+      await results.assertSuccess();
+      await results.outContains('Service removed.');
     }
   });
 });
