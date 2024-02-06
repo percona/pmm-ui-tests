0f7b72fe6b0b0c41756e47009030298a92834b6e
Saikumar <saikumar.vs@percona.com>
2024-02-06 17:48:05 +0530

    - Updates

--- cli/tests/mongoDb-psmdb.spec.ts
+++ cli/tests/mongoDb-psmdb.spec.ts
@@ -8,6 +8,9 @@ import * as cli from '@helpers/cli-helper';
 const MONGO_USERNAME = 'pmm_mongodb';
 const MONGO_PASSWORD = 'GRgrO9301RuF';
 
+const version = process.env.PSMDB_VERSION ? `${process.env.PSMDB_VERSION}` : '4.4';
+const shard_container_name = `psmdb_pmm_${version}_sharded`;
+
 test.describe('Percona Server MongoDB (PSMDB) CLI tests ', async () => {
   /**
    * @link https://github.com/percona/pmm-qa/blob/main/pmm-tests/pmm-2-0-bats-tests/modb-tests.bats#L12
@@ -255,5 +258,32 @@ test.describe('Percona Server MongoDB (PSMDB) CLI tests ', async () => {
       await output.assertSuccess();
       await output.outContains('Service removed.');
     }
+  });
+
+  /*
+  This test uses pmm-framework setup with pure docker environment.
+  */
+  test('PMM-T1853 Collect Data about Sharded collections in MongoDB', async ({}) => {
+    const serviceName= 'mongo_shards_test';
+    const hosts = (await cli.exec(`docker exec '${shard_container_name}' pmm-admin list | grep "mongodb_shraded_node" | awk -F" " \'{print $3}\'`))
+        .stdout.trim().split('\n').filter((item) => item.trim().length > 0);
+      for (const host of hosts) {
+        const ip = host.split(':')[0];
+        const port = host.split(':')[1];
+        const output = await cli.exec(`docker exec ${shard_container_name} pmm-admin add mongodb --host=${ip} --port=${port} --service-name=${serviceName} --enable-all-collectors --agent-password='mypass'`);
+        await output.assertSuccess();
+        await output.outContains('MongoDB Service added');
+
+        // Require to wait for Service to be Added and Running.
+        await new Promise(resolve => setTimeout(resolve, 5000));
+
+        const metrics = await cli.getMetrics(serviceName, 'pmm', 'mypass', shard_container_name);
+        const expectedValue = 'mongodb_shards_collection_chunks_count';
+        expect(metrics, `Scraped metrics do not contain ${expectedValue}!`).toContain(expectedValue);
+
+        const results = await cli.exec(`docker exec ${shard_container_name} pmm-admin remove mongodb ${serviceName}`);
+        await results.assertSuccess();
+        await results.outContains('Service removed.');
+      }
   });
 });
