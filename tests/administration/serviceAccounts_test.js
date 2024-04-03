5f04035d3922037fb12b5dfecc7311e562e1a491
Peter Sirotnak <sirotnak.p@gmail.com>
2024-04-03 11:10:56 +0200

    PMM-13040: Fixes command to setup agent using service token

--- tests/administration/serviceAccounts_test.js
+++ tests/administration/serviceAccounts_test.js
@@ -25,7 +25,7 @@ Scenario('@PMM-T1883', async ({ I, serviceAccountsPage, inventoryAPI }) => {
   await I.amOnPage('');
   await I.wait(30);
   await I.saveScreenshot('debug1');
-  const response = await I.verifyCommand(`pmm-agent setup --server-username=service_account --server-password=${tokenValue} --server-address=18.219.18.30 --server-insecure-tls`);
+  const response = await I.verifyCommand(`pmm-agent setup --server-username=service_token --server-password=${tokenValue} --server-address=18.219.18.30 --server-insecure-tls`);
 
   console.log(response);
 });
