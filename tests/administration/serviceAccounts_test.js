6f47c705217405d28b85154ad4a8cb96d290b1e6
Peter Sirotnak <sirotnak.p@gmail.com>
2024-04-03 11:06:39 +0200

    PMM-13040: Save screenshot

--- tests/administration/serviceAccounts_test.js
+++ tests/administration/serviceAccounts_test.js
@@ -20,7 +20,7 @@ Scenario('@PMM-T1883', async ({ I, serviceAccountsPage, inventoryAPI }) => {
   await inventoryAPI.deleteNode(oldAgentId, true);
   await I.amOnPage('');
   await I.wait(30);
-  await I.saveScreenshot();
+  await I.saveScreenshot('debug1');
   const response = await I.verifyCommand(`pmm-agent setup --server-username=service_account --server-password=${tokenValue} --server-address=18.219.18.30 --server-insecure-tls`);
 
   console.log(response);
