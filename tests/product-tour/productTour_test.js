ffb7188c7676f20a9cbe63c219522225cde8a5fe
Peter Sirotnak <sirotnak.p@gmail.com>
2024-04-03 10:57:13 +0200

    PMM-12847: Scenario .only for testing purposes

--- tests/product-tour/productTour_test.js
+++ tests/product-tour/productTour_test.js
@@ -6,7 +6,7 @@ Before(async ({ I }) => {
   await I.sendGetRequest('v1/user', { Authorization: `Basic ${await I.getAuth()}` });
 });
 
-Scenario('PMM-T1879 Verify that product tour dialog is displayed after check later button pressed. @grafana-pr', async ({ I, homePage }) => {
+Scenario.only('PMM-T1879 Verify that product tour dialog is displayed after check later button pressed. @grafana-pr', async ({ I, homePage }) => {
   await I.amOnPage('');
   await I.waitForElement(homePage.productTour.laterButton);
   await I.click(homePage.productTour.laterButton);
@@ -16,7 +16,7 @@ Scenario('PMM-T1879 Verify that product tour dialog is displayed after check lat
   await I.waitForElement(homePage.productTour.productTourModal, 10);
 });
 
-Scenario('PMM-T1880 Verify that product tour dialog is displayed after closing @grafana-pr', async ({ I, homePage }) => {
+Scenario.only('PMM-T1880 Verify that product tour dialog is displayed after closing @grafana-pr', async ({ I, homePage }) => {
   await I.amOnPage('');
   await I.waitForElement(homePage.productTour.closeButton);
   await I.click(homePage.productTour.closeButton);
@@ -24,7 +24,7 @@ Scenario('PMM-T1880 Verify that product tour dialog is displayed after closing @
   await I.waitForElement(homePage.productTour.productTourModal, 10);
 });
 
-Scenario('PMM-T1881 Verify that product tour dialog contains all the components. @grafana-pr', async ({ I, homePage }) => {
+Scenario.only('PMM-T1881 Verify that product tour dialog contains all the components. @grafana-pr', async ({ I, homePage }) => {
   await I.amOnPage('');
   await I.waitForElement(homePage.productTour.startTourButton);
   await I.click(homePage.productTour.startTourButton);
@@ -32,7 +32,7 @@ Scenario('PMM-T1881 Verify that product tour dialog contains all the components.
   await I.waitForDetached(homePage.productTour.productTourModal);
 });
 
-Scenario('PMM-T1882 Verify that product tour dialog is not displayed after skipping @grafana-pr', async ({ I, homePage }) => {
+Scenario.only('PMM-T1882 Verify that product tour dialog is not displayed after skipping @grafana-pr', async ({ I, homePage }) => {
   await I.enableProductTour();
   await I.amOnPage('');
 
