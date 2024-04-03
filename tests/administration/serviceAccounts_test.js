9ceb30a686ac2fc2506fe64417c31aaae0fd1964
Peter Sirotnak <sirotnak.p@gmail.com>
2024-04-03 15:09:20 +0200

    PMM-13040: changes for verification that nodes dashboard contains data

--- tests/administration/serviceAccounts_test.js
+++ tests/administration/serviceAccounts_test.js
@@ -1,31 +1,29 @@
-let config = require('codeceptjs').config.get();
-
 Feature('Service Accounts tests');
 
 Before(async ({ I }) => {
   await I.Authorize();
 });
 
-Scenario('@PMM-T1883', async ({ I, serviceAccountsPage, inventoryAPI }) => {
+Scenario('@PMM-T1883', async ({
+  I, serviceAccountsPage, dashboardPage, inventoryAPI, nodesOverviewPage,
+}) => {
   await I.amOnPage(serviceAccountsPage.url);
   const pmmServerUrl = (await I.grabCurrentUrl()).replace(/^(-)|[^0-9.,]+/g, '$1');
 
-  console.log(pmmServerUrl);
   await serviceAccountsPage.createServiceAccount(`service_account_${Date.now()}`, 'Admin');
   const tokenValue = await serviceAccountsPage.createServiceAccountToken(`token_name_${Date.now()}`);
 
   const oldAgentId = await I.verifyCommand('pmm-admin status | grep "Node ID" | awk -F " " \'{ print $4 }\'');
 
   if (oldAgentId) {
-    console.log(oldAgentId);
-    console.log(`Length of old agent id is: ${oldAgentId.length}`);
     await inventoryAPI.deleteNode(oldAgentId, true);
   }
 
-  await I.amOnPage('');
-  await I.wait(30);
-  await I.saveScreenshot('debug1');
-  const response = await I.verifyCommand(`pmm-agent setup --server-username=service_token --server-password=${tokenValue} --server-address=${pmmServerUrl} --server-insecure-tls --config-file=/home/ec2-user/workspace/pmm3-aws-staging-start/pmm/config/pmm-agent.yaml --paths-base=/home/ec2-user/workspace/pmm3-aws-staging-start/pmm`);
-
-  console.log(response);
+  await I.verifyCommand(`pmm-agent setup --server-username=service_token --server-password=${tokenValue} --server-address=${pmmServerUrl} --server-insecure-tls --config-file=/home/ec2-user/workspace/pmm3-aws-staging-start/pmm/config/pmm-agent.yaml --paths-base=/home/ec2-user/workspace/pmm3-aws-staging-start/pmm`);
+  await I.wait(60);
+  await I.amOnPage(nodesOverviewPage.url);
+  await dashboardPage.waitForDashboardOpened();
+  await dashboardPage.expandEachDashboardRow();
+  await dashboardPage.verifyThereAreNoGraphsWithNA(1);
+  await dashboardPage.verifyThereAreNoGraphsWithoutData(19);
 });
