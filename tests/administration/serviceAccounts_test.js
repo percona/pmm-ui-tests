f76c34485d6a4ba2cec360bc3f69fbb6284c5b75
Peter Sirotnak <sirotnak.p@gmail.com>
2024-05-03 12:53:43 +0200

    Pmm 13040 fix2 (#776)
    * PMM-13040: Move connecting mysql db
    
    * PMM-13040: use docker pmm client
    
    * PMM-13040: use docker pmm client
    
    * PMM-13040: Fixes port
    
    * PMM-13040: fix for docker container pmm client
    
    * PMM-13040: fix for docker container pmm client
    
    * PMM-13040: Adds retry.
    
    * PMM-13040: assign username in the test
    
    * PMM-13040: new method waitForGraphsToHaveData

--- tests/administration/serviceAccounts_test.js
+++ tests/administration/serviceAccounts_test.js
@@ -4,29 +4,27 @@ Before(async ({ I }) => {
   await I.Authorize();
 });
 
-const serviceAccountUsername = `service_account_${Date.now()}`;
+let serviceAccountUsername = '';
 const newServiceName = 'mysql_service_service_token1';
 
 Scenario('PMM-T1883 Configuring pmm-agent to use service account @service-account', async ({
   I, codeceptjsConfig, serviceAccountsPage, dashboardPage, inventoryAPI, nodesOverviewPage, credentials,
 }) => {
+  serviceAccountUsername = `service_account_${Date.now()}`;
   await I.amOnPage(serviceAccountsPage.url);
   const pmmServerUrl = new URL(codeceptjsConfig.config.helpers.Playwright.url).hostname;
 
   await serviceAccountsPage.createServiceAccount(serviceAccountUsername, 'Admin');
 
   const tokenValue = await serviceAccountsPage.createServiceAccountToken(`token_name_${Date.now()}`);
-  const oldNodeId = await I.verifyCommand('pmm-admin status | grep "Node ID" | awk -F " " \'{ print $4 }\'');
-  const pmmAgentConfigLocation = (await I.verifyCommand('sudo find / -name pmm-agent.yaml 2>/dev/null'))
-    .split('\n')
-    .find((agentLocation) => agentLocation.includes('/home/') || (agentLocation.includes('/usr/local/config/') || (agentLocation.includes('/usr/local/percona/pmm/') && !agentLocation.includes('docker'))));
+  const oldNodeId = await I.verifyCommand('sudo docker exec ps_pmm_8.0 pmm-admin status | grep "Node ID" | awk -F " " \'{ print $4 }\'');
 
   if (oldNodeId) {
     await inventoryAPI.deleteNode(oldNodeId, true);
   }
 
-  await I.verifyCommand(`sudo -E env "PATH=$PATH" pmm-agent setup --server-username=service_token --server-password=${tokenValue} --server-address=${pmmServerUrl} --server-insecure-tls --config-file=${pmmAgentConfigLocation}`);
-  await I.verifyCommand(`pmm-admin add mysql --username=msandbox --password=msandbox --host=127.0.0.1  --port=3317 --service-name=${newServiceName}`);
+  await I.verifyCommand(`sudo docker exec ps_pmm_8.0 pmm-agent setup --server-username=service_token --server-password=${tokenValue} --server-address=${pmmServerUrl} --server-insecure-tls --config-file=/usr/local/percona/pmm/config/pmm-agent.yaml`);
+  await I.verifyCommand(`sudo docker exec ps_pmm_8.0 pmm-admin add mysql --username=msandbox --password=msandbox --host=127.0.0.1  --port=3307 --service-name=${newServiceName}`);
   await I.wait(60);
   const nodeName = (await inventoryAPI.getAllNodes()).generic.find((node) => node.node_name !== 'pmm-server').node_name;
   const nodesUrl = I.buildUrlWithParams(nodesOverviewPage.url, {
@@ -38,32 +36,31 @@ Scenario('PMM-T1883 Configuring pmm-agent to use service account @service-accoun
   await I.amOnPage(nodesUrl);
   await dashboardPage.waitForDashboardOpened();
   await dashboardPage.expandEachDashboardRow();
-  await dashboardPage.verifyThereAreNoGraphsWithoutData(19);
+  await dashboardPage.waitForGraphsToHaveData(19, 300);
 
   await I.verifyCommand('sudo docker restart pmm-server');
   await I.wait(60);
   await I.amOnPage(nodesOverviewPage.url);
   await dashboardPage.waitForDashboardOpened();
   await dashboardPage.expandEachDashboardRow();
-  await dashboardPage.verifyThereAreNoGraphsWithoutData(19);
+  await dashboardPage.waitForGraphsToHaveData(19, 300);
 
-  await I.wait(60);
   const url = I.buildUrlWithParams(dashboardPage.mySQLInstanceOverview.clearUrl, {
-    from: 'now-30s',
+    from: 'now-1m',
     to: 'now',
     service_name: newServiceName,
   });
 
   await I.amOnPage(url);
   await I.wait(5);
-  await dashboardPage.verifyThereAreNoGraphsWithoutData(1);
-});
+  await dashboardPage.waitForGraphsToHaveData(1, 300);
+}).retry(1);
 
 Scenario('PMM-T1884 Verify disabling service account @service-account', async ({ I, serviceAccountsPage }) => {
   await I.amOnPage(serviceAccountsPage.url);
   await serviceAccountsPage.disableServiceAccount(serviceAccountUsername);
   await I.wait(10);
-  const responseDisabled = await I.verifyCommand('sudo -E env "PATH=$PATH" pmm-admin list', '', 'fail');
+  const responseDisabled = await I.verifyCommand('sudo docker exec ps_pmm_8.0 pmm-admin list', '', 'fail');
   const expectedDisabledMessage = 'Unauthorized. Please check username and password.';
 
   I.assertEqual(
