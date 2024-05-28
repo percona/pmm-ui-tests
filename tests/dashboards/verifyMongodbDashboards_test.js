c5a8a01c80431e9a805e3b9ff81eeeaf87de5afb
Peter Sirotnak <sirotnak.p@gmail.com>
2024-05-28 12:57:01 +0200

    PMM-9403: Fix echo

--- tests/dashboards/verifyMongodbDashboards_test.js
+++ tests/dashboards/verifyMongodbDashboards_test.js
@@ -43,7 +43,7 @@ Scenario(
 );
 
 Scenario('PMM-T1889 Verify Mongo replication lag graph shows correct info @nightly @dashboards', async ({ I, dashboardPage }) => {
-  const testConfigFile = '"c = rs.conf() \\nc.members[2].secondaryDelaySecs = 10\\nc.members[2].priority = 0 \\nc.members[2].hidden = true \\nrs.reconfig(c)';
+  const testConfigFile = 'c = rs.conf() \\nc.members[2].secondaryDelaySecs = 10\\nc.members[2].priority = 0 \\nc.members[2].hidden = true \\nrs.reconfig(c)';
 
   await I.verifyCommand(`echo -e "${testConfigFile}" > test.js`);
   await I.verifyCommand('sudo docker exec -it rs101 mongo "mongodb://root:root@localhost/?replicaSet=rs" < test.js');
