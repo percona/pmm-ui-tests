a2f7b13a37a09d0b8286495ac4d97b31abe32858
saikumar-vs <112166980+saikumar-vs@users.noreply.github.com>
2024-06-05 14:28:12 +0530

    Fix testcase to use tls user (#792)
    * Fix testcase to use tls user

--- tests/verifyTLSMySQLRemoteInstance_test.js
+++ tests/verifyTLSMySQLRemoteInstance_test.js
@@ -320,7 +320,7 @@ Data(instances).Scenario(
     } = current;
     const serviceName = `TLS_mysql_${version}`;
     const responseMessage = 'MySQL Service added';
-    const command = `docker exec ${container} pmm-admin add mysql --username=pmm --password=pmm --port=3306 --query-source=perfschema --tls --tls-skip-verify --tls-ca=/var/lib/mysql/ca.pem --tls-cert=/var/lib/mysql/client-cert.pem --tls-key=/var/lib/mysql/client-key.pem ${serviceName}`;
+    const command = `docker exec ${container} pmm-admin add mysql --username=pmm_tls --port=3306 --query-source=perfschema --tls --tls-skip-verify --tls-ca=/var/lib/mysql/ca.pem --tls-cert=/var/lib/mysql/client-cert.pem --tls-key=/var/lib/mysql/client-key.pem ${serviceName}`;
     const output = await I.verifyCommand(command);
 
     I.assertTrue(output.includes(responseMessage), `The ${command} was supposed to return ${responseMessage} but actually got ${output}`);
