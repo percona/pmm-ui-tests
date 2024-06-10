f46c121d1c37d2c60b2ec2868a01e833a1970aba
Peter Sirotnak <sirotnak.p@gmail.com>
2024-06-10 15:57:39 +0200

    PMM-9403: Fix test

--- tests/dashboards/verifyMongodbDashboards_test.js
+++ tests/dashboards/verifyMongodbDashboards_test.js
@@ -50,16 +50,3 @@ Scenario(
     await dashboardPage.verifyThereAreNoGraphsWithoutData(1);
   },
 );
-
-Scenario('PMM-T1889 Verify Mongo replication lag graph shows correct info @pmm-psmdb-replica-integration', async ({ I, dashboardPage }) => {
-  const lagValue = 10;
-  const testConfigFile = `c = rs.conf(); c.members[2].secondaryDelaySecs = ${lagValue}; c.members[2].priority = 0; c.members[2].hidden = true; rs.reconfig(c);`;
-
-  await I.verifyCommand(`sudo docker exec rs101 mongo "mongodb://root:root@localhost/?replicaSet=rs" --eval "${testConfigFile}"`);
-  I.amOnPage(I.buildUrlWithParams(dashboardPage.mongodbReplicaSetSummaryDashboard.cleanUrl, { from: 'now-5m' }));
-  dashboardPage.waitForDashboardOpened();
-  await dashboardPage.waitForMaxReplicationLagValuesAbove('rs103', lagValue, 90);
-  const maxValue = await I.grabTextFrom(dashboardPage.mongodbReplicaSetSummaryDashboard.elements.replicationLagMax('rs103'));
-
-  I.assertFalse(/min|hour|day|week|month|year/.test(maxValue), `Max replication value should be in seconds. Value is: ${maxValue}`);
-});
--- tests/pages/dashboardPage.js
+++ tests/pages/dashboardPage.js
@@ -1281,6 +1281,20 @@ module.exports = {
     return locate(this.fields.metricTitle).withText(metricName);
   },
 
+  graphLegendSeriesValue(metricName, value) {
+    return this.graphsLocator(metricName).find('.graph-legend-series').find('td').withText(value);
+  },
+
+  graphLegendSeriesRowByTitle(metricName, title) {
+    return this.graphsLocator(metricName).find(`//tr[@class="graph-legend-series "][td//a[@title="${title}"]]`);
+  },
+
+  graphLegendColumnValueByExpression(graphName, title, columnName, expression) {
+    return this
+      .graphLegendSeriesRowByTitle(graphName, title)
+      .find(`//td[@class="graph-legend-value ${columnName}" and number(substring-before(text(), " ")) ${expression}]`);
+  },
+
   panelByTitle(title) {
     return I.useDataQA(`data-testid Panel header ${title}`);
   },
