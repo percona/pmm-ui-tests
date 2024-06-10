6b035990a0756bbc810d4d1a7cc924be6142ae97
Peter Sirotnak <sirotnak.p@gmail.com>
2024-06-10 09:05:33 +0200

    PMM-9403: Merge issues

--- tests/backup/pages/testData.js
+++ tests/backup/pages/testData.js
@@ -8,8 +8,8 @@ const storageLocationConnection = {
 const psStorageLocationConnection = {
   endpoint: 'https://s3.us-east-2.amazonaws.com',
   bucket_name: 'pmm-backup1',
-  access_key: process.env.PMM_QA_AWS_ACCESS_KEY_ID,
-  secret_key: process.env.PMM_QA_AWS_ACCESS_KEY,
+  access_key: process.env.BACKUP_LOCATION_ACCESS_KEY,
+  secret_key: process.env.BACKUP_LOCATION_SECRET_KEY,
 };
 
 module.exports = {
