59dbd21f8609933f9b03d803991741923b78fae2
Peter Sirotnak <sirotnak.p@gmail.com>
2024-06-12 11:24:34 +0200

    PMM-7: Fix test

--- tests/backup/scheduled_test.js
+++ tests/backup/scheduled_test.js
@@ -104,7 +104,7 @@ Scenario(
 
     I.seeElementsDisabled(scheduledPage.buttons.createSchedule);
     I.fillField(scheduledPage.fields.backupName, scheduleName);
-    I.seeElementsEnabled(scheduledPage.buttons.createSchedule);
+    I.waitForEnabled(scheduledPage.buttons.createSchedule, 10);
   },
 );
 
@@ -348,7 +348,9 @@ Scenario(
     await scheduledPage.verifyBackupValues(newSchedule);
 
     // Verify schedule is disabled after copy
-    I.seeAttributesOnElements(scheduledPage.elements.toggleByName(newSchedule.name), { checked: null });
+    const isChecked = await I.grabAttributeFrom(scheduledPage.elements.toggleByName(newSchedule.name), 'checked');
+
+    I.assertEqual(isChecked, null, 'Element is checked, but should not be.');
   },
 );
 
@@ -384,7 +386,9 @@ Scenario(
 
     // Disable schedule
     I.click(scheduledPage.buttons.enableDisableByName(schedule.name));
-    I.seeAttributesOnElements(scheduledPage.elements.toggleByName(schedule.name), { checked: null });
+    const isChecked = await I.grabAttributeFrom(scheduledPage.elements.toggleByName(schedule.name), 'checked');
+
+    I.assertEqual(isChecked, null, `Element ${scheduledPage.elements.toggleByName(schedule.name).xpath} is checked, but should not be.`);
 
     // Verify the color is the same as before enabling
     I.seeCssPropertiesOnElements(scheduledPage.elements.scheduleTypeByName(schedule.name), { 'background-color': color });
