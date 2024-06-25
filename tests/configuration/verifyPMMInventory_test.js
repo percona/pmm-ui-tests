5df62476536f9f00d20d686198c4401216f5741e
Vasyl Yurkovych <59879559+yurkovychv@users.noreply.github.com>
2024-06-25 15:43:47 +0300

    PMM-12375 db version label test for v3 (#808)
    * PMM-12375 changes for v3
    
    * PMM-12547 unskip test v3
    
    * PMM-12952 add check for postgresql default value v3

--- tests/configuration/verifyPMMInventory_test.js
+++ tests/configuration/verifyPMMInventory_test.js
@@ -132,6 +132,42 @@ Scenario(
 );
 
 Scenario(
+  'PMM-T1811 - verify version displayed for added service on Inventory page @inventory @inventory-fb',
+  async ({
+    I, pmmInventoryPage, addInstanceAPI,
+  }) => {
+    const psServiceName = 'ps_5.7_version_test';
+    const rdsPostgresqlServiceName = 'pg_rds_version_test';
+    const mongoServiceName = 'mongo_4.2_version_test';
+    const pgServiceName = 'pg_15_version_test';
+
+    await addInstanceAPI.addMysql(psServiceName);
+    await addInstanceAPI.addMongodb(mongoServiceName);
+    await addInstanceAPI.addPostgresql(pgServiceName);
+    await addInstanceAPI.addRDSPostgresql(rdsPostgresqlServiceName);
+
+    I.amOnPage(pmmInventoryPage.url);
+    I.waitForVisible(pmmInventoryPage.fields.showServiceDetails(psServiceName), 20);
+
+    I.click(pmmInventoryPage.fields.showServiceDetails(psServiceName));
+    I.waitForVisible(pmmInventoryPage.fields.detailsLabelByText('version=5.7.30-33-log'), 5);
+    I.click(pmmInventoryPage.fields.hideServiceDetails(psServiceName));
+
+    I.click(pmmInventoryPage.fields.showServiceDetails(pgServiceName));
+    I.waitForVisible(pmmInventoryPage.fields.detailsLabelByText('version=15.4 - Percona Distribution'), 5);
+    I.click(pmmInventoryPage.fields.hideServiceDetails(pgServiceName));
+
+    I.click(pmmInventoryPage.fields.showServiceDetails(mongoServiceName));
+    I.waitForVisible(pmmInventoryPage.fields.detailsLabelByText('version=4.4.24'), 5);
+    I.click(pmmInventoryPage.fields.hideServiceDetails(mongoServiceName));
+
+    I.click(pmmInventoryPage.fields.showServiceDetails(rdsPostgresqlServiceName));
+    I.waitForVisible(pmmInventoryPage.fields.detailsLabelByText('version=12.14'), 300);
+    I.click(pmmInventoryPage.fields.hideServiceDetails(rdsPostgresqlServiceName));
+  },
+);
+
+Scenario(
   'PMM-T342 - Verify pmm-server node cannot be removed from PMM Inventory page @inventory',
   async ({ I, pmmInventoryPage }) => {
     const node = 'pmm-server';
