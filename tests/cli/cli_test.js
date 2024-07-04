a10f279b6868a77ea45467774764125737cefebb
Peter Sirotnak <sirotnak.p@gmail.com>
2024-07-04 12:00:50 +0200

    PMM-13220: Parse running processes response

--- tests/cli/cli_test.js
+++ tests/cli/cli_test.js
@@ -3,13 +3,13 @@ Feature('Generic PMM Server CLI Tests');
 Scenario('PMM-T1862 Verify all processes in PMM server are running under non-root user', async ({ I }) => {
   const pmmServerContainerId = await I.verifyCommand('docker ps --filter "name=pmm-server" --format "{{ .ID }}"');
 
-  const processesUser = (await I.verifyCommand(`docker top ${pmmServerContainerId} | awk '{print $1}'`))
-    .replace('UID\n', '')
+  const processesUser = (await I.verifyCommand(`docker top ${pmmServerContainerId} | awk '{print $1 " " $8}'`))
+    .replace('UID CMD\n', '')
     .split('\n');
 
   console.log(`User is: ${processesUser}`);
 
-  const nonEc2UserProcesses = processesUser.filter(() => processesUser === 'ec2-user');
+  const nonEc2UserProcesses = processesUser.filter(() => processesUser.includes('ec2-user'));
 
   I.assertTrue(nonEc2UserProcesses.length === 0);
 });
