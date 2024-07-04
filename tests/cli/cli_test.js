05f695a4f2179ead6eeade1d642c80c8ca6814d8
Peter Sirotnak <sirotnak.p@gmail.com>
2024-07-04 11:46:43 +0200

    PMM-13220: Skeleton for the test

---
+++ tests/cli/cli_test.js
@@ -0,0 +1,9 @@
+Feature('Generic PMM Server CLI Tests');
+
+Scenario('PMM-T1862 Verify all processes in PMM server are running under non-root user', async ({ I }) => {
+  const pmmServerContainerId = await I.verifyCommand('docker ps --filter "name=pmm-server" --format "{{ .ID }}"');
+
+  const processesUser = await I.verifyCommand(`docker top ${pmmServerContainerId} | awk '{print $1}'`);
+
+  console.log(`User is: ${processesUser}`);
+});
