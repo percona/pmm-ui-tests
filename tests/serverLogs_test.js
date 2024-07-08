0f384f58df71303aff52e9f10476e6a9ac368094
yurkovychv <vasyl.yurkovych@percona.com>
2024-07-08 14:50:30 +0300

    PMM-13205 tweak line_count tests to be executed as part of FB

--- tests/serverLogs_test.js
+++ tests/serverLogs_test.js
@@ -7,22 +7,30 @@ const filename = 'logs.zip';
 const fileNameToCheck = 'pmm-managed.log';
 const baseUrl = codeceptjsConfig.config.helpers.Playwright.url;
 
-Scenario('Verify no line_count parameter when downloading logs @nightly', async ({ I, codeceptjsConfig }) => {
+BeforeSuite(async ({ locationsAPI }) => {
+  // Simple request to generate 51k lines in logs
+  for (let i = 0; i < 10000; i++) {
+    await locationsAPI.getLocationsList();
+  }
+});
+
+// @settings-fb tag added in order to execute these tests on FB
+
+Scenario('Verify no line_count parameter when downloading logs @settings-fb', async ({ I }) => {
   const outputPath = await I.downloadZipFile(`${baseUrl}/logs.zip`, filename);
   const actualLineCount = I.getFileLineCount(outputPath, fileNameToCheck);
 
   assert.ok(actualLineCount === 50001, `File ${fileNameToCheck} has ${actualLineCount} lines, but expected 50001`);
 });
 
-// @settings-fb tag added in order to execute this test on FB
-Scenario('Verify line_count=10 parameter when downloading logs @settings-fb @nightly', async ({ I, codeceptjsConfig }) => {
+Scenario('Verify line_count=10 parameter when downloading logs @settings-fb', async ({ I }) => {
   const outputPath = await I.downloadZipFile(`${baseUrl}/logs.zip?line-count=10`, filename);
   const actualLineCount = I.getFileLineCount(outputPath, fileNameToCheck);
 
   assert.ok(actualLineCount === 11, `File ${fileNameToCheck} has ${actualLineCount} lines, but expected 11`);
 });
 
-Scenario('Verify line_count=-1 parameter when downloading logs @nightly', async ({ I, codeceptjsConfig }) => {
+Scenario('Verify line_count=-1 parameter when downloading logs @settings-fb', async ({ I }) => {
   const outputPath = await I.downloadZipFile(`${baseUrl}/logs.zip?line-count=-1`, filename);
   const actualLineCount = I.getFileLineCount(outputPath, fileNameToCheck);
 
