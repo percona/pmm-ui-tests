6a50c6980d9a9b0191192ac0f197717aa4b50fea
Peter Sirotnak <sirotnak.p@gmail.com>
2024-07-14 10:35:05 +0200

    PMM-13232: Fix certificate locators

--- tests/pages/remoteInstancesPage.js
+++ tests/pages/remoteInstancesPage.js
@@ -142,9 +142,9 @@ module.exports = {
     subscriptionID: '$azure_subscription_id-text-input',
     tableStatsGroupTableLimit: '$tablestats_group_table_limit-number-input',
     tenantID: '$azure_tenant_id-text-input',
-    tlscaInput: '$tls_ca-textarea-input',
-    tlsCertificateInput: '$tls_cert-textarea-input',
-    tlsCertificateKeyInput: '$tls_key-textarea-input',
+    tlscaInput: locate('$tls_ca-textarea-input'),
+    tlsCertificateInput: locate('$tls_cert-textarea-input'),
+    tlsCertificateKeyInput: locate('$tls_key-textarea-input'),
     tlsCertificateFilePasswordInput: '$tls_certificate_file_password-password-input',
     tlsCertificateKey: '$tls_certificate_key-textarea-input',
     usePerformanceSchema2: '$qan_mysql_perfschema-field-container',
@@ -265,9 +265,15 @@ module.exports = {
       I.dontSeeElement(this.fields.tlsCertificateInput);
       I.click(this.fields.useTLS);
       I.waitForElement(this.fields.tlscaInput, 30);
-      await this.fillFileContent(this.fields.tlscaInput, details.tlsCAFile);
-      await this.fillFileContent(this.fields.tlsCertificateInput, details.tlsCertFile);
-      await this.fillFileContent(this.fields.tlsCertificateKeyInput, details.tlsKeyFile);
+      I.usePlaywrightTo('Fill TLS ca field', async ({ page }) => {
+        await page.fill(this.fields.tlscaInput.toXPath(), details.tlsCA);
+      });
+      I.usePlaywrightTo('Fill TLS certificate field', async ({ page }) => {
+        await page.fill(this.fields.tlsCertificateInput.toXPath(), details.tlsCert);
+      });
+      I.usePlaywrightTo('Fill TLS certificate key field', async ({ page }) => {
+        await page.fill(this.fields.tlsCertificateKeyInput.toXPath(), details.tlsKey);
+      });
       if (details.serviceType === 'postgres_ssl') I.click(this.fields.usePgStatStatements);
 
       if (details.serviceType === 'mysql_ssl') I.click(this.fields.skipTLSL);
