6989108615ae17b5b11f2e87167fe5b02f9f10c3
Saikumar <saikumar.vs@percona.com>
2024-07-15 22:30:10 +0530

    Updates for PDPGSQL UI Test for v3

--- tests/qa-integration/pmm_pdpgsql_integration_test.js
+++ tests/qa-integration/pmm_pdpgsql_integration_test.js
@@ -13,15 +13,18 @@ Scenario(
   }) => {
     I.amOnPage(dashboardPage.postgresqlInstanceSummaryDashboard.url);
     dashboardPage.waitForDashboardOpened();
-    await dashboardPage.applyFilter('Service Name', 'PDPGSQL_');
+    await dashboardPage.applyFilter('Service Name', 'pdpgsql_');
+    // Grab containerName from ServiceName
+    const grabServiceName = await I.grabTextFrom('//label[@for="var-service_name"]//following-sibling::*');
+    const partsServiceName = grabServiceName.split('_service');
+    const containerServiceName = partsServiceName[0];
+
     await dashboardPage.expandEachDashboardRow();
-    I.click(adminPage.fields.metricTitle);
     adminPage.performPageDown(5);
     adminPage.performPageUp(5);
-    dashboardPage.verifyMetricsExistence(dashboardPage.postgresqlInstanceSummaryDashboard.metrics);
-    await dashboardPage.verifyThereAreNoGraphsWithNA();
+    await dashboardPage.verifyMetricsExistence(dashboardPage.postgresqlInstanceSummaryDashboard.metrics);
     await dashboardPage.verifyThereAreNoGraphsWithoutData(1);
-    await I.verifyCommand('pmm-admin list | grep "postgresql_pgstatmonitor_agent" | grep "Running"');
-    await I.verifyCommand('pmm-admin list | grep "postgres_exporter" | grep "Running"');
+    await I.verifyCommand(`docker exec ${containerServiceName} pmm-admin list | grep "postgresql_pgstatmonitor_agent" | grep "Running"`);
+    await I.verifyCommand(`docker exec ${containerServiceName} pmm-admin list | grep "postgres_exporter" | grep "Running"`);
   },
 );
