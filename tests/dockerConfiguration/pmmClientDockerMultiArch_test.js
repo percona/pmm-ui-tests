8c97f828c8df6af80163b089424b56a75fd4d86c
Peter Sirotnak <sirotnak.p@gmail.com>
2024-08-14 12:24:18 +0200

    PMM-13276: test different pmm server address

--- tests/dockerConfiguration/pmmClientDockerMultiArch_test.js
+++ tests/dockerConfiguration/pmmClientDockerMultiArch_test.js
@@ -16,7 +16,7 @@ BeforeSuite(async ({ I }) => {
 
   await I.verifyCommand(`docker run -d 
           --name pmm-client 
-          -e PMM_AGENT_SERVER_ADDRESS=${process.env.SERVER_IP}
+          -e PMM_AGENT_SERVER_ADDRESS=pmm-server
           -e PMM_AGENT_SERVER_USERNAME=admin 
           -e PMM_AGENT_SERVER_PASSWORD=${SERVER_PASSWORD}
           -e PMM_AGENT_SERVER_INSECURE_TLS=1 
