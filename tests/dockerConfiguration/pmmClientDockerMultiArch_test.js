e4e2f1146035e79770225e51fc9fdfe559643528
Peter Sirotnak <sirotnak.p@gmail.com>
2024-08-14 14:25:29 +0200

    PMM-13276: Log IP

--- tests/dockerConfiguration/pmmClientDockerMultiArch_test.js
+++ tests/dockerConfiguration/pmmClientDockerMultiArch_test.js
@@ -7,14 +7,9 @@ BeforeSuite(async ({ I }) => {
   const networkName = 'pmm-ui-tests-network';
 
   console.log(`Ip address is: ${process.env.SERVER_IP}`);
-  console.log(`Ip address is: ${process.env.PMM_UI_URL}`);
   console.log(`Architecture is: ${process.env.ARCHITECTURE}`);
-  console.log(await I.verifyCommand('docker ps -a'));
 
   await I.verifyCommand(`docker network create ${networkName}`);
-  // await I.verifyCommand(`docker network connect ${networkName} pmm-server`);
-  // I.wait(60);
-
   await I.verifyCommand(`docker run -d 
           --name pmm-client 
           -e PMM_AGENT_SERVER_ADDRESS=${process.env.SERVER_IP}:443
@@ -30,14 +25,16 @@ BeforeSuite(async ({ I }) => {
           -e PMM_AGENT_SETUP_NODE_TYPE=container
           --network ${networkName} 
           ${DOCKER_IMAGE}`);
-  I.wait(10);
-  await I.verifyCommand(`docker exec pmm-client-${process.env.ARCHITECTURE} pmm-agent --force --server-insecure-tls --server-url=https://admin:${SERVER_PASSWORD}@pmm-server:443 --config-file=/usr/local/percona/pmm2/config/pmm-agent.yaml`, null, 'fail', true);
 
+  I.wait(10);
+  console.log(await I.verifyCommand('docker logs pmm-client'));
+  await I.verifyCommand(`docker exec pmm-client-${process.env.ARCHITECTURE} pmm-agent --force --server-insecure-tls --server-url=https://admin:${SERVER_PASSWORD}@pmm-server:443 --config-file=/usr/local/percona/pmm2/config/pmm-agent.yaml`, null, 'fail', true);
   await I.verifyCommand(`docker run -d 
            --name mysql-multiarch 
            --network ${networkName}  
            -e MYSQL_ROOT_PASSWORD=testPassword 
            mysql:8`);
+
   I.wait(15);
   console.log(await I.verifyCommand('docker logs pmm-client'));
   await I.verifyCommand('docker exec pmm-client pmm-admin add mysql --query-source=perfschema --username=root --password=testPassword --host=mysql-multiarch --port=3306');
