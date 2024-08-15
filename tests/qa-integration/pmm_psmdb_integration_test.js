9d5071343023deb87c84e4d9645de9d26f7bbd67
Peter Sirotnak <sirotnak.p@gmail.com>
2024-08-15 13:09:23 +0200

    PMM-7: Expand row

--- tests/qa-integration/pmm_psmdb_integration_test.js
+++ tests/qa-integration/pmm_psmdb_integration_test.js
@@ -176,7 +176,7 @@ Scenario(
   }) => {
     I.amOnPage(`${dashboardPage.mongodbReplicaSetSummaryDashboard.url}&var-replset=rs1`);
     dashboardPage.waitForDashboardOpened();
-
+    await dashboardPage.expandRows('Replication Lag');
     const username = 'dba';
     const password = 'test1234';
 
@@ -217,7 +217,7 @@ Scenario('PMM-T1889 Verify Mongo replication lag graph shows correct info @pmm-p
   await I.verifyCommand(`sudo docker exec rs101 mongo "mongodb://root:root@localhost/?replicaSet=rs" --eval "${testConfigFile}"`);
   I.amOnPage(I.buildUrlWithParams(dashboardPage.mongodbReplicaSetSummaryDashboard.cleanUrl, { from: 'now-5m', refresh: '5s' }));
   dashboardPage.waitForDashboardOpened();
-  await dashboardPage.expandRows('Replication Lag');
+  await dashboardPage.expandRows(graphName);
   await I.waitForElement(dashboardPage.graphLegendColumnValueByExpression(graphName, serviceName, 'max', '>= 10'), 120);
 
   const maxValue = await I.grabTextFrom(dashboardPage.graphLegendSeriesRowByTitle(graphName, serviceName));
