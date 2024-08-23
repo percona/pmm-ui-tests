d5baab5d9edd69d417fa64b14cca3ab031bdbed9
Peter Sirotnak <sirotnak.p@gmail.com>
2024-08-23 11:35:28 +0200

    PMM-13276 (#837)
    * PMM-13276: Start pmm client container
    
    * PMM-13276: Remove volume for pmm client
    
    * PMM-13276: add mysql and verify dashboard
    
    * PMM-13276: Fix typo
    
    * PMM-13276: Create network
    
    * PMM-13276: Fixtypo
    
    * PMM-13276: Fixtypo
    
    * PMM-13276: run container detached
    
    * PMM-13276: echo running containers
    
    * PMM-13276: Fix async method
    
    * PMM-13276: debug commands
    
    * PMM-13276: Change network type
    
    * PMM-13276: fix container tag
    
    * PMM-13276: Debug command
    
    * PMM-13276: Debug command
    
    * PMM-13276: Debug command
    
    * PMM-13276: Debug command
    
    * PMM-13276: Test different password
    
    * PMM-13276: Connect pmm server to network
    
    * PMM-13276: config pmm agent
    
    * PMM-13276: Skip config command
    
    * PMM-13276: add config command with correct status
    
    * PMM-13276: Log config
    
    * PMM-13276: force config
    
    * PMM-13276: remove status command
    
    * PMM-13276: Wait for mysql to start
    
    * PMM-13276: Test screenshot
    
    * PMM-13276: Test screenshot
    
    * PMM-13276: Refactor tests
    
    * PMM-13276: change count of expected elements without data.
    
    * PMM-13276: change count of expected elements without data.
    
    * PMM-13276: change count of expected elements without data.
    
    * PMM-13276: change count of expected elements without data.
    
    * PMM-13276: Refactors testcase
    
    * PMM-13276: Refactors testcase
    
    * PMM-13276: logs server IP address
    
    * PMM-13276: logs server IP address
    
    * PMM-13276: logs server IP address
    
    * PMM-13276: Fix pmm server address for client
    
    * PMM-13276: Skip connecting pmm server to network
    
    * PMM-13276: Logs for pmm client.
    
    * PMM-13276: Test different password
    
    * PMM-13276: log architecture
    
    * PMM-13276: change node name
    
    * PMM-13276: change node name
    
    * PMM-13276: do not connect to the server
    
    * PMM-13276: test different pmm server address
    
    * PMM-13276: log running containers
    
    * PMM-13276: log running containers
    
    * PMM-13276: change network
    
    * PMM-13276: change network
    
    * PMM-13276: refactors
    
    * PMM-13276: Log IP
    
    * PMM-13276: Log IP
    
    * PMM-13276: Log IP
    
    * PMM-13276: Log IP
    
    * PMM-13276: Log IP
    
    * PMM-13276: Log IP
    
    * PMM-13276: Log IP
    
    * PMM-13276: Log IP
    
    * PMM-13276: Log IP
    
    * PMM-13276: Log IP
    
    * PMM-13276: Log IP
    
    * PMM-13276: Log IP
    
    * PMM-13276: Log IP
    
    * PMM-13276: Log IP
    
    * PMM-13276: Log IP
    
    * PMM-13276: Refactor test
    
    * PMM-13276: Fix test name according to zephyr

---
+++ tests/dockerConfiguration/pmmClientDockerMultiArch_test.js
@@ -0,0 +1,53 @@
+Feature('Test PMM client multi arch docker container').retry(1);
+
+BeforeSuite(async ({ I }) => {
+  const pmmClientDockerTag = process.env.CLIENT_VERSION || 'perconalab/pmm-client-test:dev-latest';
+  const pmmServerAdminPassword = process.env.ADMIN_PASSWORD || 'admin';
+  const networkName = 'pmm2-ui-tests_pmm-network';
+  const pmmServerAddress = process.env.ARCHITECTURE === 'agent-amd64' ? 'pmm-server' : process.env.SERVER_IP;
+
+  await I.verifyCommand(`docker network create ${networkName} || true`);
+  await I.verifyCommand(`docker run -d 
+          --name pmm-client-${process.env.ARCHITECTURE}
+          -e PMM_AGENT_SERVER_ADDRESS=${pmmServerAddress}
+          -e PMM_AGENT_SERVER_USERNAME=admin 
+          -e PMM_AGENT_SERVER_PASSWORD=${pmmServerAdminPassword}
+          -e PMM_AGENT_SERVER_INSECURE_TLS=1 
+          -e PMM_AGENT_PORTS_MIN=41000
+          -e PMM_AGENT_PORTS_MAX=41500
+          -e PMM_AGENT_SETUP=1 
+          -e PMM_AGENT_CONFIG_FILE=config/pmm-agent.yaml 
+          -e PMM_AGENT_SETUP_NODE_NAME=pmm-client-${process.env.ARCHITECTURE}
+          -e PMM_AGENT_SETUP_FORCE=1
+          -e PMM_AGENT_SETUP_NODE_TYPE=container
+          --network ${networkName} 
+          ${pmmClientDockerTag}`);
+
+  I.wait(10);
+
+  await I.verifyCommand(`docker exec pmm-client-${process.env.ARCHITECTURE} pmm-agent setup --force --config-file=/usr/local/percona/pmm2/config/pmm-agent.yaml --server-address=${pmmServerAddress}:443 --server-insecure-tls --server-username=admin --server-password=${pmmServerAdminPassword}`);
+
+  await I.verifyCommand(`docker run -d 
+           --name mysql-multiarch 
+           --network ${networkName}  
+           -e MYSQL_ROOT_PASSWORD=testPassword 
+           mysql:8`);
+
+  I.wait(15);
+
+  await I.verifyCommand(`docker exec pmm-client-${process.env.ARCHITECTURE} pmm-admin add mysql --query-source=perfschema --username=root --password=testPassword --host=mysql-multiarch --port=3306`);
+});
+
+Before(async ({ I }) => {
+  await I.Authorize();
+});
+
+Scenario('PMM-T1923 Verify sanity check on pmm-client docker multi arch image (amd64, arm64) @client-docker-multi-arch', async ({ I, dashboardPage, adminPage }) => {
+  const url = I.buildUrlWithParams(dashboardPage.mysqlInstanceSummaryDashboard.clearUrl, { from: 'now-5m' });
+
+  I.amOnPage(url);
+  dashboardPage.waitForDashboardOpened();
+  await dashboardPage.expandEachDashboardRow();
+  await dashboardPage.verifyThereAreNoGraphsWithNA(4);
+  await dashboardPage.verifyThereAreNoGraphsWithoutData(5);
+});
