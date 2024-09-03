f47dda1e4cdcf367a9fc615bf619e22774c21156
yurkovychv <vasyl.yurkovych@percona.com>
2024-09-03 11:52:49 +0300

    PMM-7 add bug reference

--- tests/configuration/verifyPMMSettingsPageFunctionality_test.js
+++ tests/configuration/verifyPMMSettingsPageFunctionality_test.js
@@ -347,6 +347,7 @@ Scenario(
 
     I.assertTrue(publicAddressValue.length > 0, 'Expected the Public Address Input Field to be not empty!');
     pmmSettingsPage.applyChanges();
+    // Remove wait after https://perconadev.atlassian.net/browse/PMM-13340 is fixed
     I.wait(5);
     I.refreshPage();
     await pmmSettingsPage.waitForPmmSettingsPageLoaded();
