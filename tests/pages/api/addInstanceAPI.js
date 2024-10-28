6ccabe115fe9f636622c8ec2ed1f872c7e9b02ad
Peter Sirotnak <sirotnak.p@gmail.com>
2024-10-28 14:11:39 +0100

    PMM-7: Fixes for ssl

--- tests/pages/api/addInstanceAPI.js
+++ tests/pages/api/addInstanceAPI.js
@@ -94,6 +94,8 @@ module.exports = {
     const headers = { Authorization: `Basic ${await I.getAuth()}` };
     const resp = await I.sendPostRequest('v1/management/services', body, headers);
 
+    console.log(resp);
+
     I.assertEqual(resp.status, 200, `Instance ${connection.serviceName} was not added for monitoring`);
   },
 
@@ -148,6 +150,8 @@ module.exports = {
     };
     const headers = { Authorization: `Basic ${await I.getAuth()}` };
     const resp = await I.sendPostRequest('v1/management/services', body, headers);
+
+    console.log(resp);
 
     I.assertEqual(resp.status, 200, `Instance ${connection.serviceName} was not added for monitoring`);
   },
