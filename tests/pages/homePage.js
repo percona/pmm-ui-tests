6f78c8071ea3fca88169e94dbfdc651c540c8c12
Peter Sirotnak <sirotnak.p@gmail.com>
2024-10-29 15:59:22 +0100

    PMM-7: Fixes for the ssl

--- tests/pages/homePage.js
+++ tests/pages/homePage.js
@@ -130,7 +130,7 @@ module.exports = {
 
     I.waitForElement(pmmUpgradePage.elements.updateNowButton);
     I.click(pmmUpgradePage.elements.updateNowButton);
-    I.wait(120);
+    I.wait(180);
 
     // I.waitForElement(pmmUpgradePage.elements.updateSuccess, 360);
   },
