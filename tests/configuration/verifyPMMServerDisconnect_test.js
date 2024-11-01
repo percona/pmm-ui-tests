7fd4f21c8b074b9cfed8d2bf0428cce0d81c5814
yurkovychv <vasyl.yurkovych@percona.com>
2024-11-01 12:58:07 +0200

    PMM-7 clickhouse tests tweaks

--- tests/configuration/verifyPMMServerDisconnect_test.js
+++ tests/configuration/verifyPMMServerDisconnect_test.js
@@ -10,10 +10,10 @@ BeforeSuite(async ({ I }) => {
   await I.verifyCommand(`PMM_SERVER_IMAGE=${serverImage} docker compose -f docker-compose-disconnect.yml up -d pmm-server-disconnect`);
   await I.asyncWaitFor(async () => await I.verifyCommand(`echo $(curl -s -o /dev/null -w '%{http_code}' 127.0.0.1:${pmmServerPort}/ping)`) === '200', 60);
   await I.verifyCommand('docker compose -f docker-compose-disconnect.yml up -d pmm-client');
-  await I.verifyCommand('docker compose -f docker-compose-disconnect.yml up -d mysql5.7');
+  await I.verifyCommand('docker compose -f docker-compose-disconnect.yml up -d ps8');
   clientServerNetwork = await I.verifyCommand('docker inspect pmm-client-disconnect -f \'{{range $k, $v := .NetworkSettings.Networks}}{{printf "%s\\n" $k}}{{end}}\' | grep -o \'.*server-network\'');
-  await I.asyncWaitFor(async () => await I.verifyCommand('echo $(docker container logs mysql-disconnect-5.7 2>&1 | grep "Server hostname (bind-address)")') !== '', 60);
-  await I.verifyCommand('docker exec pmm-client-disconnect pmm-admin add mysql --username=root --password=7B*53@lCdflR --host=mysql-disconnect-5.7 --port=3306 --query-source=perfschema mysql-disconnect-5.7');
+  I.wait(20);
+  await I.verifyCommand('docker exec pmm-client-disconnect pmm-admin add mysql --username=root --password=7B*53@lCdflR --host=ps8 --port=3306 --query-source=perfschema ps8');
   // wait for the data to be scraped from db
   I.wait(60);
 });
@@ -28,7 +28,7 @@ AfterSuite(async ({ I }) => {
 
 const withCustomBaseUrl = (url) => `${basePmmUrl}${url}`;
 
-Scenario.only(
+Scenario(
   '@PMM-T1442 Verify metrics are saved if PMM server was offline @disconnect',
   async ({ I, dashboardPage }) => {
     await I.amOnPage(withCustomBaseUrl(dashboardPage.mySQLInstanceOverview.url));
