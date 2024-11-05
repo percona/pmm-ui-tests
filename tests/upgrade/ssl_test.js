1ce14f7b472cbb6853d4070ec75163ddab584510
Peter Sirotnak <sirotnak.p@gmail.com>
2024-11-05 12:44:28 +0100

    PMM-7: Fixes for mongo ssl

--- tests/upgrade/ssl_test.js
+++ tests/upgrade/ssl_test.js
@@ -85,7 +85,7 @@ Data(sslinstances).Scenario(
         address: container,
         cluster: 'mongodb_ssl_remote_cluster',
         environment: 'mongodb_ssl_remote_cluster',
-        tls_certificate_file_password: await I.verifyCommand('docker exec psmdb-server cat /mongodb_certs/ca-certs.pem'),
+        tls_certificate_file_password: '',
         tls_certificate_key: await I.verifyCommand('docker exec psmdb-server cat /mongodb_certs/client.pem'),
         tls_ca: await remoteInstancesPage.getFileContent('/srv/qa-integration/pmm_psmdb_diffauth_setup/pki/ca.crt'),
       };
