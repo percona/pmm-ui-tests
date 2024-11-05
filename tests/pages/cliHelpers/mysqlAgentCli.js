2bb4b2eecee8c676fc48c787cd5dff8a00c39978
Peter Sirotnak <sirotnak.p@gmail.com>
2024-11-05 10:28:35 +0100

    PMM-7: Fixes for the mysql slowlog

--- tests/pages/cliHelpers/mysqlAgentCli.js
+++ tests/pages/cliHelpers/mysqlAgentCli.js
@@ -3,16 +3,18 @@ const { I, inventoryAPI } = inject();
 class MysqlAgentCli {
   async verifyMySqlAgentLogLevel(exporterType, dbDetails, logLevel = 'warn') {
     const logLvlFlag = logLevel ? `--log-level=${logLevel}` : '';
-    const addAgentResponse = await I.verifyCommand(`docker exec ${dbDetails.container_name} pmm-admin inventory add agent ${exporterType} --password=${dbDetails.password} --push-metrics ${logLvlFlag} ${dbDetails.pmm_agent_id} ${dbDetails.service_id} ${dbDetails.username}`);
+    const addAgentResponse = await I.verifyCommand(`docker exec ${dbDetails.container_name} pmm-admin inventory add agent ${exporterType} --password=${dbDetails.password} ${exporterType === "mysqld-exporter" ? '--push-metrics': ''} ${logLvlFlag} ${dbDetails.pmm_agent_id} ${dbDetails.service_id} ${dbDetails.username}`);
+    const agent_id = addAgentResponse.split('\n').find((row) => row.includes('PMM-Agent ID')).split(':')[1].trim();
 
     console.log(addAgentResponse);
+    console.log(`Agent ID is: ${agent_id}`);
 
-    // const actualLogLevel = await getLogLevel(agent_id, exporterType);
+    const actualLogLevel = await getLogLevel(agent_id, exporterType);
 
-    // I.say(`Actual log level is: ${actualLogLevel}`);
-    // I.assertEqual(actualLogLevel, getLogLevelResponse(logLevel), `Was expecting Mysql Exporter for service ${dbDetails.service_name} added again via inventory command and log level to have ${getLogLevelResponse(logLevel)} set, actual log level was: ${actualLogLevel}`);
+    I.say(`Actual log level is: ${actualLogLevel}`);
+    I.assertEqual(actualLogLevel, getLogLevelResponse(logLevel), `Was expecting Mysql Exporter for service ${dbDetails.service_name} added again via inventory command and log level to have ${getLogLevelResponse(logLevel)} set, actual log level was: ${actualLogLevel}`);
 
-    // await I.verifyCommand(`docker exec ${dbDetails.container_name} pmm-admin inventory remove agent ${agent_id}`);
+    await I.verifyCommand(`docker exec ${dbDetails.container_name} pmm-admin inventory remove agent ${agent_id}`);
   }
 }
 
