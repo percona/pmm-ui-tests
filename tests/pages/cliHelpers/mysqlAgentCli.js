82cab2c5c9c43aab397e60255b791161fae49697
Peter Sirotnak <sirotnak.p@gmail.com>
2024-11-05 12:30:53 +0100

    PMM-7: Fixes for mongo ssl

--- tests/pages/cliHelpers/mysqlAgentCli.js
+++ tests/pages/cliHelpers/mysqlAgentCli.js
@@ -9,7 +9,7 @@ class MysqlAgentCli {
     console.log(addAgentResponse);
     console.log(`Agent ID is: ${agent_id}`);
 
-    I.wait(15);
+    I.wait(60);
 
     const actualLogLevel = await getLogLevel(agent_id, exporterType);
 
