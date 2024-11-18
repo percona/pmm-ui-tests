3b53c98adc370c8327e02b1bf2e0a21c6cdb9bee
Peter Sirotnak <sirotnak.p@gmail.com>
2024-11-18 12:00:26 +0100

    PMM-7: Log count of queries

--- tests/qa-integration/pmm_ps_integration_test.js
+++ tests/qa-integration/pmm_ps_integration_test.js
@@ -175,6 +175,9 @@ Scenario(
     await queryAnalyticsPage.filters.selectFilter(dbName);
     queryAnalyticsPage.waitForLoaded();
     for (let i = 0; i <= 24; i++) {
+      console.log(`Count of queries is: ${await I.grabTextFrom(queryAnalyticsPage.data.elements.totalItems)}`);
+      console.log(`Count of queries is: ${parseInt(await I.grabTextFrom(queryAnalyticsPage.data.elements.totalItems), 10)}`);
+
       const countOfQueries = parseInt(await I.grabTextFrom(queryAnalyticsPage.data.elements.totalItems), 10);
 
       I.wait(10);
