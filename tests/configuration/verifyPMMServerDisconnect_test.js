ffe1055a3bf4cb20e93b37d3722ac30e09e10d6e
yurkovychv <vasyl.yurkovych@percona.com>
2024-11-28 20:00:07 +0200

    PMM-13573 update disconnect test

--- tests/configuration/verifyPMMServerDisconnect_test.js
+++ tests/configuration/verifyPMMServerDisconnect_test.js
@@ -13,16 +13,6 @@ BeforeSuite(async ({ I }) => {
   await I.verifyCommand('docker exec pmm-client-disconnect pmm-admin add mysql --username=root --password=7B*53@lCdflR --host=ps8 --port=3306 --query-source=perfschema ps8');
   // wait for the data to be scraped from db
   I.wait(60);
-
-  await I.verifyCommand(`PMM_SERVER_IMAGE=${process.env.DOCKER_VERSION} docker-compose -f docker-compose-disconnect.yml up -d pmm-server-disconnect`);
-  await I.asyncWaitFor(async () => await I.verifyCommand(`echo $(curl -s -o /dev/null -w '%{http_code}' 127.0.0.1:${pmmServerPort}/ping)`) === '200', 100);
-  await I.verifyCommand('docker-compose -f docker-compose-disconnect.yml up -d pmm-client');
-  await I.verifyCommand('docker-compose -f docker-compose-disconnect.yml up -d mysql5.7');
-  clientServerNetwork = await I.verifyCommand('docker inspect pmm-client-disconnect -f \'{{range $k, $v := .NetworkSettings.Networks}}{{printf "%s\\n" $k}}{{end}}\' | grep -o \'.*server-network\'');
-  await I.asyncWaitFor(async () => await I.verifyCommand('echo $(docker container logs mysql-disconnect-5.7 2>&1 | grep "Server hostname (bind-address)")') !== '', 100);
-  await I.verifyCommand('docker exec pmm-client-disconnect pmm-admin add mysql --username=root --password=7B*53@lCdflR --host=mysql-disconnect-5.7 --port=3306 --query-source=perfschema mysql-disconnect-5.7');
-  // wait for the data to be scraped from db
-  I.wait(60);
 });
 
 Before(async ({ I }) => {
