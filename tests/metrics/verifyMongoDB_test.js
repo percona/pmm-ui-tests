b6d3620680c3085af0b0e150a121d515cb13636e
Peter Sirotnak <sirotnak.p@gmail.com>
2024-12-03 14:03:51 +0100

    PMM-12219: create user

--- tests/metrics/verifyMongoDB_test.js
+++ tests/metrics/verifyMongoDB_test.js
@@ -50,8 +50,25 @@ AfterSuite(async ({ I }) => {
 Scenario(
   'PMM-T1241 - Verify add mongoDB service with "+" in user password @not-ui-pipeline @mongodb-exporter',
   async ({ I, grafanaAPI }) => {
+    await I.say(await I.verifyCommand(`docker exer rs101 mongo "mongodb://${connection.username}:${connection.password}@localhost/" << EOF
+      db.getSiblingDB("admin").createUser({
+        user: "test_user",
+          pwd: "pass+",
+          roles: [
+            { role: "explainRole", db: "admin" },
+            { role: "clusterMonitor", db: "admin" },
+            { role: "read", db: "local" },
+            { "db" : "admin", "role" : "readWrite", "collection": "" },
+            { "db" : "admin", "role" : "backup" },
+            { "db" : "admin", "role" : "clusterMonitor" },
+            { "db" : "admin", "role" : "restore" },
+            { "db" : "admin", "role" : "pbmAnyAction" }
+          ]
+      });
+    EOF`));
+
     await I.say(
-      await I.verifyCommand(`pmm-admin add mongodb --port=27017 --password=${mongo_test_user.password} --username='${mongo_test_user.username}' --service-name=${mongodb_service_name}`),
+      await I.verifyCommand(`pmm-admin add mongodb --port=${connection.port} --password=${mongo_test_user.password} --username='${mongo_test_user.username}' --service-name=${mongodb_service_name}`),
     );
 
     await grafanaAPI.waitForMetric('mongodb_up', { type: 'service_name', value: mongodb_service_name }, 65);
