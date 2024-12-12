fa981a16cc3efb94d0714982c6dfe46196d3e59a
Matej Kubinec <matej.kubinec@3pillarglobal.com>
2024-12-12 10:45:14 +0100

    PMM-7 Reorder requests

--- tests/product-tour/productTour_test.js
+++ tests/product-tour/productTour_test.js
@@ -3,10 +3,11 @@ Feature('Pmm Product tour tests');
 Before(async ({ I, settingsAPI }) => {
   await I.stopMockingProductTourApi();
   await I.Authorize();
+  // get request will create the initial state
+  await I.sendGetRequest('v1/users/me', { Authorization: `Basic ${await I.getAuth()}` });
   // todo: it would be better to disable updates once PMM-13608 is fixed
   // snooze update modal
   await settingsAPI.setTourOptions(false, true, '3.2.0');
-  await I.sendGetRequest('v1/users/me', { Authorization: `Basic ${await I.getAuth()}` });
 });
 
 Scenario('PMM-T1879 Verify that product tour dialog is displayed after check later button pressed. @grafana-pr', async ({ I, homePage }) => {
