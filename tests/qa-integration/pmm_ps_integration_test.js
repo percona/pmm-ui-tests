759d75c41b66462eefac731fdf1c99c2bbbb548d
Peter Sirotnak <sirotnak.p@gmail.com>
2025-01-16 11:39:45 +0100

    PMM-7: debug

--- tests/qa-integration/pmm_ps_integration_test.js
+++ tests/qa-integration/pmm_ps_integration_test.js
@@ -10,7 +10,7 @@ Before(async ({ I, settingsAPI }) => {
 });
 
 const version = process.env.PS_VERSION ? `${process.env.PS_VERSION}` : '8.0';
-const container_name = `ps_pmm_${version}`;
+const container_name = `ps_8.0__1`;
 const remoteServiceName = 'remote_pmm-mysql-integration';
 
 const connection = {
