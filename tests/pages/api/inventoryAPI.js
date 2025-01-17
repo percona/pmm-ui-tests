da50267d7b32851a5fb8aa9c90652793c4e08b97
Peter Sirotnak <sirotnak.p@gmail.com>
2025-01-17 10:17:56 +0100

    PMM-7: debug

--- tests/pages/api/inventoryAPI.js
+++ tests/pages/api/inventoryAPI.js
@@ -51,6 +51,8 @@ module.exports = {
   async apiGetNodeInfoByServiceName(serviceType, serviceName, excludeSubstring) {
     const resp = await this.apiGetServices(serviceType);
 
+    console.log(resp.data)
+
     const data = Object.values(resp.data).flat()
       .filter(({ service_name }) => service_name.includes(serviceName));
 
