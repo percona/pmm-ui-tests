3e32f3cd8ec0607e7eba920fd6e5a672806b5b0d
Peter Sirotnak <sirotnak.p@gmail.com>
2025-01-17 11:53:08 +0100

    PMM-7: debug

--- tests/qa-integration/pmm_pdpgsql_integration_test.js
+++ tests/qa-integration/pmm_pdpgsql_integration_test.js
@@ -18,16 +18,10 @@ Scenario(
 
     I.amOnPage(I.buildUrlWithParams(dashboardPage.postgresqlInstanceSummaryDashboard.cleanUrl, { from: 'now-5m', service_name: clientServiceName }));
     await dashboardPage.waitForDashboardOpened();
-    await dashboardPage.applyFilter('Service Name', 'pdpgsql_');
-    // Grab containerName from ServiceName
-    const grabServiceName = await I.grabTextFrom('//label[@for="var-service_name"]//following-sibling::*');
-    const partsServiceName = grabServiceName.split('_service');
-    const containerServiceName = partsServiceName[0];
-    I.wait(60);
     await dashboardPage.expandEachDashboardRow();
     await dashboardPage.verifyMetricsExistence(dashboardPage.postgresqlInstanceSummaryDashboard.metrics);
     await dashboardPage.verifyThereAreNoGraphsWithoutData();
-    await I.verifyCommand(`docker exec ${containerServiceName} pmm-admin list | grep "postgresql_pgstatmonitor_agent" | grep "Running"`);
-    await I.verifyCommand(`docker exec ${containerServiceName} pmm-admin list | grep "postgres_exporter" | grep "Running"`);
+    await I.verifyCommand('pmm-admin list | grep "postgresql_pgstatmonitor_agent" | grep "Running"');
+    await I.verifyCommand('pmm-admin list | grep "postgres_exporter" | grep "Running"');
   },
 ).retry(3);
