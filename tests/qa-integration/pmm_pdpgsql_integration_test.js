cebbefb96e45e48499d133506ac496952343bb95
Peter Sirotnak <sirotnak.p@gmail.com>
2025-01-17 12:00:47 +0100

    PMM-7: debug

--- tests/qa-integration/pmm_pdpgsql_integration_test.js
+++ tests/qa-integration/pmm_pdpgsql_integration_test.js
@@ -25,3 +25,22 @@ Scenario(
     await I.verifyCommand('pmm-admin list | grep "postgres_exporter" | grep "Running"');
   },
 ).retry(3);
+
+Scenario(
+  'Verify QAN after PDPGSQL Instances is added @pdpgsql-pmm-integration @not-ui-pipeline @pmm-migration',
+  async ({
+    I, queryAnalyticsPage, inventoryAPI, adminPage,
+  }) => {
+    const clientServiceName = (await inventoryAPI.apiGetNodeInfoByServiceName(SERVICE_TYPE.POSTGRESQL, 'PDPGSQL_')).service_name;
+
+    I.amOnPage(I.buildUrlWithParams(queryAnalyticsPage.url, { from: 'now-5m' }));
+    queryAnalyticsPage.waitForLoaded();
+    await adminPage.applyTimeRange('Last 12 hours');
+    queryAnalyticsPage.waitForLoaded();
+    await queryAnalyticsPage.filters.selectFilterInGroup(clientServiceName, 'Service Name');
+    queryAnalyticsPage.waitForLoaded();
+    const count = await queryAnalyticsPage.data.getCountOfItems();
+
+    assert.ok(count > 0, `The queries for service ${clientServiceName} instance do NOT exist, check QAN Data`);
+  },
+).retry(1);
