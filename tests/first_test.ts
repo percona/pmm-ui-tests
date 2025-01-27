58bbbdd0fc2a5f7ee809d79f68039c1891aa3970
Peter Sirotnak <sirotnak.p@gmail.com>
2025-01-27 09:31:46 +0100

    PMM-7: Adds psmdb

--- tests/first_test.ts
+++ tests/first_test.ts
@@ -16,7 +16,7 @@ Data(data).Scenario('This is a test', async ({ I, api, current }) => {
     case 'mysql':
       await I.verifyCommand(`pmm-admin add mysql --node-id=${service.node_id} --pmm-agent-id=${pmmAgentId} --port=${service.port} --password=${current.password} --host=${service.address} --query-source=perfschema --agent-password=uitests --custom-labels="testing=upgrade" upgrade-${current.serviceType}`);
       break;
-    case 'psmdb':
+    case 'mongodb':
       await I.verifyCommand(`pmm-admin add mongodb --username=${current.username} --password="${current.password}" --port=${service.port} --host=${service.address} --agent-password=uitests --custom-labels="testing=upgrade" upgrade-${current.serviceType}`);
       break;
     default:
