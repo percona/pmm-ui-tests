a3405955e5e1611a81b196840b99e2bfdde0e474
Peter Sirotnak <sirotnak.p@gmail.com>
2025-02-10 11:43:35 +0100

    PMM-13733: Fixes for pdpgsql

---
+++ tests/migration/pdpgsql_test.js
@@ -0,0 +1,45 @@
+const assert = require('assert');
+const { SERVICE_TYPE } = require('../helper/constants');
+
+Feature('Postgres Tests after PMM migration to V3');
+
+Scenario(
+  'PMM-T1980 - Verify Postgres dashboard after PMM migration to V3 @pmm-migration',
+  async ({
+    I, dashboardPage, inventoryAPI,
+  }) => {
+    const service = (await inventoryAPI.apiGetNodeInfoByServiceName(SERVICE_TYPE.POSTGRESQL, 'pdpgsql_'));
+
+    I.amOnPage(I.buildUrlWithParams(dashboardPage.postgresqlInstanceSummaryDashboard.cleanUrl, { from: 'now-5m', service_name: service.service_name }));
+    await dashboardPage.waitForDashboardOpened();
+    await dashboardPage.expandEachDashboardRow();
+    await dashboardPage.verifyMetricsExistence(dashboardPage.postgresqlInstanceSummaryDashboard.metrics);
+    await dashboardPage.verifyThereAreNoGraphsWithoutData();
+    await I.verifyCommand('pmm-admin list | grep "postgresql_pgstatmonitor_agent" | grep "Running"');
+    await I.verifyCommand('pmm-admin list | grep "postgres_exporter" | grep "Running"');
+
+    await inventoryAPI.verifyServiceExistsAndHasRunningStatus({
+      serviceType: SERVICE_TYPE.POSTGRESQL,
+      service: 'postgresql',
+    }, service.service_name);
+  },
+).retry(3);
+
+Scenario(
+  'PMM-T1981 - Verify QAN for Postgres after PMM migration to V3 @not-ui-pipeline @pmm-migration',
+  async ({
+    I, queryAnalyticsPage, inventoryAPI, adminPage,
+  }) => {
+    const clientServiceName = (await inventoryAPI.apiGetNodeInfoByServiceName(SERVICE_TYPE.POSTGRESQL, 'pdpgsql_')).service_name;
+
+    I.amOnPage(I.buildUrlWithParams(queryAnalyticsPage.url, { from: 'now-5m' }));
+    queryAnalyticsPage.waitForLoaded();
+    await adminPage.applyTimeRange('Last 12 hours');
+    queryAnalyticsPage.waitForLoaded();
+    await queryAnalyticsPage.filters.selectFilterInGroup(clientServiceName, 'Service Name');
+    queryAnalyticsPage.waitForLoaded();
+    const count = await queryAnalyticsPage.data.getCountOfItems();
+
+    assert.ok(count > 0, `The queries for service ${clientServiceName} instance do NOT exist, check QAN Data`);
+  },
+).retry(1);
--- tests/qa-integration/pmm_pdpgsql_integration_test.js
+++ tests/qa-integration/pmm_pdpgsql_integration_test.js
@@ -1,5 +1,4 @@
 const assert = require('assert');
-const { SERVICE_TYPE } = require('../helper/constants');
 
 Feature('PMM + PDPGSQL Integration Scenarios');
 
@@ -8,57 +7,22 @@ Before(async ({ I }) => {
 });
 
 Scenario(
-  'PMM-T1262 - Verify Postgresql Dashboard Instance Summary has Data @not-ui-pipeline @pdpgsql-pmm-integration @pmm-migration',
+  'PMM-T1262 - Verify Postgresql Dashboard Instance Summary has Data @not-ui-pipeline @pdpgsql-pmm-integration',
   async ({
-    I, dashboardPage, inventoryAPI,
-  }) => {
-    let service = (await inventoryAPI.apiGetNodeInfoByServiceName(SERVICE_TYPE.POSTGRESQL, 'PDPGSQL_'));
-
-    if (!service) {
-      service = (await inventoryAPI.apiGetNodeInfoByServiceName(SERVICE_TYPE.POSTGRESQL, 'pdpgsql_'));
-    }
-
-    I.amOnPage(I.buildUrlWithParams(dashboardPage.postgresqlInstanceSummaryDashboard.cleanUrl, { from: 'now-5m', service_name: service.service_name }));
+           I, dashboardPage, adminPage,
+         }) => {
+    I.amOnPage(dashboardPage.postgresqlInstanceSummaryDashboard.url);
     await dashboardPage.waitForDashboardOpened();
+    await dashboardPage.applyFilter('Service Name', 'pdpgsql_');
+    // Grab containerName from ServiceName
+    const grabServiceName = await I.grabTextFrom('//label[@for="var-service_name"]//following-sibling::*');
+    const partsServiceName = grabServiceName.split('_service');
+    const containerServiceName = partsServiceName[0];
+    I.wait(60);
     await dashboardPage.expandEachDashboardRow();
     await dashboardPage.verifyMetricsExistence(dashboardPage.postgresqlInstanceSummaryDashboard.metrics);
     await dashboardPage.verifyThereAreNoGraphsWithoutData();
-    if (process.env.JOB_NAME === 'pmm3-migration-tests') {
-      await I.verifyCommand('pmm-admin list | grep "postgresql_pgstatmonitor_agent" | grep "Running"');
-      await I.verifyCommand('pmm-admin list | grep "postgres_exporter" | grep "Running"');
-    } else {
-      const containerServiceName = await I.verifyCommand('docker ps -f name=pdpgsql --format "{{ .Names }}"');
-
-      await I.verifyCommand(`docker exec ${containerServiceName} pmm-admin list | grep "postgresql_pgstatmonitor_agent" | grep "Running"`);
-      await I.verifyCommand(`docker exec ${containerServiceName} pmm-admin list | grep "postgres_exporter" | grep "Running"`);
-    }
-
-    await inventoryAPI.verifyServiceExistsAndHasRunningStatus({
-      serviceType: SERVICE_TYPE.POSTGRESQL,
-      service: 'postgresql',
-    }, service.service_name);
+    await I.verifyCommand(`docker exec ${containerServiceName} pmm-admin list | grep "postgresql_pgstatmonitor_agent" | grep "Running"`);
+    await I.verifyCommand(`docker exec ${containerServiceName} pmm-admin list | grep "postgres_exporter" | grep "Running"`);
   },
 ).retry(3);
-
-Scenario(
-  'Verify QAN after PDPGSQL Instances is added @pdpgsql-pmm-integration @not-ui-pipeline @pmm-migration',
-  async ({
-    I, queryAnalyticsPage, inventoryAPI, adminPage,
-  }) => {
-    let clientServiceName = (await inventoryAPI.apiGetNodeInfoByServiceName(SERVICE_TYPE.POSTGRESQL, 'PDPGSQL_')).service_name;
-
-    if (!clientServiceName) {
-      clientServiceName = (await inventoryAPI.apiGetNodeInfoByServiceName(SERVICE_TYPE.POSTGRESQL, 'pdpgsql_')).service_name;
-    }
-
-    I.amOnPage(I.buildUrlWithParams(queryAnalyticsPage.url, { from: 'now-5m' }));
-    queryAnalyticsPage.waitForLoaded();
-    await adminPage.applyTimeRange('Last 12 hours');
-    queryAnalyticsPage.waitForLoaded();
-    await queryAnalyticsPage.filters.selectFilterInGroup(clientServiceName, 'Service Name');
-    queryAnalyticsPage.waitForLoaded();
-    const count = await queryAnalyticsPage.data.getCountOfItems();
-
-    assert.ok(count > 0, `The queries for service ${clientServiceName} instance do NOT exist, check QAN Data`);
-  },
-).retry(1);
