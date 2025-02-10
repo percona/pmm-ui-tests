40b53433fa28cb74924aa052d2d1333a53924a65
Peter Sirotnak <sirotnak.p@gmail.com>
2025-02-10 12:10:53 +0100

    PMM-13733: Fixes for ps

--- tests/migration/ps_test.js
+++ tests/migration/ps_test.js
@@ -29,23 +29,18 @@ Scenario(
 Scenario(
   'PMM-T1983 - Verify QAN have data for Percona Server after PMM migration to V3 @not-ui-pipeline @pmm-migration',
   async ({
-    I, queryAnalyticsPage, inventoryAPI,
+    I, queryAnalyticsPage, inventoryAPI, adminPage,
   }) => {
     const clientServiceName = (await inventoryAPI.apiGetNodeInfoByServiceName(SERVICE_TYPE.POSTGRESQL, 'ps_8')).service_name;
-    const serviceList = [clientServiceName];
 
-    console.log(`Serivce List is: ${JSON.stringify(serviceList)}`);
+    I.amOnPage(I.buildUrlWithParams(queryAnalyticsPage.url, { from: 'now-5m' }));
+    queryAnalyticsPage.waitForLoaded();
+    await adminPage.applyTimeRange('Last 12 hours');
+    queryAnalyticsPage.waitForLoaded();
+    await queryAnalyticsPage.filters.selectFilterInGroup(clientServiceName, 'Service Name');
+    queryAnalyticsPage.waitForLoaded();
+    const count = await queryAnalyticsPage.data.getCountOfItems();
 
-    for (const service of serviceList) {
-      I.amOnPage(I.buildUrlWithParams(queryAnalyticsPage.url, { from: 'now-5m' }));
-      queryAnalyticsPage.waitForLoaded();
-      await adminPage.applyTimeRange('Last 12 hours');
-      queryAnalyticsPage.waitForLoaded();
-      await queryAnalyticsPage.filters.selectFilterInGroup(service, 'Service Name');
-      queryAnalyticsPage.waitForLoaded();
-      const count = await queryAnalyticsPage.data.getCountOfItems();
-
-      assert.ok(count > 0, `The queries for service ${service} instance do NOT exist, check QAN Data`);
-    }
+    assert.ok(count > 0, `The queries for service ${clientServiceName} instance do NOT exist, check QAN Data`);
   },
 ).retry(1);
