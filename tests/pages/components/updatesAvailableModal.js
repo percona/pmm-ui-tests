58e15351716dfb4fca0ba60ed6f1be3fe5e9fe16
yurkovychv <vasyl.yurkovych@percona.com>
2025-03-07 09:34:09 +0200

    PMM-12242 fix locator for updates modal

--- tests/pages/components/updatesAvailableModal.js
+++ tests/pages/components/updatesAvailableModal.js
@@ -2,7 +2,7 @@ const { I } = inject();
 
 class UpdatesAvailableModalComponent {
   constructor() {
-    this.root = locate('[role="dialog"]').withDescendant('$multiple-updates-modal');
+    this.root = locate('[role="dialog"]').withDescendant('[data-testid$="-updates-modal"]');
     this.closeIcon = this.root.find('[aria-label="Close"]');
     this.dismissButton = this.root.find('button').withText('Dismiss');
     this.goToUpdatesPage = this.root.find('button').withText('Go to updates page');
