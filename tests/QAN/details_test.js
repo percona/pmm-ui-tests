84fb0e287e62d88feedb80e616b673e719449e98
Peter Sirotnak <sirotnak.p@gmail.com>
2025-03-26 10:49:47 +0100

    PMM-13636: POC

--- tests/QAN/details_test.js
+++ tests/QAN/details_test.js
@@ -175,11 +175,11 @@ Scenario(
     await I.verifyCommand(`mysql -h 127.0.0.1 -u ${username} -p${password} --port 3317 -e "USE test; CREATE TABLE t1 (c1 INT NOT NULL, c2 VARCHAR(100) NOT NULL, PRIMARY KEY (c1)); insert into t1 values(1,1),(2,2),(3,3),(4,5); explain select * from t1 where c1=1; explain select * from t1 where c2=1; explain select * from t1 where c2>1 and c2<=3;"`);
 
     I.amOnPage(I.buildUrlWithParams(queryAnalyticsPage.url, { from: 'now-15m', refresh: '5s' }));
-    I.wait(60);
-    I.refreshPage();
+    // I.wait(60);
+    // I.refreshPage();
     queryAnalyticsPage.waitForLoaded();
     queryAnalyticsPage.data.searchByValue('explain select * from t1');
-    queryAnalyticsPage.waitForLoaded();
+    I.waitForInvisible(queryAnalyticsPage.data.elements.noResultTableText, 240);
     queryAnalyticsPage.data.selectRow(1);
     queryAnalyticsPage.queryDetails.openExplainTab();
     const expectedError = queryAnalyticsPage.queryDetails.messages.notSupportedExplain;
