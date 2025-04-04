a616eced313b3ff69507209f5ae3bc1221641291
Peter Sirotnak <sirotnak.p@gmail.com>
2025-04-04 07:22:31 +0200

    PMM-13171: Test Fixes

--- tests/dockerConfiguration/externalPostgres_test.js
+++ tests/dockerConfiguration/externalPostgres_test.js
@@ -2,31 +2,11 @@ const { adminPage } = inject();
 
 Feature('Test PMM server with external PostgreSQL').retry(1);
 
-const pathToPMMFramework = adminPage.pathToPMMTests;
 const DOCKER_IMAGE = process.env.DOCKER_VERSION || 'perconalab/pmm-server:3-dev-latest';
-
-const runPMMWithExternalPGWithSSL = `docker run -d -p 8082:80 -p 447:443 \ 
-    --name PMM-T1678 \
-    --network external-pg \
-    -v ${pathToPMMFramework}tls-ssl-setup/postgres/14:/certs \
-    -e PERCONA_TEST_POSTGRES_SSL_CA_PATH=/certs/ca.crt \
-    -e PERCONA_TEST_POSTGRES_SSL_KEY_PATH=/certs/client.pem \
-    -e PERCONA_TEST_POSTGRES_SSL_CERT_PATH=/certs/client.crt \
-    -e PERCONA_TEST_POSTGRES_SSL_MODE=verify-ca \
-    -e PERCONA_TEST_POSTGRES_DBNAME=postgres \
-    -e PERCONA_TEST_POSTGRES_ADDR=pgsql_14:5432 \
-    -e PERCONA_TEST_POSTGRES_USERNAME=pmm \
-    -e PERCONA_TEST_POSTGRES_DBPASSWORD=pmm \
-    ${DOCKER_IMAGE}`;
-
 const data = new DataTable(['composeName', 'containerName', 'postgresqlAddress', 'serverPort']);
 
-data.add(['docker-compose-external-pg', 'pmm-server-external-postgres', 'external-postgres:5432', '8081']);
-// data.add(['docker-compose-external-pg-ssl', 'pmm-server-external-postgres-ssl', 'external-postgres-ssl:5432', '8082']);
-
-BeforeSuite(async ({ I }) => {
-  // await I.verifyCommand(`PMM_SERVER_IMAGE=${DOCKER_IMAGE} docker compose -f docker-compose-external-pg-ssl.yml up -d`);
-});
+// data.add(['docker-compose-external-pg', 'pmm-server-external-postgres', 'external-postgres:5432', '8081']);
+data.add(['docker-compose-external-pg-ssl', 'pmm-server-external-postgres-ssl', 'external-postgres-ssl:5432', '8082']);
 
 AfterSuite(async ({ I }) => {
   await I.verifyCommand('docker compose -f docker-compose-external-pg.yml down -v || true');
