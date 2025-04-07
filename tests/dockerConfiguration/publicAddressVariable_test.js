484f99cdc453148cd8b37f9ff441e5f13ec75be8
Peter Sirotnak <sirotnak.p@gmail.com>
2025-04-07 18:16:11 +0200

    PMM-13171 (#963)

--- tests/dockerConfiguration/publicAddressVariable_test.js
+++ tests/dockerConfiguration/publicAddressVariable_test.js
@@ -2,9 +2,10 @@ const assert = require('assert');
 
 Feature('Tests for PMM_PUBLIC_ADDRESS environment variable');
 
-const dockerVersion = process.env.DOCKER_VERSION || 'perconalab/pmm-server:dev-latest';
+const dockerVersion = process.env.DOCKER_VERSION || 'perconalab/pmm-server:3-dev-latest';
 const contanerName = 'pmm-server-public-address';
 const publicIPs = new DataTable(['testCase', 'publicAddress']);
+const basePmmUrl = 'http://127.0.0.1:8085/';
 
 publicIPs.add(['PMM-T1173', '127.0.0.1']);
 publicIPs.add(['PMM-T1173', '127.0.0.1:8443']);
@@ -12,13 +13,13 @@ publicIPs.add(['PMM-T1174', 'ec2-18-188-74-98.us-east-2.compute.amazonaws.com'])
 publicIPs.add(['PMM-T1174', 'ec2-18-188-74-98.us-east-2.compute.amazonaws.com:8443']);
 
 const runContainerWithPublicAddressVariable = async (I, publicAddress) => {
-  await I.verifyCommand(`docker run -d --restart always -e PERCONA_TEST_PLATFORM_ADDRESS=https://check-dev.percona.com:443 -e PMM_PUBLIC_ADDRESS=${publicAddress} --publish 8085:80 --publish 8443:443 --name ${contanerName} ${dockerVersion}`);
+  await I.verifyCommand(`docker run -d --restart always -e PERCONA_TEST_PLATFORM_ADDRESS=https://check-dev.percona.com:443 -e PMM_PUBLIC_ADDRESS=${publicAddress} --publish 8085:8080 --publish 8443:8443 --name ${contanerName} ${dockerVersion}`);
   await I.wait(30);
 };
 
 const runContainerWithPublicAddressVariableUpgrade = async (I, publicAddress) => {
-  await I.verifyCommand(`docker run -d --restart always -e PERCONA_TEST_PLATFORM_ADDRESS=https://check-dev.percona.com:443 -e PMM_PUBLIC_ADDRESS=${publicAddress} --publish 8085:80 --publish 8443:443 --name ${contanerName} percona/pmm-server:latest`);
-  await I.verifyCommand(`docker exec ${contanerName} yum update -y percona-release`);
+  await I.verifyCommand(`docker run -d --restart always -e PERCONA_TEST_PLATFORM_ADDRESS=https://check-dev.percona.com:443 -e PMM_PUBLIC_ADDRESS=${publicAddress} --publish 8085:8080 --publish 8443:8443 --name ${contanerName} percona/pmm-server:latest`);
+  await I.verifyCommand(`docker exec --user root ${contanerName} yum update -y percona-release`);
   await I.verifyCommand(`docker exec ${contanerName} sed -i'' -e 's^/release/^/experimental/^' /etc/yum.repos.d/pmm2-server.repo`);
   await I.verifyCommand(`docker exec ${contanerName} percona-release enable percona experimental`);
   await I.verifyCommand(`docker exec ${contanerName} yum clean all`);
@@ -26,24 +27,21 @@ const runContainerWithPublicAddressVariableUpgrade = async (I, publicAddress) =>
   await I.wait(30);
 };
 
-Before(async ({ I, portalAPI }) => {
-  await I.Authorize('admin', 'admin');
-});
-
 After(async ({ I, portalAPI }) => {
   await I.verifyCommand('docker stop pmm-server-public-address');
   await I.verifyCommand('docker rm pmm-server-public-address');
 });
 
 Data(publicIPs).Scenario(
-  'PMM-T1173 PMM-T1174 Verify PMM_PUBLIC_ADDRESS env variable with IP @docker-configuration',
+  'PMM-T1173 + PMM-T1174 - Verify PMM_PUBLIC_ADDRESS env variable with IP @docker-configuration',
   async ({
     I, pmmSettingsPage, current,
   }) => {
-    const basePmmUrl = 'http://127.0.0.1:8085/';
     const { publicAddress } = current;
 
     await runContainerWithPublicAddressVariable(I, publicAddress);
+    await I.Authorize('admin', 'admin', basePmmUrl);
+
     await I.amOnPage(basePmmUrl + pmmSettingsPage.advancedSettingsUrl);
     await I.waitForVisible(pmmSettingsPage.fields.publicAddressInput, 30);
     const setPublicAddress = await I.grabValueFrom(pmmSettingsPage.fields.publicAddressInput);
@@ -52,7 +50,7 @@ Data(publicIPs).Scenario(
   },
 );
 
-Scenario(
+Scenario.skip(
   'PMM-T1176 Verify PMM_PUBLIC_ADDRESS env variable after upgrade @docker-configuration',
   async ({
     I, pmmSettingsPage, homePage,
@@ -76,7 +74,7 @@ Scenario(
 );
 
 Scenario(
-  'PMM-T1177 Verify PMM_PUBLIC_ADDRESS env variable can be updated @docker-configuration',
+  'PMM-T1177 - Verify PMM_PUBLIC_ADDRESS env variable can be updated @docker-configuration',
   async ({
     I, pmmSettingsPage,
   }) => {
@@ -84,6 +82,8 @@ Scenario(
     const basePmmUrl = `http://${serverIP}:8085/`;
 
     await runContainerWithPublicAddressVariable(I, '127.0.0.5');
+    await I.wait(30);
+    await I.Authorize('admin', 'admin', basePmmUrl);
     await I.amOnPage(basePmmUrl + pmmSettingsPage.advancedSettingsUrl);
     await I.waitForVisible(pmmSettingsPage.fields.publicAddressInput, 30);
     await pmmSettingsPage.clearPublicAddress();
--- tests/dockerConfiguration/verifySrvDataDirectory_test.js
+++ tests/dockerConfiguration/verifySrvDataDirectory_test.js
@@ -4,42 +4,49 @@ const moment = require('moment');
 Feature('Test PMM server with srv volume and password enw variable');
 
 let testCaseName = '';
-const dockerVersion = process.env.DOCKER_VERSION || 'perconalab/pmm-server:dev-latest';
+const dockerVersion = process.env.DOCKER_VERSION || 'perconalab/pmm-server:3-dev-latest';
 
 const runContainerWithoutDataContainer = async (I) => {
-  await I.verifyCommand(`docker run -v $HOME/srvNoData:/srv -d --restart always --publish 8081:80 --name pmm-server-srv ${dockerVersion}`);
+  await I.verifyCommand('mkdir $HOME/srvNoData/ || true');
+  await I.verifyCommand('chmod -R 777 $HOME/srvNoData/ || true');
+  await I.verifyCommand(`docker run -v $HOME/srvNoData:/srv -d --restart always --publish 8081:8080 --name pmm-server-srv ${dockerVersion}`);
 };
 
 const runContainerWithPasswordVariable = async (I) => {
-  await I.verifyCommand(`docker run -v $HOME/srvPassword:/srv -d -e GF_SECURITY_ADMIN_PASSWORD=newpass --restart always --publish 8082:80 --name pmm-server-password ${dockerVersion}`);
+  await I.verifyCommand('mkdir $HOME/srvPassword/ || true');
+  await I.verifyCommand('chmod -R 777 $HOME/srvPassword/');
+  await I.verifyCommand(`docker run -v $HOME/srvPassword:/srv -d -e GF_SECURITY_ADMIN_PASSWORD=newpass --restart always --publish 8082:8080 --name pmm-server-password ${dockerVersion}`);
 };
 
 const runContainerWithPasswordVariableUpgrade = async (I) => {
-  await I.verifyCommand('docker run -v $HOME/srvPasswordUpgrade:/srv -d -e GF_SECURITY_ADMIN_PASSWORD=newpass --restart always --publish 8084:80 --name pmm-server-password-upgrade percona/pmm-server:latest');
+  await I.verifyCommand('mkdir $HOME/srvPasswordUpgrade || true');
+  await I.verifyCommand('chmod -R 777 $HOME/srvPasswordUpgrade/');
+  await I.verifyCommand(`docker run -v $HOME/srvPasswordUpgrade:/srv -d -e GF_SECURITY_ADMIN_PASSWORD=newpass --restart always --publish 8089:8080 --name pmm-server-password-upgrade ${dockerVersion}`);
+  I.wait(30);
   await I.verifyCommand('docker exec pmm-server-password-upgrade yum update -y percona-release');
-  await I.verifyCommand('docker exec pmm-server-password-upgrade sed -i\'\' -e \'s^/release/^/experimental/^\' /etc/yum.repos.d/pmm2-server.repo');
+  await I.verifyCommand('docker exec pmm-server-password-upgrade sed -i\'\' -e \'s^/release/^/experimental/^\' /etc/yum.repos.d/pmm3-server.repo');
   await I.verifyCommand('docker exec pmm-server-password-upgrade percona-release enable percona experimental');
   await I.verifyCommand('docker exec pmm-server-password-upgrade yum clean all');
   await I.verifyCommand('docker restart pmm-server-password-upgrade');
 };
 
 const runContainerWithDataContainer = async (I) => {
-  await I.verifyCommand(`docker run -v srvFolder:/srv -d --restart always --publish 8083:80 --name pmm-server-srv ${dockerVersion}`);
+  await I.verifyCommand(`docker run -v srvFolder:/srv -d --restart always --publish 8083:8080 --name pmm-server-empty-data-container ${dockerVersion}`);
 };
 
 const stopAndRemoveContainerWithoutDataContainer = async (I) => {
-  await I.verifyCommand('docker stop pmm-server-srv');
-  await I.verifyCommand('docker rm pmm-server-srv');
+  await I.verifyCommand('docker stop pmm-server-srv || true');
+  await I.verifyCommand('docker rm pmm-server-srv || true');
 };
 
 const stopAndRemoveContainerWithPasswordVariable = async (I) => {
-  await I.verifyCommand('docker stop pmm-server-password');
-  await I.verifyCommand('docker rm pmm-server-password');
+  await I.verifyCommand('docker stop pmm-server-password || true');
+  await I.verifyCommand('docker rm pmm-server-password || true');
 };
 
 const stopAndRemoveContainerWithDataContainer = async (I) => {
-  await I.verifyCommand('docker stop pmm-server-srv');
-  await I.verifyCommand('docker rm pmm-server-srv');
+  await I.verifyCommand('docker stop pmm-server-empty-data-container || true');
+  await I.verifyCommand('docker rm pmm-server-empty-data-container || true');
 };
 
 After(async ({ I }) => {
@@ -47,7 +54,7 @@ After(async ({ I }) => {
     await stopAndRemoveContainerWithoutDataContainer(I);
   } else if (testCaseName === 'PMM-T1244') {
     await stopAndRemoveContainerWithDataContainer(I);
-    await I.verifyCommand('docker volume rm srvFolder');
+    await I.verifyCommand('docker volume rm srvFolder || true');
   } else if (testCaseName === 'PMM-T1255') {
     await stopAndRemoveContainerWithPasswordVariable(I);
   }
@@ -56,13 +63,13 @@ After(async ({ I }) => {
 Scenario(
   'PMM-T1243 Verify PMM Server without data container @docker-configuration',
   async ({
-    I, adminPage, queryAnalyticsPage, dashboardPage,
+    I, queryAnalyticsPage, dashboardPage,
   }) => {
     const basePmmUrl = 'http://127.0.0.1:8081/';
 
     await runContainerWithoutDataContainer(I);
-    await I.Authorize('admin', 'admin');
     await I.wait(120);
+    await I.Authorize('admin', 'admin', basePmmUrl);
     testCaseName = 'PMM-T1243';
     await I.amOnPage(basePmmUrl + queryAnalyticsPage.url);
     await I.waitForInvisible(queryAnalyticsPage.data.elements.noResultTableText, 180);
@@ -72,7 +79,6 @@ Scenario(
     assert.ok(qanRows > 0, 'Query Analytics are empty');
     await I.amOnPage(`${basePmmUrl + dashboardPage.nodeSummaryDashboard.url}?orgId=1&refresh=5s`);
     await dashboardPage.waitForAllGraphsToHaveData(300);
-    await dashboardPage.verifyThereAreNoGraphsWithNA();
     await dashboardPage.verifyThereAreNoGraphsWithoutData();
 
     await stopAndRemoveContainerWithoutDataContainer(I);
@@ -81,23 +87,26 @@ Scenario(
     const logs = await I.verifyCommand('docker logs pmm-server-srv');
 
     assert.ok(!logs.includes('Error: The directory named as part of the path /srv/logs/supervisord.log does not exist'));
-    await I.amOnPage(basePmmUrl + queryAnalyticsPage.url);
-    adminPage.setAbsoluteTimeRange(moment().subtract({ hours: 12 }).format('YYYY-MM-DD HH:mm:00'), moment().subtract({ minutes: 1 }).format('YYYY-MM-DD HH:mm:00'));
+    const url = I.buildUrlWithParams(
+      basePmmUrl + queryAnalyticsPage.url,
+      { from: 'now-30m' },
+    );
 
-    await I.waitForInvisible(queryAnalyticsPage.data.elements.noResultTableText, 180);
+    await I.amOnPage(url);
+
+    await I.waitForInvisible(queryAnalyticsPage.data.elements.noResultTableText, 240);
     await I.waitForVisible(queryAnalyticsPage.data.elements.queryRows);
     const qanRowsAfterRestart = await I.grabNumberOfVisibleElements(queryAnalyticsPage.data.elements.queryRows);
 
     assert.ok(qanRowsAfterRestart > 0, 'Query Analytics are empty after restart of docker container');
 
     await I.amOnPage(`${basePmmUrl + dashboardPage.nodeSummaryDashboard.url}?orgId=1&refresh=5s`);
-    await dashboardPage.verifyThereAreNoGraphsWithNA();
     await dashboardPage.verifyThereAreNoGraphsWithoutData();
   },
 );
 
 Scenario(
-  'PMM-T1244 Verify PMM Server with empty data container @docker-configuration',
+  'PMM-T1244 - Verify PMM Server with empty data container @docker-configuration',
   async ({
     I, adminPage, dashboardPage, queryAnalyticsPage,
   }) => {
@@ -105,8 +114,8 @@ Scenario(
 
     await I.verifyCommand('docker volume create srvFolder');
     await runContainerWithDataContainer(I);
-    await I.Authorize('admin', 'admin');
     await I.wait(120);
+    await I.Authorize('admin', 'admin', basePmmUrl);
     testCaseName = 'PMM-T1244';
     await I.amOnPage(basePmmUrl + queryAnalyticsPage.url);
     I.dontSeeElement(queryAnalyticsPage.data.elements.noResultTableText);
@@ -116,13 +125,12 @@ Scenario(
     assert.ok(qanRows > 0, 'Query Analytics are empty');
     await I.amOnPage(`${basePmmUrl + dashboardPage.nodeSummaryDashboard.url}?orgId=1&refresh=5s`);
     await dashboardPage.waitForAllGraphsToHaveData(300);
-    await dashboardPage.verifyThereAreNoGraphsWithNA();
     await dashboardPage.verifyThereAreNoGraphsWithoutData();
 
     await stopAndRemoveContainerWithDataContainer(I);
     await runContainerWithDataContainer(I);
     await I.wait(60);
-    const logs = await I.verifyCommand('docker logs pmm-server-srv');
+    const logs = await I.verifyCommand('docker logs pmm-server-empty-data-container');
 
     assert.ok(!logs.includes('Error: The directory named as part of the path /srv/logs/supervisord.log does not exist'));
     await I.amOnPage(basePmmUrl + queryAnalyticsPage.url);
@@ -136,13 +144,12 @@ Scenario(
 
     await I.amOnPage(`${basePmmUrl + dashboardPage.nodeSummaryDashboard.url}?orgId=1&refresh=5s`);
     await dashboardPage.waitForAllGraphsToHaveData(180);
-    await dashboardPage.verifyThereAreNoGraphsWithNA();
     await dashboardPage.verifyThereAreNoGraphsWithoutData();
-    I.say(await I.verifyCommand('docker logs pmm-server-srv'));
+    I.say(await I.verifyCommand('docker logs pmm-server-empty-data-container'));
   },
 );
-// Skipped due to bug: https://jira.percona.com/browse/PMM-10325
-Scenario.skip(
+
+Scenario(
   'PMM-T1255 Verify GF_SECURITY_ADMIN_PASSWORD environment variable @docker-configuration',
   async ({
     I, homePage, loginPage,
@@ -157,20 +164,20 @@ Scenario.skip(
 
     assert.ok(!logs.includes('Configuration warning: unknown environment variable "GF_SECURITY_ADMIN_PASSWORD=newpass".'));
 
-    await I.Authorize();
+    await I.Authorize('admin', 'admin', basePmmUrl);
     await I.amOnPage(basePmmUrl + homePage.url);
-    await I.waitForVisible('//*[contains(text(), "invalid username or password")]');
+    await I.waitForVisible('//h1[text()="Percona Monitoring and Management"]');
     await I.unAuthorize();
     await I.refreshPage();
     await I.waitInUrl(loginPage.url);
-    await I.Authorize('admin', 'newpass');
+    await I.Authorize('admin', 'newpass', basePmmUrl);
     await I.wait(1);
     await I.refreshPage();
     await I.waitForElement(homePage.fields.dashboardHeaderLocator, 60);
     await I.verifyCommand('docker exec -t pmm-server-password change-admin-password anotherpass');
     await I.unAuthorize();
     await I.waitInUrl(loginPage.url);
-    await I.Authorize('admin', 'anotherpass');
+    await I.Authorize('admin', 'anotherpass', basePmmUrl);
     await I.wait(5);
     await I.refreshPage();
     await I.waitForElement(homePage.fields.dashboardHeaderLocator, 60);
@@ -178,16 +185,16 @@ Scenario.skip(
 );
 
 Scenario(
-  'PMM-T1256 Verify GF_SECURITY_ADMIN_PASSWORD environment variable after upgrade @docker-configuration',
+  'PMM-T1256 Verify GF_SECURITY_ADMIN_PASSWORD environment variable after upgrade',
   async ({
     I, homePage,
   }) => {
-    const basePmmUrl = 'http://127.0.0.1:8084/';
+    const basePmmUrl = 'http://127.0.0.1:8089/';
 
     await runContainerWithPasswordVariableUpgrade(I);
     await I.wait(30);
     testCaseName = 'PMM-T1256';
-    await I.Authorize('admin', 'newpass');
+    await I.Authorize('admin', 'newpass', basePmmUrl);
     await I.amOnPage(basePmmUrl + homePage.url);
     await I.waitForElement(homePage.fields.dashboardHeaderLocator, 60);
     const { versionMinor } = await homePage.getVersions();
@@ -195,7 +202,7 @@ Scenario(
     await homePage.upgradePMM(versionMinor, 'pmm-server-password-upgrade');
     await I.unAuthorize();
     await I.wait(5);
-    await I.Authorize('admin', 'newpass');
+    await I.Authorize('admin', 'newpass', basePmmUrl);
     await I.refreshPage();
     await I.waitForElement(homePage.fields.dashboardHeaderLocator, 60);
   },
