d10c3907b19c666c6efd520741608f69c3111d2a
Peter Sirotnak <sirotnak.p@gmail.com>
2025-04-11 11:08:54 +0200

    PMM-13171: PR Fixes

--- tests/dockerConfiguration/verifySrvDataDirectory_test.js
+++ tests/dockerConfiguration/verifySrvDataDirectory_test.js
@@ -8,19 +8,19 @@ const dockerVersion = process.env.DOCKER_VERSION || 'perconalab/pmm-server:3-dev
 
 const runContainerWithoutDataContainer = async (I) => {
   await I.verifyCommand('mkdir $HOME/srvNoData/ || true');
-  await I.verifyCommand('chown -R 1001:1001 $HOME/srvNoData/ || true');
+  await I.verifyCommand('sudo chown -R 1001:1001 $HOME/srvNoData/ || true');
   await I.verifyCommand(`docker run -v $HOME/srvNoData:/srv -d --restart always --publish 8081:8080 --name pmm-server-srv ${dockerVersion}`);
 };
 
 const runContainerWithPasswordVariable = async (I) => {
   await I.verifyCommand('mkdir $HOME/srvPassword/ || true');
-  await I.verifyCommand('chown -R 1001:1001 $HOME/srvPassword/');
+  await I.verifyCommand('sudo chown -R 1001:1001 $HOME/srvPassword/');
   await I.verifyCommand(`docker run -v $HOME/srvPassword:/srv -d -e GF_SECURITY_ADMIN_PASSWORD=newpass --restart always --publish 8082:8080 --name pmm-server-password ${dockerVersion}`);
 };
 
 const runContainerWithPasswordVariableUpgrade = async (I) => {
   await I.verifyCommand('mkdir $HOME/srvPasswordUpgrade || true');
-  await I.verifyCommand('chown -R 1001:1001 $HOME/srvPasswordUpgrade/');
+  await I.verifyCommand('sudo chown -R 1001:1001 $HOME/srvPasswordUpgrade/');
   await I.verifyCommand(`docker run -v $HOME/srvPasswordUpgrade:/srv -d -e GF_SECURITY_ADMIN_PASSWORD=newpass --restart always --publish 8089:8080 --name pmm-server-password-upgrade ${dockerVersion}`);
   I.wait(30);
   await I.verifyCommand('docker exec pmm-server-password-upgrade yum update -y percona-release');
