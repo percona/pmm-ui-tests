a977af7b32d7032fbf1a0065569dcdc2ac1e1f3d
Peter Sirotnak <sirotnak.p@gmail.com>
2025-05-12 07:41:30 +0200

    PMM-7: Adds clickhouse tests to upgrade

--- tests/upgrade/externalDataSources_test.js
+++ tests/upgrade/externalDataSources_test.js
@@ -4,7 +4,7 @@ Before(async ({ I }) => {
   await I.Authorize();
 });
 
-Scenario('PMM-T2018 - Verify internal clickhouse is not running when using external clickhouse @post-external-data-sources-upgrade', async ({ I }) => {
+Scenario('PMM-T2018 - Verify internal clickhouse is not running when using external clickhouse @pre-external-data-sources-upgrade @post-external-data-sources-upgrade', async ({ I }) => {
   const response = await I.DockerContainerExec('pmm-server', 'supervisorctl status');
 
   I.assertFalse(response.includes('clickhouse'), 'Clickhouse should not run on pmm server!');
