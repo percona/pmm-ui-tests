8acfced5920325a840fc301312af49ad6ff2b509
Peter Sirotnak <sirotnak.p@gmail.com>
2025-05-21 13:24:15 +0200

    PMM-7: Fixes for the new grafana

--- tests/QAN/overview_test.js
+++ tests/QAN/overview_test.js
@@ -268,7 +268,8 @@ Scenario(
   },
 );
 
-Scenario(
+// TODO: Un-skip and refactor after https://perconadev.atlassian.net/browse/PMM-14002 is fixed
+Scenario.skip(
   'PMM-T204 - Verify small and N/A values on sparkline @qan',
   async ({ I, queryAnalyticsPage }) => {
     const secondCell = queryAnalyticsPage.data.elements.queryValue(3, 3);
