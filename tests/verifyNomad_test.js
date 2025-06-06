b2afa308b2c982a70fa355e56f76bf74bfda1417
Peter Sirotnak <sirotnak.p@gmail.com>
2025-06-06 09:10:11 +0200

    PMM-7: Fix PMM-T2036

---
+++ tests/verifyNomad_test.js
@@ -0,0 +1,5 @@
+Feature('Tests for Nomad in PMM');
+
+Scenario('PMM-T2026 - Verify nomad client is not running if Nomad server is stopped @nomad', async () => {
+  console.log('Running Nomad test');
+});
