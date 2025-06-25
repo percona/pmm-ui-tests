c6e119bd1447f41fb1726e881d0273512917f011
Peter Sirotnak <sirotnak.p@gmail.com>
2025-06-25 10:07:50 +0200

    PMM-7: Remove volume after test

--- tests/dockerConfiguration/externalPostgres_test.js
+++ tests/dockerConfiguration/externalPostgres_test.js
@@ -11,8 +11,6 @@ data.add(['docker-compose-external-pg-ssl', 'pmm-server-external-postgres-ssl', 
 AfterSuite(async ({ I }) => {
   await I.verifyCommand('docker compose -f docker-compose-external-pg.yml down -v || true');
   await I.verifyCommand('docker compose -f docker-compose-external-pg-ssl.yml down -v || true');
-  await I.verifyCommand('docker volume rm pmm-server-external-pg-ssl || true');
-  await I.verifyCommand('docker volume rm pmm-server-external-pg || true');
 });
 
 Data(data).Scenario(
