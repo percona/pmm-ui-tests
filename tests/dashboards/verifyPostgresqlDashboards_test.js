c0012b8208e2a97b6e7ac664db42a581e7ef90d5
Peter Sirotnak <sirotnak.p@gmail.com>
2025-06-27 09:58:15 +0200

    PMM-7: Fix pgsql dashboards

--- tests/dashboards/verifyPostgresqlDashboards_test.js
+++ tests/dashboards/verifyPostgresqlDashboards_test.js
@@ -23,11 +23,11 @@ Scenario(
   'PMM-T2050 - Verify PostgreSQL Instance Summary Dashboard @nightly @dashboards',
   async ({ I, dashboardPage }) => {
     const { service_name } = await inventoryAPI.getServiceDetailsByStartsWithName('pdpgsql_');
-    const url = I.buildUrlWithParams(dashboardPage.postgresqlInstanceSummaryDashboard.url, { service_name, from: 'now-5m' });
+    const url = I.buildUrlWithParams(dashboardPage.postgresqlInstanceSummaryDashboard.url, { service_name, from: 'now-30m' });
 
     I.amOnPage(url);
     dashboardPage.waitForDashboardOpened();
-    await dashboardPage.verifySlowQueriesPanel('5 minutes');
+    await dashboardPage.verifySlowQueriesPanel('30 minutes');
     await dashboardPage.expandEachDashboardRow();
     await dashboardPage.verifyMetricsExistence(dashboardPage.postgresqlInstanceSummaryDashboard.metrics);
     await dashboardPage.verifyThereAreNoGraphsWithoutData();
