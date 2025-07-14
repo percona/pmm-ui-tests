eb746eb3403f5adabb86ace8a6264ad7ac255070
Peter Sirotnak <sirotnak.p@gmail.com>
2025-07-14 16:31:34 +0200

    PMM-7: debug

--- tests/qa-integration/pmm_psmdb_integration_test.js
+++ tests/qa-integration/pmm_psmdb_integration_test.js
@@ -216,15 +216,14 @@ Scenario(
 ).retry(1);
 
 Scenario('PMM-T1889 - Verify Mongo replication lag graph shows correct info @pmm-psmdb-replica-integration', async ({ I, dashboardPage, adminPage, codeceptjsConfig }) => {
-  console.log(codeceptjsConfig);
   const lagValue = 10;
   const testConfigFile = `c = rs.conf(); c.members[2].secondaryDelaySecs = ${lagValue}; c.members[2].priority = 0; c.members[2].hidden = true; rs.reconfig(c);`;
   const serviceName = 'rs103';
   const graphName = 'Replication Lag';
 
   await I.verifyCommand(`sudo docker exec rs101 mongo "mongodb://root:root@localhost/?replicaSet=rs" --eval "${testConfigFile}"`);
-  I.amOnPage(I.buildUrlWithParameters(dashboardPage.mongodbReplicaSetSummaryDashboard.cleanUrl, { from: 'now-5m', refresh: '5s' }));
-  console.log(I.buildUrlWithParameters(dashboardPage.mongodbReplicaSetSummaryDashboard.cleanUrl, { from: 'now-5m', refresh: '5s' }))
+  console.log(await I.buildUrlWithParameters(dashboardPage.mongodbReplicaSetSummaryDashboard.cleanUrl, { from: 'now-5m', refresh: '5s' }))
+  I.amOnPage(await I.buildUrlWithParameters(dashboardPage.mongodbReplicaSetSummaryDashboard.cleanUrl, { from: 'now-5m', refresh: '5s' }));
   I.wait(10);
   console.log(await I.grabCurrentUrl());
   dashboardPage.waitForDashboardOpened();
