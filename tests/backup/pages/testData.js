d14e11ed6da8632742c924282f4f993c1729abd8
Peter Sirotnak <sirotnak.p@gmail.com>
2025-09-19 14:54:59 +0200

    PMM-14156: Fix minio location

--- tests/backup/pages/testData.js
+++ tests/backup/pages/testData.js
@@ -1,7 +1,7 @@
 const { isOvFAmiJenkinsJob } = require('../../helper/constants');
 
 const storageLocationConnection = {
-  endpoint: isOvFAmiJenkinsJob ? 'http://127.0.0.1:9001' : 'http://minio:9000',
+  endpoint: isOvFAmiJenkinsJob ? `http://${process.env.JENKINS_IP}:9001` : 'http://minio:9000',
   bucket_name: 'bcp',
   access_key: 'minio1234',
   secret_key: 'minio1234',
