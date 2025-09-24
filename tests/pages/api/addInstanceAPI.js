a348abab71911545dd1444629540cc59ce881f94
Peter Sirotnak <sirotnak.p@gmail.com>
2025-09-24 13:47:47 +0200

    PMM-14156: Fix

--- tests/pages/api/addInstanceAPI.js
+++ tests/pages/api/addInstanceAPI.js
@@ -349,6 +349,8 @@ module.exports = {
     const headers = { Authorization: `Basic ${await I.getAuth()}` };
     const resp = await I.sendPostRequest('v1/management/services', body, headers);
 
+    console.log(resp.data);
+
     I.assertEqual(
       resp.status,
       200,
