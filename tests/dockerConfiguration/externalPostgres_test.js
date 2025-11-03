350d9e8535eb8dba97707ab5c7f43c5fe0ee6b1e
Peter Sirotnak <sirotnak.p@gmail.com>
2025-11-03 08:28:13 +0100

    PMM-7: Fix tests

--- tests/dockerConfiguration/externalPostgres_test.js
+++ tests/dockerConfiguration/externalPostgres_test.js
@@ -29,7 +29,9 @@ Data(data).Scenario(
     const serviceName = 'pmm-server-postgresql';
     const postgresDataSourceLocator = locate('div').withChild(locate('h2 > a').withText('PostgreSQL'));
 
+    console.log(await I.verifyCommand('ls /usr/bin/'));
     const ansibleResult = await I.verifyCommand(`ANSIBLE_INTERPRETER_PYTHON=auto_silent ansible-playbook --connection=local --inventory 127.0.0.1, --limit 127.0.0.1 testdata/external-services/${ansibleName}.yml --extra-vars "pmm_server_image=${dockerImage}"`);
+
     console.log(`Ansible result is: ${ansibleResult}`);
 
     await I.Authorize('admin', 'admin', basePmmUrl);
