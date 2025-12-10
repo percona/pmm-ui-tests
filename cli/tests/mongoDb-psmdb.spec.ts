3578f91c071ab56b24a2c55fab28cc102056798f
yurkovychv <vasyl.yurkovych@percona.com>
2025-12-10 18:44:33 +0200

    PMM-14375 add --agent-env-vars tests

--- cli/tests/mongoDb-psmdb.spec.ts
+++ cli/tests/mongoDb-psmdb.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 import * as cli from '@helpers/cli-helper';
-import { removeMongoService } from '@root/helpers/pmm-admin';
+import { getPmmAdminMinorVersion, removeMongoService } from '@root/helpers/pmm-admin';
 import { clientCredentialsFlags } from '@helpers/constants';
 
 const replIpPort = 'rs101:27017';
@@ -8,11 +8,13 @@ const mongoPushMetricsServiceName = 'mongo_push_1';
 const mongoPullMetricsServiceName = 'mongo_pull_1';
 const mongoServiceName = 'mongo_service_1';
 const containerName = 'rs101';
+let adminVersion: number;
 
 test.describe('Percona Server MongoDB (PSMDB) CLI tests', async () => {
   test.beforeAll(async ({}) => {
     const result = await cli.exec(`docker ps | grep ${containerName} | awk '{print $NF}'`);
     await result.outContains(containerName, 'PSMDB rs101 docker container should exist. please run pmm-framework with --database psmdb,SETUP_TYPE=pss');
+    adminVersion = await getPmmAdminMinorVersion(containerName);
   });
 
   test('run pmm-admin', async ({}) => {
@@ -103,6 +105,27 @@ test.describe('Percona Server MongoDB (PSMDB) CLI tests', async () => {
     });
 
     await removeMongoService(containerName, serviceName);
+  });
+
+  test('PMM-T2128 verify environment variable passed using --agent-env-vars parameter from pmm agent to mongodb_exporter', async ({}) => {
+    test.skip(adminVersion < 6, 'This test is relevant for pmm-client version 3.6.0 and above');
+
+    await expect(async () => {
+      const mongodbExporterPid = await cli.exec(`docker exec ${containerName} pgrep -f mongodb_exporter`);
+      const vmAgentOpts = await cli.exec(`docker exec ${containerName} cat /proc/${mongodbExporterPid.getStdOutLines()[0]}/environ`);
+      await vmAgentOpts.outContains('KRB5_CLIENT_KTNAME=/keytabs/mongodb.keytab');
+    }).toPass({
+      timeout: 60_000,
+      intervals: [5_000],
+    });
+  });
+
+  test('PMM-T2129 verify validation for --agent-env-vars parameter when adding mondogb for monitoring', async ({}) => {
+    test.skip(adminVersion < 6, 'This test is relevant for pmm-client version 3.6.0 and above');
+
+    const output = await cli.exec(`docker exec ${containerName} pmm-admin add mongodb ${clientCredentialsFlags} --agent-env-vars="TEST=123" --service-name=test`);
+    await output.exitCodeEquals(1);
+    await output.outContains('invalid environment variable name: TEST=123 (must match [A-Z_][A-Z0-9_]*)');
   });
 
   test('PMM-T2005 verify PBM Agent health status metric is correct', async ({}) => {
