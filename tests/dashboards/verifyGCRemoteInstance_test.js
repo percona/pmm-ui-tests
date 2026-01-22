4cc79c218d7298a13a6d3d66e0abffb9262cb3e1
yurkovychv <vasyl.yurkovych@percona.com>
2026-01-22 13:52:30 +0200

    PMM-7 remove commented code

--- tests/dashboards/verifyGCRemoteInstance_test.js
+++ tests/dashboards/verifyGCRemoteInstance_test.js
@@ -136,8 +136,6 @@ Data(instances).Scenario(
 
     I.amOnPage(I.buildUrlWithParams(queryAnalyticsPage.url, { from: 'now-5m', service_name: instanceDetails.serviceName }));
     queryAnalyticsPage.waitForLoaded();
-    // await queryAnalyticsPage.filters.selectFilter(instanceDetails.serviceName);
-    // queryAnalyticsPage.waitForLoaded();
     const count = await queryAnalyticsPage.data.getCountOfItems();
 
     assert.ok(count > 0, `The queries for service ${instanceDetails.serviceName} instance do NOT exist`);
