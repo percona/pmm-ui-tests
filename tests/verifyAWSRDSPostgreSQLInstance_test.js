0ca14e945fb77e8ee9d0eda27baa86e3b8131455
Vasyl Yurkovych <59879559+yurkovychv@users.noreply.github.com>
2026-01-22 16:25:14 +0200

    PMM-7 update dashboard tests for AWS and Aurora (#1044)
    * PMM-7 update dashboard tests for AWS and Aurora
    
    * PMM-7 fix aurora pg 16 test
    
    * PMM-7 retry GCP test
    
    * PMM-7 retry GCP test
    
    * PMM-7 adjust GCP test
    
    * PMM-7 adjust GCP test
    
    * PMM-7 remove commented code

--- tests/verifyAWSRDSPostgreSQLInstance_test.js
+++ tests/verifyAWSRDSPostgreSQLInstance_test.js
@@ -151,7 +151,7 @@ Data(instances).Scenario(
 
     // Wait 10 seconds before test to start getting metrics
     I.wait(10);
-    I.amOnPage(I.buildUrlWithParams(dashboardPage.postgresqlInstanceOverviewDashboard.cleanUrl, {
+    I.amOnPage(I.buildUrlWithParams(dashboardPage.postgresqlInstanceOverviewDashboard.url, {
       node_name: serviceName,
       from: 'now-5m',
     }));
