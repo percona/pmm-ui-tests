b47941f6ac812f172833586953715003bb4e6f08
Peter Sirotnak <sirotnak.p@gmail.com>
2026-02-09 10:17:45 +0100

    PMM-7: Code cleanup

--- tests/upgrade/amiOvfUpgrade_test.js
+++ tests/upgrade/amiOvfUpgrade_test.js
@@ -4,7 +4,7 @@ Before(async ({ I }) => {
   await I.Authorize();
 });
 
-Scenario('PMM-T11111 - Change password before upgrade in AMI/OVF tests @ami-ovf-pre-upgrade', async ({ I, changePasswordPage }) => {
+Scenario('Change password before upgrade in AMI/OVF tests @ami-ovf-pre-upgrade', async ({ I, changePasswordPage }) => {
   I.amOnPage(changePasswordPage.url);
   changePasswordPage.fillChangePasswordForm(process.env.ADMIN_PASSWORD, 'pmm3admin!');
   changePasswordPage.applyChanges();
