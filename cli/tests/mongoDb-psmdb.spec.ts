8e01875e992e7d8b3ef0c93f7f7580ed47257f03
Peter Sirotnak <sirotnak.p@gmail.com>
2026-03-09 18:21:02 +0100

    PMM-14412: Mongo inventory change agent mongodb-exporter

--- cli/tests/mongoDb-psmdb.spec.ts
+++ cli/tests/mongoDb-psmdb.spec.ts
@@ -274,6 +274,7 @@ test.describe('Percona Server MongoDB (PSMDB) CLI tests', async () => {
 
   test('PMM-T9999 - TEST06', async ({ }) => {
     const agentId = (await cli.exec(`docker exec ${containerName} pmm-admin inventory list agents | grep "mongodb_exporter" | awk -F" " '{print $3}'`)).getStdOutLines()[0];
+    console.log((await cli.exec(`docker exec ${containerName} pmm-admin list`)).stdout);
     const disabledOutput = await cli.exec(`docker exec ${containerName} pmm-admin inventory change agent mongodb-exporter ${agentId} --enable=false`);
     await disabledOutput.assertSuccess();
     const disabledAgentListOutput = await cli.exec(`docker exec ${containerName} pmm-admin list`);
