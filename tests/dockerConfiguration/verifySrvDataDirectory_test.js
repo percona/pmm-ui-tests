0b59d1b6fdd56180a9f0c47ff9738034a2ee5c9a
Peter Sirotnak <sirotnak.p@gmail.com>
2026-03-11 13:30:03 +0100

    PMM-7: Fix GF_SECURITY_ADMIN_PASSWORD

--- tests/dockerConfiguration/verifySrvDataDirectory_test.js
+++ tests/dockerConfiguration/verifySrvDataDirectory_test.js
@@ -175,8 +175,7 @@ Scenario(
     await I.wait(1);
     await I.amOnPage(basePmmUrl + homePage.url);
     await I.waitForElement(homePage.fields.dashboardHeaderLocator, 60);
-    console.log('Response for anotherpass is: ');
-    console.log(await I.verifyCommand('docker exec -t pmm-server-password change-admin-password anotherpass'));
+    await I.verifyCommand('docker exec -t pmm-server-password change-admin-password anotherpass');
     await I.unAuthorize();
     await I.wait(5);
     await I.amOnPage(basePmmUrl + loginPage.url);
