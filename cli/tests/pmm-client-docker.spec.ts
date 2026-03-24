b62185dce346f968f3365f6ddfc39f1660072560
Peter Sirotnak <sirotnak.p@gmail.com>
2026-03-24 07:14:55 +0100

    PMM-7: Fix tests

--- cli/tests/pmm-client-docker.spec.ts
+++ cli/tests/pmm-client-docker.spec.ts
@@ -6,10 +6,10 @@ test.describe('PMM Client Docker CLI tests', async () => {
     const response = await cli.exec('docker compose -f test-setup/docker-compose-pmm-client.yaml up -d --quiet-pull');
     await cli.exec('sleep 2');
     const mysqlResponse = await cli.exec('docker exec pmm-client-1 pmm-admin add mysql --username=pmm --password=pmm-pass --service-name=ps-8.0 --query-source=perfschema --host=ps-1 --port=3306 --server-url=https://admin:admin@pmm-server-1:8443 --server-insecure-tls=true');
-    console.log(mysqlResponse);
     await cli.exec('docker exec pmm-client-1 pmm-admin add postgresql --query-source=pgstatements --username=pmm --password=pmm-pass --service-name=pdpgsql-1 --host=pdpgsql-1 --port=5432 --server-url=https://admin:admin@pmm-server-1:8443 --server-insecure-tls=true');
     await cli.exec('docker exec pmm-client-1 pmm-admin add mongodb --username=pmm --password=pmm-pass --service-name=mongodb-7.0  --host=psmdb-1 --port=27017 --server-url=https://admin:admin@pmm-server-1:8443 --server-insecure-tls=true');
   });
+
   /**
    * @link https://github.com/percona/pmm-qa/blob/main/pmm-tests/pmm-2-0-bats-tests/pmm-client-docker-tests.bats#L6
    */
