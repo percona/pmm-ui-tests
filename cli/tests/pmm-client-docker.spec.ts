f64f762cfe72191940635e207d62a30129d8028c
Peter Sirotnak <sirotnak.p@gmail.com>
2026-03-24 10:09:08 +0100

    PMM-7: Fix tests

--- cli/tests/pmm-client-docker.spec.ts
+++ cli/tests/pmm-client-docker.spec.ts
@@ -4,9 +4,12 @@ import * as cli from '@helpers/cli-helper';
 test.describe('PMM Client Docker CLI tests', async () => {
   test.beforeAll(async ({}) => {
     const response = await cli.exec('docker compose -f test-setup/docker-compose-pmm-client.yaml up -d --quiet-pull');
-    await cli.exec('docker network connect docker-client-check pmm-server')
+    console.log(response);
+    const connectResponse = await cli.exec('docker network connect docker-client-check pmm-server');
+    console.log(connectResponse);
     await cli.exec('sleep 2');
-    await cli.exec('docker exec pmm-client-1 pmm-admin add mysql --username=pmm --password=pmm-pass --service-name=ps-8.0 --query-source=perfschema --host=ps-1 --port=3306 --server-url=https://admin:admin@pmm-server:8443 --server-insecure-tls=true');
+    const mysql = await cli.exec('docker exec pmm-client-1 pmm-admin add mysql --username=pmm --password=pmm-pass --service-name=ps-8.0 --query-source=perfschema --host=ps-1 --port=3306 --server-url=https://admin:admin@pmm-server:8443 --server-insecure-tls=true');
+    console.log(mysql);
     await cli.exec('docker exec pmm-client-1 pmm-admin add postgresql --query-source=pgstatements --username=pmm --password=pmm-pass --service-name=pdpgsql-1 --host=pdpgsql-1 --port=5432 --server-url=https://admin:admin@pmm-server:8443 --server-insecure-tls=true');
     await cli.exec('docker exec pmm-client-1 pmm-admin add mongodb --username=pmm --password=pmm-pass --service-name=mongodb-7.0  --host=psmdb-1 --port=27017 --server-url=https://admin:admin@pmm-server:8443 --server-insecure-tls=true');
   });
