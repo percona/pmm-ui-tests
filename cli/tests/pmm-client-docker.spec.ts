43829069651447122662de84131b808091a1b067
yurkovychv <vasyl.yurkovych@percona.com>
2026-04-08 16:13:58 +0300

    update cli tests

--- cli/tests/pmm-client-docker.spec.ts
+++ cli/tests/pmm-client-docker.spec.ts
@@ -3,7 +3,7 @@ import * as cli from '@helpers/cli-helper';
 
 test.describe('PMM Client Docker CLI tests', { tag: '@client-docker' }, async () => {
   test.beforeAll(async ({}) => {
-    await cli.exec('docker compose -f test-setup/docker-compose-pmm-client.yaml up -d --quiet-pull');
+    await cli.exec(`DOCKER_VERSION=${process.env.DOCKER_VERSION} CLIENT_DOCKER_VERSION=${process.env.CLIENT_DOCKER_VERSION} docker compose -f test-setup/docker-compose-pmm-client.yaml up -d`);
     await cli.exec('sleep 2');
     await cli.exec('docker exec pmm-client-1 pmm-admin add mysql --username=pmm --password=pmm-pass --service-name=ps-8.0 --query-source=perfschema --host=ps-1 --port=3306 --server-url=https://admin:admin@pmm-server-1:8443 --server-insecure-tls=true');
     await cli.exec('docker exec pmm-client-1 pmm-admin add postgresql --query-source=pgstatements --username=pmm --password=pmm-pass --service-name=pdpgsql-1 --host=pdpgsql-1 --port=5432 --server-url=https://admin:admin@pmm-server-1:8443 --server-insecure-tls=true');
@@ -86,7 +86,7 @@ test.describe('PMM Client Docker CLI tests', { tag: '@client-docker' }, async ()
 test.describe('-promscrape.maxScapeSize tests', { tag: '@client-docker' }, async () => {
   const defaultScrapeSize = '64';
   test.beforeAll(async () => {
-    await (await cli.exec('docker compose -f test-setup/docker-compose-scrape-intervals.yml up -d')).assertSuccess();
+    await (await cli.exec(`DOCKER_VERSION=${process.env.DOCKER_VERSION} CLIENT_DOCKER_VERSION=${process.env.CLIENT_DOCKER_VERSION} docker compose -f test-setup/docker-compose-scrape-intervals.yml up -d`)).assertSuccess();
   });
 
   test.afterAll(async () => {
