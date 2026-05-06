9d274d55e4cbfe8822bf4acb915a7bdb3452571e
Peter Sirotnak <sirotnak.p@gmail.com>
2026-05-06 10:26:00 +0200

    PMM-7: Fix dashboard location

--- tests/upgrade/dashboards_test.js
+++ tests/upgrade/dashboards_test.js
@@ -48,6 +48,8 @@ Scenario(
     const resp2 = await grafanaAPI.createCustomDashboard('test-dashboard', experimentalFolder.id);
     const workFolder = await I.verifyCommand('pwd');
 
+    console.log(`Dashboard json file location is: ${workFolder}/dashboard.json`)
+
     await I.writeFileSync(`${workFolder}/dashboard.json`, JSON.stringify({
       DASHBOARD1_UID: resp1.uid,
       DASHBOARD2_UID: resp2.uid,
